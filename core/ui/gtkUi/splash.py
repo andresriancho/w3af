8951154aaa2bcef54a43c2786ee90ea6bb45435c
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2008-04-07 17:58:07 +0000

    Fixed small bug that would never have raised.

--- core/ui/gtkUi/splash.py
+++ core/ui/gtkUi/splash.py
@@ -19,6 +19,8 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
+import sys
+
 try:
     import pygtk
     pygtk.require('2.0')
