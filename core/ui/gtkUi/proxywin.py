4799693d04f9ddb83b2929277ddfad671ad127bb
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2008-08-03 23:25:46 +0000

    Removed some unnecesary code that was moved to the main.py .

--- core/ui/gtkUi/proxywin.py
+++ core/ui/gtkUi/proxywin.py
@@ -59,16 +59,6 @@ class ProxiedRequests(entries.RememberingWindow):
         super(ProxiedRequests,self).__init__(w3af, "proxytool", "w3af - Proxy", onDestroy=self._close)
         self.set_icon_from_file('core/ui/gtkUi/data/w3af_icon.png')
         self.w3af = w3af
-        
-        # This is inited before all, to have a full logging facility.
-        coreOutputPlugins = self.w3af.getEnabledPlugins('output')
-        omOutputPlugins = om.out.getOutputPlugins()
-        tmp = []
-        tmp.extend(coreOutputPlugins)
-        tmp.extend(omOutputPlugins)
-        if 'gtkOutput' not in tmp:
-            tmp.append('gtkOutput')
-        om.out.setOutputPlugins( tmp )
 
         # toolbar elements
         uimanager = gtk.UIManager()
