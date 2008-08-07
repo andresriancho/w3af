7e319a0a69ceca46f1c2a2d340592a833d6c16b8
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2008-08-07 02:58:40 +0000

    Better regular expression for finding emails fixes bug [ 2037018 ] webSpider hangs when scanning files with encoded contents and makes email detection better.

--- core/data/parsers/abstractParser.py
+++ core/data/parsers/abstractParser.py
@@ -58,8 +58,8 @@ class abstractParser:
             documentString = re.sub( '[^\w@\\.]', ' ', documentString )
             
             # Now we have a clean documentString; and we can match the mail addresses!
-            emailRegex = '[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}'
-            for email in re.findall(emailRegex, documentString,  re.IGNORECASE):
+            emailRegex = '([A-Z0-9\._%-]{1,45}@([A-Z0-9\.-]{1,45}\.){1,10}[A-Z]{2,4})'
+            for email, domain in re.findall(emailRegex, documentString,  re.IGNORECASE):
                 if email not in self._emails:
                     self._emails.append( email )
                     
