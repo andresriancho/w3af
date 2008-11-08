60226b6baa3cee032a443e4a752d1f8c8f3c1d4c
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2008-11-08 22:10:34 +0000

    New ssn test pages. New test script. Re-tested plugin and added some performance enhancements.

--- plugins/grep/ssn.py
+++ plugins/grep/ssn.py
@@ -20,17 +20,20 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
 
-
 import core.controllers.outputManager as om
+
 # options
 from core.data.options.option import option
 from core.data.options.optionList import optionList
+
 from core.controllers.basePlugin.baseGrepPlugin import baseGrepPlugin
+
 import core.data.kb.knowledgeBase as kb
 import core.data.kb.vuln as vuln
-from core.data.getResponseType import *
-import re
 import core.data.constants.severity as severity
+
+import re
+
 
 class ssn(baseGrepPlugin):
     '''
@@ -41,43 +44,55 @@ class ssn(baseGrepPlugin):
 
     def __init__(self):
         baseGrepPlugin.__init__(self)
+        
         self._ssnResponses = []
+        
         # match numbers of the form: 'nnn-nn-nnnn', 'nnnnnnnnn', 'nnn nn nnnn'
         regex = '(?:^|[^\d])(\d{3})(?:[\- ]?)(\d{2})(?:[\- ]?)(\d{4})(?:[^\d]|$)'
         self._regex = re.compile(regex)
+        
         # re that removes tags
         self._re_removeTags = re.compile('(<.*?>|</.*?>)')
         
     def _testResponse(self, request, response):
+        '''
+        Plugin entry point, find the SSN numbers.
+        
+        @return: None.
+        '''
         if response.is_text_or_html() and response.getCode() == 200:
-            found_ssn = self._findSsn(response.getBody())
+            found_ssn = self._find_SSN(response.getBody())
             if found_ssn:
                 v = vuln.vuln()
                 v.setURL( response.getURL() )
                 v.setId( response.id )
                 v.setSeverity(severity.LOW)
                 v.setName( 'US Social Security Number disclosure' )
-                v.setDesc( 'The URL: "' + v.getURL() + '" possibly discloses a US Social Security Number: "'+ found_ssn +'"' )
+                msg = 'The URL: "' + v.getURL() + '" possibly discloses a US '
+                msg += 'Social Security Number: "'+ found_ssn +'"'
+                v.setDesc( msg )
                 kb.kb.append( self, 'ssn', v )
      
-    def _findSsn(self, body):
+    def _find_SSN(self, body):
+        '''
+        Remove the tags and apply the ssn regex.
+        @return: True if the 
+        '''
         # Now, remove html tags
-        bodyWithoutTags = self._re_removeTags.sub('', body)
-        validate_res = False
-        lines = str(bodyWithoutTags).split('\n')
-        for line in lines:
-            res = self._regex.search(str(line))
-            if (res != None):
-                validate_res = self._validSsn(res)
-                if validate_res:
-                    break
-        return validate_res
+        body_without_tags = self._re_removeTags.sub('', body)
+        
+        validated_ssn = None
+        for match in self._regex.finditer(body_without_tags):
+            validated_ssn = self._validate_SSN(match)
+            if validated_ssn:
+                break
+        return validated_ssn
     
-    def _validSsn(self, potential_ssn):
-       '''
-       This method is called to validate the digits of the 9-digit number
-       found, to confirm that it is a valid SSN. All the publicly available SSN
-       checks are performed. The number is an SSN if: 
+    def _validate_SSN(self, potential_ssn):
+        '''
+        This method is called to validate the digits of the 9-digit number
+        found, to confirm that it is a valid SSN. All the publicly available SSN
+        checks are performed. The number is an SSN if: 
         1. the first three digits <= 772
         2. the number does not have all zeros in any digit group 3+2+4 i.e. 000-xx-####,
         ###-00-#### or ###-xx-0000 are not allowed
@@ -86,29 +101,35 @@ class ssn(baseGrepPlugin):
         5. the number is not equal to 078-05-1120
 
         Source of information: wikipedia and socialsecurity.gov
-       '''
-       area_code        = int(potential_ssn.group(1))
-       group_number     = int(potential_ssn.group(2))
-       serial_number    = int(potential_ssn.group(3))
-       '''Checks'''
-       if ((area_code > 772) or (area_code == 0) or (area_code == 666)):
+        '''
+        area_code        = int(potential_ssn.group(1))
+        group_number     = int(potential_ssn.group(2))
+        serial_number    = int(potential_ssn.group(3))
+        
+        # Checks
+        if ((area_code > 772) or (area_code == 0) or (area_code == 666)):
             om.out.debug("area_code erred out: " + str(area_code) )
             return False
-       if (group_number == 0):
+        if (group_number == 0):
             om.out.debug("group_number erred out: "+ str( group_number) )
             return False
-       if (serial_number == 0):
+        if (serial_number == 0):
             om.out.debug("serial_number erred out: "+ str(serial_number))
             return False
-       if ((area_code == 987) and (group_number == 65) and ((4320 <= serial_number) or (serial_number <= 4329))):
-            om.out.debug("advt area code erred out: " + str(area_code) + ' ' +str(group_number) + ' ' +str(serial_number) )
+        if ((area_code == 987) and (group_number == 65) and \
+            ((4320 <= serial_number) or (serial_number <= 4329))):
+            msg = "advt area code erred out: " + str(area_code) + ' ' 
+            msg += str(group_number) + ' ' +str(serial_number)
+            om.out.debug( msg )
             return False
-       if ((area_code == 78) and (group_number == 5) and (serial_number == 1120)):
-            om.out.debug("invalid ssn erred out: "+ str(area_code)  + ' ' + str(group_number) + ' ' + str(serial_number) )
+        if ((area_code == 78) and (group_number == 5) and (serial_number == 1120)):
+            msg = "invalid ssn erred out: "+ str(area_code)  + ' ' 
+            msg += str(group_number) + ' ' + str(serial_number)
+            om.out.debug( msg )
             return False
        
-       # If none of above conditions, then we have a valid ssn in the document. And we return it
-       return str(area_code)+'-'+ str(group_number)+ '-' + str(serial_number)
+        # If none of above conditions, then we have a valid ssn in the document. And we return it
+        return str(area_code)+'-'+ str(group_number)+ '-' + str(serial_number)
 
 
     def end(self):
@@ -117,7 +138,6 @@ class ssn(baseGrepPlugin):
         '''
         # Print results
         self.printUniq( kb.kb.getData( 'ssn', 'ssn' ), 'URL' )
-
 
     def getOptions( self ):
         '''
@@ -137,6 +157,7 @@ class ssn(baseGrepPlugin):
         This plugins scans every response page to find the strings that are likely to be 
         the US social security numbers. 
         '''
+        
     def getPluginDeps( self ):
         '''
         @return: A list with the names of the plugins that should be run before the
