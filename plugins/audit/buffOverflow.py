55eaaa3a5d8fc32b40f492bb852a31c66bd739dc
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2008-11-15 13:52:50 +0000

    Added a false positive check.

--- plugins/audit/buffOverflow.py
+++ plugins/audit/buffOverflow.py
@@ -20,10 +20,11 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
 
-from core.data.fuzzer.fuzzer import createMutants, createRandAlpha
 import core.controllers.outputManager as om
+
 from core.controllers.basePlugin.baseAuditPlugin import baseAuditPlugin
 from core.controllers.w3afException import w3afException, w3afMustStopException
+from core.data.fuzzer.fuzzer import createMutants, createRandAlpha
 
 import core.data.kb.knowledgeBase as kb
 import core.data.kb.vuln as vuln
@@ -34,40 +35,41 @@ import core.data.constants.severity as severity
 from core.data.options.option import option
 from core.data.options.optionList import optionList
 
+
 class buffOverflow(baseAuditPlugin):
     '''
     Find buffer overflow vulnerabilities.
     @author: Andres Riancho ( andres.riancho@gmail.com )
     '''
 
-    '''
-    Some notes:
-        On apache, when an overflow happends on a cgic script, this is written to the log:
-            *** stack smashing detected ***: /var/www/w3af/bufferOverflow/buffOverflow.cgi terminated, referer: http://localhost/w3af/bufferOverflow/buffOverflow.cgi
-            Premature end of script headers: buffOverflow.cgi, referer: http://localhost/w3af/bufferOverflow/buffOverflow.cgi
-
-        On apache, when an overflow happends on a cgic script, this is returned to the user:
-            <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-            <html><head>
-            <title>500 Internal Server Error</title>
-            </head><body>
-            <h1>Internal Server Error</h1>
-            <p>The server encountered an internal error or
-            misconfiguration and was unable to complete
-            your request.</p>
-            <p>Please contact the server administrator,
-             webmaster@localhost and inform them of the time the error occurred,
-            and anything you might have done that may have
-            caused the error.</p>
-            <p>More information about this error may be available
-            in the server error log.</p>
-            <hr>
-            <address>Apache/2.0.55 (Ubuntu) mod_python/3.2.8 Python/2.4.4c1 PHP/5.1.6 Server at localhost Port 80</address>
-            </body></html>
-            
-        Note that this is  an apache error 500, not the more common PHP error 500.
-    '''
     def __init__(self):
+        '''
+        Some notes:
+            On apache, when an overflow happends on a cgic script, this is written to the log:
+                *** stack smashing detected ***: /var/www/w3af/bufferOverflow/buffOverflow.cgi terminated, referer: http://localhost/w3af/bufferOverflow/buffOverflow.cgi
+                Premature end of script headers: buffOverflow.cgi, referer: http://localhost/w3af/bufferOverflow/buffOverflow.cgi
+
+            On apache, when an overflow happends on a cgic script, this is returned to the user:
+                <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+                <html><head>
+                <title>500 Internal Server Error</title>
+                </head><body>
+                <h1>Internal Server Error</h1>
+                <p>The server encountered an internal error or
+                misconfiguration and was unable to complete
+                your request.</p>
+                <p>Please contact the server administrator,
+                 webmaster@localhost and inform them of the time the error occurred,
+                and anything you might have done that may have
+                caused the error.</p>
+                <p>More information about this error may be available
+                in the server error log.</p>
+                <hr>
+                <address>Apache/2.0.55 (Ubuntu) mod_python/3.2.8 Python/2.4.4c1 PHP/5.1.6 Server at localhost Port 80</address>
+                </body></html>
+                
+            Note that this is  an apache error 500, not the more common PHP error 500.
+        '''
         baseAuditPlugin.__init__(self)
         
     def _fuzzRequests(self, freq ):
@@ -78,8 +80,9 @@ class buffOverflow(baseAuditPlugin):
         '''
         om.out.debug( 'bufferOverflow plugin is testing: ' + freq.getURL() )
         
-        strList = self._getStringList()
-        mutants = createMutants( freq , strList )
+        str_list = self._get_string_list()
+        oResponse = self._sendMutant( freq , analyze=False ).getBody()
+        mutants = createMutants( freq , str_list, oResponse=oResponse )
             
         for mutant in mutants:
             targs = (mutant,)
@@ -87,7 +90,8 @@ class buffOverflow(baseAuditPlugin):
             
     def _sendMutant( self, mutant, analyze=True, grepResult=True ):
         '''
-        Sends a mutant to the remote web server. I override the _sendMutant of basePlugin just to handle errors in a different way.
+        Sends a mutant to the remote web server. I override the _sendMutant of basePlugin
+        just to handle errors in a different way.
         '''
         url = mutant.getURI()
         data = mutant.getData()
@@ -102,21 +106,27 @@ class buffOverflow(baseAuditPlugin):
         
         functor = getattr( self._urlOpener , method )
         try:
-            res = apply( functor, args, {'data': data, 'headers': headers, 'grepResult': grepResult } )
+            kwdargs = {'data': data, 'headers': headers, 'grepResult': grepResult }
+            res = apply( functor, args, kwdargs )
         except (w3afException,w3afMustStopException):
             i = info.info( mutant )
             i.setName( 'Possible buffer overflow vulnerability' )
             if data:
-                i.setDesc( 'A possible (most probably a false positive than a bug) buffer overflow was found when requesting: ' + url + ' . Using method: ' + method + '. The data sent was: ' + data )
+                msg = 'A possible (most probably a false positive than a bug) buffer overflow was'
+                msg += ' found when requesting: "' + url + '", using HTTP method ' + method
+                msg += '. The data sent was: "' + data + '".'
+                i.setDesc( msg )
             else:
-                i.setDesc( 'A possible (most probably a false positive than a bug) buffer overflow was found when requesting: ' + url + ' . Using method: ' + method )              
+                msg = 'A possible (most probably a false positive than a bug) buffer overflow was'
+                msg += ' found when requesting: "' + url + '", using HTTP method ' + method + '.'
+                i.setDesc( msg )
             kb.kb.append( self, 'buffOverflow', i )
         else:
             if analyze:
                 self._analyzeResult( mutant, res )
             return res
         
-    def _getStringList( self ):
+    def _get_string_list( self ):
         '''
         @return: This method returns a list of strings that could overflow a buffer.
         '''
@@ -125,27 +135,29 @@ class buffOverflow(baseAuditPlugin):
         ### TODO !! if lengths = [ 65 , 257 , 513 , 1025, 2049, 4097, 8000 ]
         ### then i get a badStatusLine exception from urllib2, is seems to be a internal error.
         ### tested against tomcat 5.5.7
-        for l in lengths:
-            strings.append( createRandAlpha( l ) )  
+        for i in lengths:
+            strings.append( createRandAlpha( i ) )  
         return strings
         
     def _analyzeResult( self, mutant, response ):
         '''
         Analyze results of the _sendMutant method.
         '''
-        for error in self._getErrors():
-            # Remember that httpResponse objects have a faster "__in__" than
-            # the one in strings; so string in response.getBody() is slower than
-            # string in response            
-            #
-            # hmmm... maybe I should check that the string wasn't in the original response?
+        for error in self._get_errors():
+            
+            # Check if the error string is in the response
             if error in response:
-                v = vuln.vuln( mutant )
-                v.setId( response.id )
-                v.setSeverity(severity.MEDIUM)
-                v.setName( 'Buffer overflow vulnerability' )
-                v.setDesc( 'A possible buffer overflow (detection is really hard...) was found at: ' + mutant.foundAt() )
-                kb.kb.append( self, 'buffOverflow', v )
+                # And not in the originally requested (non fuzzed) request
+                if not error not in mutant.getOriginalResponseBody():
+                    # vuln, vuln!
+                    v = vuln.vuln( mutant )
+                    v.setId( response.id )
+                    v.setSeverity(severity.MEDIUM)
+                    v.setName( 'Buffer overflow vulnerability' )
+                    msg = 'A possible buffer overflow (detection is really hard...) was found at: '
+                    msg += mutant.foundAt()
+                    v.setDesc( msg )
+                    kb.kb.append( self, 'buffOverflow', v )
     
     def end(self):
         '''
@@ -154,12 +166,16 @@ class buffOverflow(baseAuditPlugin):
         self._tm.join( self )
         self.printUniq( kb.kb.getData( 'buffOverflow', 'buffOverflow' ), 'VAR' )
     
-    def _getErrors( self ):
+    def _get_errors( self ):
         '''
         @return: A list of errors that are shown when a buffer overflow is triggered
         '''
         res = []
-        res.append('<html><head>\n<title>500 Internal Server Error</title>\n</head><body>\n<h1>Internal Server Error</h1>')
+        
+        error = '<html><head>\n<title>500 Internal Server Error</title>\n</head><body>\n<h1>'
+        error += 'Internal Server Error</h1>'
+        res.append( error )
+        
         res.append('*** stack smashing detected ***:')
         return res
     
@@ -192,7 +208,7 @@ class buffOverflow(baseAuditPlugin):
         @return: A DETAILED description of the plugin functions and features.
         '''
         return '''
-        This plugin will find buffer overflow vulnerabilities.
+        This plugin finds buffer overflow vulnerabilities.
         
         Users have to know that detecting a buffer overflow vulnerability will be only possible if the server is configured
         to return errors, and the application is developed in cgi-c or some other language that allows the programmer to
