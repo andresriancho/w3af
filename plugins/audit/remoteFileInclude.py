1fbc914b08eba8351c374f854c1f9f3a905552a3
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2008-11-18 01:46:20 +0000

    Complete rewrite of the plugin. Adding test scripts for both techniques.

--- plugins/audit/remoteFileInclude.py
+++ plugins/audit/remoteFileInclude.py
@@ -20,22 +20,29 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
 
-
-from core.data.fuzzer.fuzzer import createMutants, createRandAlNum
 import core.controllers.outputManager as om
+
 # options
 from core.data.options.option import option
 from core.data.options.optionList import optionList
 
 from core.controllers.basePlugin.baseAuditPlugin import baseAuditPlugin
-import core.data.kb.knowledgeBase as kb
+from core.data.fuzzer.fuzzer import createMutants, createRandAlNum
 import core.data.parsers.urlParser as urlParser
-from core.controllers.w3afException import w3afException
-import os, time
-from core.controllers.daemons.webserver import webserver
+
+import core.data.kb.knowledgeBase as kb
 import core.data.kb.vuln as vuln
-import core.data.constants.w3afPorts as w3afPorts
 import core.data.constants.severity as severity
+
+from core.controllers.w3afException import w3afException
+from core.controllers.daemons.webserver import webserver
+import core.data.constants.w3afPorts as w3afPorts
+
+import os, time
+
+CONFIG_ERROR_MSG = 'audit.remoteFileInclude plugin has to be correctly configured to use.'
+CONFIG_ERROR_MSG += ' Please set the local address and port, or use the official w3af site'
+CONFIG_ERROR_MSG += ' as the target server for remote inclusions.'
 
 class remoteFileInclude(baseAuditPlugin):
     '''
@@ -45,75 +52,87 @@ class remoteFileInclude(baseAuditPlugin):
 
     def __init__(self):
         baseAuditPlugin.__init__(self)
-        self._rfiUrl = ''
-        self._rfiResult = ''
-        self._run = True
-        self._listenPort = w3afPorts.REMOTEFILEINCLUDE
-        self._listenAddress = ''
-        self._wS = None
-        self._usew3afSite = True
+        
+        # Internal variables
+        self._error_reported = False
+        self._webserver = None
+        
+        # User configured parameters
+        self._rfi_url = ''
+        self._rfi_result = ''
+        self._listen_port = w3afPorts.REMOTEFILEINCLUDE
+        self._listen_address = ''
+        self._use_w3af_site = True
         
     def _fuzzRequests(self, freq ):
         '''
         Tests an URL for remote file inclusion vulnerabilities.
         
-        @param freq: A fuzzableRequest
+        @param freq: A fuzzableRequest object
         '''
-        if self._run:
-            om.out.debug( 'remoteFileInclude plugin is testing: ' + freq.getURL() )
-            vulnerable = []
-            if self._listenAddress == '':
-                self._run = False
-                raise w3afException('remoteFileInclude plugin has to be correctly configured to use.')
-            else:
-                # Everything is ok ! , go on with the rfi tests.
-                if not self._usew3afSite:
-                    om.out.information('w3af is running a webserver to include remote files on the server.')
-                    self._startServer()             
-                
-                self._check( freq )
-                
-                if not self._usew3afSite:
-                    self._stopServer()
-                # Wait for threads to finish
-                self._tm.join( self )
-                    
-    def _getRfiUrl( self ):
+        # Sanity check
+        if not self._correctly_configured():
+            # Report error to the user only once
+            self._error_reported = True
+            raise w3afException(CONFIG_ERROR_MSG)
+        
+        if not self._error_reported:
+            # The plugin is going to use two different techniques:
+            # 1- create a request that will include a file from the w3af official site
+            self._local_test_inclusion( freq )
+            
+            # 2- create a request that will include a file from a local web server        
+            if self._use_w3af_site:
+                self._w3af_site_test_inclusion( freq )
+    
+    def _correctly_configured(self):
         '''
-        With setOptions the user entered a URL that is the one to be included.
-        This method returns that URL.
-        
-        @return: A string, see above.
+        @return: True if the plugin is correctly configured to run.
         '''
-        return self._rfiUrl
-        
-    def _getRfiResult( self ):
+        if not self._use_w3af_site and self._listen_address == '':
+            return False
+        else:
+            return True
+    
+    def _local_test_inclusion(self, freq):
         '''
-        With setOptions the user entered the expected result of the inclusion.
-        For example, at RfiUrl the user entered :
-        http://foo/toBeIncluded.txt
-        With the following contents :
-        $a = 'mfsa09';
-        $b = 'a059mu2';
-        echo $a . $b;
+        Check for RFI using a local web server
         
-        The expected result for the remote file inclusion would be the following string:
-        mfsa09a059mu2
-        
-        This method returns that expected result.
-        
-        @return: A string, see above.
+        @param freq: A fuzzableRequest object
+        @return: None, everything is saved to the kb
         '''
-        return self._rfiResult
-
-    def _check( self, freq ):
+        om.out.debug( 'RFI test using local web server for URL: ' + freq.getURL() )
+        om.out.debug('w3af is running a webserver')
+        self._start_server()             
+        
+        # Perform the real work
+        self._test_inclusion( freq )
+            
+        self._stop_server()
+        
+        # Wait for threads to finish
+        self._tm.join( self )
+            
+    def _w3af_site_test_inclusion(self, freq):
+        '''
+        Check for RFI using the official w3af site.
+        
+        @param freq: A fuzzableRequest object
+        @return: None, everything is saved to the kb
+        '''        
+        self._rfi_url = 'http://w3af.sourceforge.net/w3af/remoteFileInclude.html'
+        self._rfi_result = 'w3af is goood!'
+        # Perform the real work
+        self._test_inclusion( freq )
+        
+    def _test_inclusion( self, freq ):
         '''
         Checks a fuzzableRequest for remote file inclusion bugs.
         
         @return: None
         '''
-        rfiUrlList = [ self._getRfiUrl() ]
-        mutants = createMutants( freq, rfiUrlList )
+        rfi_url_list = [ self._rfi_url,  ]
+        mutants = createMutants( freq, rfi_url_list )
         
         for mutant in mutants:
             if self._hasNoBug( 'remoteFileInclude','remoteFileInclude',mutant.getURL() , mutant.getVar() ):
@@ -126,10 +145,7 @@ class remoteFileInclude(baseAuditPlugin):
         '''
         Analyze results of the _sendMutant method.
         '''
-        # Remember that httpResponse objects have a faster "__in__" than
-        # the one in strings; so string in response.getBody() is slower than
-        # string in response        
-        if self._getRfiResult() in response:
+        if self._rfi_result in response:
             v = vuln.vuln( mutant )
             v.setId( response.id )
             v.setSeverity(severity.HIGH)
@@ -144,80 +160,64 @@ class remoteFileInclude(baseAuditPlugin):
         self._tm.join( self )
         self.printUniq( kb.kb.getData( 'remoteFileInclude', 'remoteFileInclude' ), 'VAR' )
 
-    def _startServer(self):
+    def _start_server(self):
         '''
-        Starts a webserver for including files. 
+        Starts a webserver for including files, and configure the parameters.
         '''
         # First, generate the php file to be included.
         rand1 = createRandAlNum( 9 )
         rand2 = createRandAlNum( 9 )
         filename = createRandAlNum()
-        phpStr = '<? \n echo "'
-        phpStr += rand1 + '";\n'
-        phpStr += ' echo "'
-        phpStr += rand2 + '";\n'
-        phpStr += ' ?>'
+        php_code = '<? \n echo "'
+        php_code += rand1 + '";\n'
+        php_code += ' echo "'
+        php_code += rand2 + '";\n'
+        php_code += ' ?>'
         
         # Write the php to the webroot
         f = open( os.path.join('webroot' + os.path.sep, filename ) , 'w')
-        f.write( phpStr )
+        f.write( php_code )
         f.close()
         
         # Define the required parameters
-        self._rfiUrl = 'http://' + self._listenAddress +':' + str(self._listenPort) +'/' + filename
-        self._rfiResult = rand1 + rand2
+        self._rfi_url = 'http://' + self._listen_address +':' + str(self._listen_port) +'/' + filename
+        self._rfi_result = rand1 + rand2
         
-        self._wS = webserver( self._listenAddress, self._listenPort , 'webroot' + os.path.sep)
-        self._wS.start2()
+        self._webserver = webserver( self._listen_address, self._listen_port , 'webroot' + os.path.sep)
+        self._webserver.start2()
         time.sleep( 0.2 )
         
-    def _stopServer( self ):
+    def _stop_server( self ):
 
-        if self._wS != None:
-            self._wS.stop()
+        if self._webserver != None:
+            self._webserver.stop()
             # Remove the file
-            filename = self._rfiUrl[self._rfiUrl.rfind(os.path.sep)+1:]
+            filename = self._rfi_url[self._rfi_url.rfind(os.path.sep)+1:]
             os.remove( os.path.join('webroot' + os.path.sep, filename ) )
-            self._wS = None
+            self._webserver = None
 
     def getOptions( self ):
         '''
         @return: A list of option objects for this plugin.
         '''
-        d1 = 'Remote URL to include when testing for remote file inclusion.'
-        o1 = option('rfiUrl', self._rfiUrl, d1, 'string')
-        
-        d2 = 'Expected result of inclusion'
-        h2 = 'For example, at RfiUrl the user entered :\
-          http://foo/toBeIncluded.txt\
-          With the following content :\
-          \
-          $a = \'mfsa09\';\
-          $b = \'a059mu2\';\
-          echo $a . $b;\
-          \
-         The rfiResult for the remote file inclusion would be the following string:\
-         mfsa09a059mu2'
-        o2 = option('rfiResult', self._rfiResult, d2, 'string', help=h2)
-        
-        d3 = 'IP address that the webserver will use to receive requests'
-        h3 = 'When the two previous settings are left unconfigured, w3af runs a webserver\
-          to serve the files to the target web app when doing remote file inclusions. This \
-          setting configures where the webserver is going to listen.'
-        o3 = option('listenAddress', self._listenAddress, d3, 'string', help=h3)
+        d1 = 'IP address that the webserver will use to receive requests'
+        h1 = 'w3af runs a webserver to serve the files to the target web application \
+        when doing remote file inclusions. This setting configures where the webserver\
+        is going to listen for requests.'
+        o1 = option('listenAddress', self._listen_address, d1, 'string', help=h1)
 
-        d4 = 'Port that the webserver will use to receive requests'
-        o4 = option('listenPort', self._listenPort, d4, 'integer')
+        d2 = 'TCP port that the webserver will use to receive requests'
+        o2 = option('listenPort', self._listen_port, d2, 'integer')
 
-        d5 = 'Use w3af site to test remote file inclusion. If set to True this will override all other options.'
-        o5 = option('usew3afSite', self._usew3afSite, d5, 'boolean')
+        d3 = 'Use w3af site to test for remote file inclusion'
+        h3 =  'The plugin can use the w3af site to test for remote file inclusions, which is\
+        convinient when you are performing a test behind a NAT firewall.'
+        o3 = option('usew3afSite', self._use_w3af_site, d3, 'boolean',  help=h3)
         
         ol = optionList()
         ol.add(o1)
         ol.add(o2)
         ol.add(o3)
-        ol.add(o4)
-        ol.add(o5)
         return ol
         
     def setOptions( self, optionsMap ):
@@ -228,18 +228,12 @@ class remoteFileInclude(baseAuditPlugin):
         @parameter optionsMap: A dictionary with the options for the plugin.
         @return: No value is returned.
         ''' 
-        self._listenAddress = optionsMap['listenAddress'].getValue()
-        self._listenPort = optionsMap['listenPort'].getValue()
-        
-        if optionsMap['usew3afSite'].getValue():
-            self._usew3afSite = True
-            self._rfiUrl = 'http://w3af.sourceforge.net/w3af/remoteFileInclude.html'
-            self._rfiResult = 'w3af is goood!'
-            self._listenPort = 'notUsed'
-            self._listenAddress = 'notUsed'
+        self._listen_address = optionsMap['listenAddress'].getValue()
+        self._listen_port = optionsMap['listenPort'].getValue()
+        self._use_w3af_site = optionsMap['usew3afSite'].getValue()
                 
-        if self._listenAddress == '':
-            raise w3afException('remoteFileInclude plugin has to be correctly configured to use.')
+        if self._listen_address == '' and not self._use_w3af_site:
+            om.out.error(CONFIG_ERROR_MSG)
 
     def getPluginDeps( self ):
         '''
@@ -253,20 +247,16 @@ class remoteFileInclude(baseAuditPlugin):
         @return: A DETAILED description of the plugin functions and features.
         '''
         return '''
-        This plugin will find remote file inclusion vulnerabilities.
+        This plugin finds remote file inclusion vulnerabilities.
         
-        Five configurable parameters exist:
-            - rfiUrl
-            - rfiResult
+        Three configurable parameters exist:
             - listenAddress
             - listenPort
             - usew3afSite
         
-        This plugin will send the value of "rfiUrl" to each injctable parameter, and search for "rfiResult" in the response.
-        There are two ways of running this plugin, one is the most common one, by using the w3af site ( w3af.sf.net ) as
-        the place from where the web application will fetch the remote file. The other way to test for inclusion is to run a 
-        webserver on the machine that is sending the tests, this is configured using the "listenAddress" and "listenPort" 
-        parameters.
-        
-        Configuring True in "usew3afSite" will automatically configure all the other variables.
+        There are two ways of running this plugin, one is the most common one, by using the w3af
+        site ( w3af.sf.net ) as the place from where the target web application will fetch the
+        remote file. The other way to test for inclusion is to run a webserver on the local machine
+        that is performing the scan. The second option is configured using the "listenAddress" and
+        "listenPort" parameters.
         '''
