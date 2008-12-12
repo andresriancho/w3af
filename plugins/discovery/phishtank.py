ae0e0a98df6d7483ee493e65676cf6a67a7c6324
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2008-12-12 00:41:44 +0000

    Pretty code, new way of saving data.

--- plugins/discovery/phishtank.py
+++ plugins/discovery/phishtank.py
@@ -21,23 +21,24 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
 import core.controllers.outputManager as om
+
 # options
 from core.data.options.option import option
 from core.data.options.optionList import optionList
 
 from core.controllers.basePlugin.baseDiscoveryPlugin import baseDiscoveryPlugin
-from core.controllers.w3afException import w3afRunOnce
+from core.controllers.w3afException import w3afRunOnce, w3afException
+
 import core.data.kb.knowledgeBase as kb
 import core.data.kb.vuln as vuln
-from core.data.parsers.urlParser import *
-import urllib
-import socket
+import core.data.constants.severity as severity
+import core.data.parsers.urlParser as urlParser
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-
 import os.path
-import core.data.constants.severity as severity
+import socket
+
 
 class phishtank(baseDiscoveryPlugin):
     '''
@@ -49,13 +50,19 @@ class phishtank(baseDiscoveryPlugin):
     def __init__(self):
         baseDiscoveryPlugin.__init__(self)
         self._run = True
+
+        # Internal variables
+        self._fuzzable_requests = []
         
         # User defines variable
-        self._phishtankDB = 'plugins' + os.path.sep + 'discovery' + os.path.sep + 'phishtank' + os.path.sep + 'index.xml'
-        self._updateDB = False
+        self._phishtank_DB = 'plugins' + os.path.sep + 'discovery'
+        self._phishtank_DB += os.path.sep + 'phishtank' + os.path.sep + 'index.xml'
+        self._update_DB = False
         
     def discover(self, fuzzableRequest ):
-        self._fuzzableRequests = []
+        '''
+        Plugin entry point, perform all the work.
+        '''
         
         if not self._run:
             # This will remove the plugin from the discovery plugins to be runned.
@@ -64,31 +71,36 @@ class phishtank(baseDiscoveryPlugin):
             # Run one time
             self._run = False
             
-            if self._updateDB:
-                self._doUpdate()
+            if self._update_DB:
+                self._do_update()
             
-            domain = getDomain( fuzzableRequest.getURL() )
-            toCheckList = self._getToCheck( domain )
+            domain = urlParser.getDomain( fuzzableRequest.getURL() )
+            to_check_list = self._get_to_check( domain )
             
-            phishTankMatches = self._isInPhishtank( toCheckList )
-            for ptm in phishTankMatches:
+            # I found some URLs, create fuzzable requests
+            phishtank_matches = self._is_in_phishtank( to_check_list )
+            for ptm in phishtank_matches:
                 response = self._urlOpener.GET( ptm.url )
-                self._fuzzableRequests.extend( self._createFuzzableRequests( response ) )
-                
+                self._fuzzable_requests.extend( self._createFuzzableRequests( response ) )
+            
+            # Only create the vuln object once
+            if phishtank_matches:
                 v = vuln.vuln()
                 v.setURL( ptm.url )
                 v.setId( response.id )
                 v.setName( 'Phishing scam' )
                 v.setSeverity(severity.LOW)
-                v.setDesc( ptm.url + ' seems to be involved in a phishing scam. Please see ' + ptm.moreInfoURL + ' for more info.' )
+                desc = 'The URL: "' + ptm.url + '" seems to be involved in a phishing scam. Please see "'
+                desc += ptm.more_info_URL + '" for more info.'
+                v.setDesc( desc )
                 kb.kb.append( self, 'phishtank', v )
                 om.out.vulnerability( v.getDesc(), severity=v.getSeverity() )
                 
-        return self._fuzzableRequests
+        return self._fuzzable_requests
         
-    def _getToCheck( self, domain ):
+    def _get_to_check( self, domain ):
         '''
-        @return: From the domain, get a list of fqdn, rootDomain and IP address.
+        @return: From the domain, get a list of FQDN, rootDomain and IP address.
         '''
         res = []
         
@@ -110,7 +122,7 @@ class phishtank(baseDiscoveryPlugin):
             
         rootDomain = ''
         try:
-            rootDomain = getRootDomain( domain )
+            rootDomain = urlParser.getRootDomain( domain )
         except Exception, e:
             om.out.debug( str(e) )
         else:
@@ -120,15 +132,19 @@ class phishtank(baseDiscoveryPlugin):
         return res
             
         
-    def _isInPhishtank( self, toCheckList ):
+    def _is_in_phishtank( self, to_check_list ):
         '''
-        Reads the phishtank db and tries to match the entries on that db with the toCheckList
+        Reads the phishtank db and tries to match the entries on that db with the to_check_list
         @return: A list with the sites to match against the phishtank db
         '''
         class phishTankMatch:
-            def __init__( self, url, moreInfoURL ):
+            '''
+            Represents a phish tank match between the site I'm scanning and
+            something in the index.xml file.
+            '''
+            def __init__( self, url, more_info_URL ):
                 self.url = url
-                self.moreInfoURL = moreInfoURL
+                self.more_info_URL = more_info_URL
         
         class phishtankHandler(ContentHandler):
             '''
@@ -148,59 +164,59 @@ class phishtank(baseDiscoveryPlugin):
                     </status>
                 </entry>
             '''
-            def __init__ (self, toCheckList):
-                self._toCheckList= toCheckList;
-                self.insideEntry = False
-                self.insideURL = False
-                self.insideDetail = False
+            def __init__ (self, to_check_list):
+                self._to_check_list = to_check_list
+                self.inside_entry = False
+                self.inside_URL = False
+                self.inside_detail = False
                 self.matches = []
         
             def startElement(self, name, attrs):
                 if name == 'entry':
-                        self.insideEntry = True
+                    self.inside_entry = True
                 elif name == 'url':
-                        self.insideURL= True;
-                        self.url = "";
+                    self.inside_URL = True
+                    self.url = ""
                 elif name == 'phish_detail_url':
-                        self.insideDetail = True;
-                        self.phish_detail_url = "";
+                    self.inside_detail = True
+                    self.phish_detail_url = ""
                 return
             
             def characters (self, ch):
-                if self.insideURL:
-                        self.url += ch
-                if self.insideDetail:
-                        self.phish_detail_url += ch
+                if self.inside_URL:
+                    self.url += ch
+                if self.inside_detail:
+                    self.phish_detail_url += ch
             
             def endElement(self, name):
                 if name == 'phish_detail_url':
-                        self.insideDetail = False
+                    self.inside_detail = False
                 if name == 'url':
-                        self.insideURL = False
+                    self.inside_URL = False
                 if name == 'entry':
-                        self.insideEntry = False
-                        #
-                        #   Now I try to match the entry with an element in the toCheckList
-                        #
-                        phishDomain = getDomain( self.url )
-                        for url in self._toCheckList:
-                            if url == phishDomain or phishDomain.endswith('.' + url ):
-                                ptm = phishTankMatch( self.url, self.phish_detail_url )
-                                self.matches.append( ptm )
+                    self.inside_entry = False
+                    #
+                    #   Now I try to match the entry with an element in the to_check_list
+                    #
+                    phish_domain = urlParser.getDomain( self.url )
+                    for url in self._to_check_list:
+                        if url == phish_domain or phish_domain.endswith('.' + url ):
+                            ptm = phishTankMatch( self.url, self.phish_detail_url )
+                            self.matches.append( ptm )
         
-        fd = None
+        file_handler = None
         try:
-            fd = file( self._phishtankDB )
+            file_handler = file( self._phishtank_DB )
         except Exception, e:
-            raise w3afException('Failed to open phishtank database file: ' + self._phishtankDB )
+            raise w3afException('Failed to open phishtank database file: ' + self._phishtank_DB )
         
         parser = make_parser()   
-        curHandler = phishtankHandler( toCheckList )
+        curHandler = phishtankHandler( to_check_list )
         parser.setContentHandler( curHandler )
         om.out.debug( 'Starting the phishtank xml parsing. ' )
         
         try:
-            parser.parse( fd )
+            parser.parse( file_handler )
         except Exception, e:
             om.out.debug( 'XML parsing error: ' + str(e) )
             raise w3afException('phishtank database file is not a valid XML file.' )
@@ -211,44 +227,47 @@ class phishtank(baseDiscoveryPlugin):
 
         
     def setOptions( self, OptionList ):
-        self._phishtankDB = OptionList['dbFile'].getValue()
-        self._updateDB = OptionList['updateDB'].getValue()
+        self._phishtank_DB = OptionList['dbFile'].getValue()
+        self._update_DB = OptionList['updateDB'].getValue()
     
     def getOptions( self ):
         '''
         @return: A list of option objects for this plugin.
         '''    
         d1 = 'The path to the phishtank database file.'
-        o1 = option('dbFile', self._phishtankDB, d1, 'string')
+        o1 = option('dbFile', self._phishtank_DB, d1, 'string')
         
         d2 = 'Update the local phishtank database.'
-        h2 = 'If True, the plugin will download the phishtank database from http://www.phishtank.com/ .'
-        o2 = option('updateDB', self._updateDB, d2, 'boolean', help=h2)
+        h2 = 'If True, the plugin will download the phishtank database'
+        h2 += ' from http://www.phishtank.com/ .'
+        o2 = option('updateDB', self._update_DB, d2, 'boolean', help=h2)
         
         ol = optionList()
         ol.add(o1)
         ol.add(o2)
         return ol
 
-    def _doUpdate(self):
+    def _do_update(self):
         '''
         This method is called to update the database.
         '''
-        fd = None
+        file_handler = None
         try:
-            fd = open( self._phishtankDB, 'w' )
+            file_handler = open( self._phishtank_DB, 'w' )
         except:
-            raise w3afException('Failed to open file: ' + self._phishtankDB )
-            
-        om.out.information('Updating the phishtank database, this will take some minutes ( almost 7MB to download ).')
+            raise w3afException('Failed to open file: ' + self._phishtank_DB )
+        
+        msg = 'Updating the phishtank database, this will take some minutes'
+        msg += ' ( almost 7MB to download ).'
+        om.out.information( msg )
         res = self._urlOpener.GET('http://data.phishtank.com/data/online-valid/')
         om.out.information('Download complete, writing to the database file.')
         
         try:
-            fd.write( res.getBody() )
-            fd.close()
+            file_handler.write( res.getBody() )
+            file_handler.close()
         except:
-            raise w3afException('Failed to write to file: ' + self._phishtankDB )
+            raise w3afException('Failed to write to file: ' + self._phishtank_DB )
         else:
             return True
         
