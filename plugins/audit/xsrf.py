e157a370103131d090b2c441a1ade37efbc8590f
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2008-12-18 12:13:20 +0000

    Pretty code.

--- plugins/audit/xsrf.py
+++ plugins/audit/xsrf.py
@@ -20,18 +20,21 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
 
-import core.data.kb.vuln as vuln
 import core.controllers.outputManager as om
+
 # options
 from core.data.options.option import option
 from core.data.options.optionList import optionList
 
 from core.controllers.basePlugin.baseAuditPlugin import baseAuditPlugin
-import core.data.kb.knowledgeBase as kb
 from core.controllers.w3afException import w3afException
 from core.data.exchangableMethods import isExchangable
 from core.data.parsers.urlParser import hasQueryString
+
+import core.data.kb.knowledgeBase as kb
+import core.data.kb.vuln as vuln
 import core.data.constants.severity as severity
+
 
 class xsrf(baseAuditPlugin):
     '''
@@ -50,9 +53,11 @@ class xsrf(baseAuditPlugin):
 
     def __init__(self):
         baseAuditPlugin.__init__(self)
-        self._vulnSimple = []
-        self._vulnComplex = []
-        self._alreadyReported = False
+        
+        # Internal variables
+        self._vuln_simple = []
+        self._vuln_complex = []
+        self._already_reported = False
 
     def audit(self, freq ):
         '''
@@ -72,7 +77,7 @@ class xsrf(baseAuditPlugin):
             v.setMethod( freq.getMethod() )
             v.setDesc( 'The URL: ' + freq.getURL() + ' is vulnerable to cross site request forgery.' )
             v.setId( 0 )
-            self._vulnSimple.append( v )
+            self._vuln_simple.append( v )
         
         elif freq.getMethod() =='POST' and len ( freq.getDc() ) and isExchangable( self, freq ):
             # This is a POST request that can be sent using a GET and querystring
@@ -82,10 +87,13 @@ class xsrf(baseAuditPlugin):
             v.setSeverity(severity.LOW)
             v.setDc( freq.getDc() )
             v.setName( 'Cross site request forgery vulnerability' )
-            v.setMethod( freq.getMethod() )         
-            v.setDesc( 'The URL: ' + freq.getURL() + ' is vulnerable to cross site request forgery. It allows the attacker to exchange the method from POST to GET when sending data to the server.' )
+            v.setMethod( freq.getMethod() )
+            msg = 'The URL: ' + freq.getURL() + ' is vulnerable to cross site request forgery.'
+            msg += ' It allows the attacker to exchange the method from POST to GET when sending'
+            msg += ' data to the server.'
+            v.setDesc( msg )
             v.setId( 0 )
-            self._vulnComplex.append( v )
+            self._vuln_complex.append( v )
     
     def end( self ):
         '''
@@ -95,26 +103,29 @@ class xsrf(baseAuditPlugin):
         cookies = kb.kb.getData( 'collectCookies', 'cookies' )
         for cookie in cookies:
             if cookie.has_key('persistent'):
-                if not self._alreadyReported:
+                if not self._already_reported:
                     om.out.information('The web application sent a persistent cookie.')
                     hasPersistentCookie = True
-                    self._alreadyReported = True
+                    self._already_reported = True
         
         # If there is at least one persistent cookie
         if hasPersistentCookie:
-            if len( self._vulnSimple ):
-                om.out.vulnerability('The following scripts are vulnerable to a trivial form of XSRF:', severity=severity.LOW)
+            if len( self._vuln_simple ):
+                om.out.vulnerability('The following scripts are vulnerable to a trivial form of XSRF:',
+                        severity=severity.LOW)
                 
-                frStr = list(set([ str(v.getURL()) for v in self._vulnSimple ]))
-                kb.kb.save( self, 'get_xsrf', self._vulnSimple )
+                frStr = list(set([ str(v.getURL()) for v in self._vuln_simple ]))
+                kb.kb.save( self, 'get_xsrf', self._vuln_simple )
                 
                 for i in frStr:
                     om.out.vulnerability( '- ' + i, severity=severity.LOW )
             
-            if len( self._vulnComplex ):
-                om.out.vulnerability('The following scripts allow an attacker to send POST data as query string data (this makes XSRF more easy to exploit):', severity=severity.LOW)            
-                frStr = list(set([ str(fr) for fr in self._vulnComplex ]))
-                kb.kb.save( self, 'post_xsrf', self._vulnComplex )
+            if len( self._vuln_complex ):
+                msg = 'The following scripts allow an attacker to send POST data as query string'
+                msg +=' data (this makes XSRF more easy to exploit):'
+                om.out.vulnerability(msg, severity=severity.LOW)
+                frStr = list(set([ str(fr) for fr in self._vuln_complex ]))
+                kb.kb.save( self, 'post_xsrf', self._vuln_complex )
                 
                 for i in frStr:
                     om.out.vulnerability( '- ' + i, severity=severity.LOW )
@@ -148,7 +159,8 @@ class xsrf(baseAuditPlugin):
         @return: A DETAILED description of the plugin functions and features.
         '''
         return '''
-        This plugin will find Cross Site Request Forgeries (XSRF) vulnerabilities on the web application.
+        This plugin finds Cross Site Request Forgeries (XSRF) vulnerabilities.
+        
         The simplest type of XSRF is checked, to be vulnerable, the web application must have sent a permanent
         cookie, and the aplicacion must have query string parameters.
         '''
