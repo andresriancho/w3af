e2827dda2f318b1b222c792605fef5becaa2ecb6
ulises2k <ulises2k@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2009-01-30 19:36:34 +0000

    remove.

--- extlib/fpconst-0.7.2/build/lib/readline.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: UTF-8 -*-
-#this file is needed in site-packages to emulate readline
-#necessary for rlcompleter since it relies on the existance
-#of a readline module
-from pyreadline import *
