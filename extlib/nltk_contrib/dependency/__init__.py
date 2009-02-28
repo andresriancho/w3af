036a3b70b668c018daa616a4d904d360f2b38dc3
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2009-02-28 22:49:22 +0000

    Adding some more packages needed by nltk.

---
+++ extlib/nltk_contrib/dependency/__init__.py
@@ -0,0 +1,7 @@
+# Natural Language Toolkit: Dependency Trees
+#
+# Author: Ewan Klein <ewan@inf.ed.ac.uk>
+# URL: <http://www.nltk.org/>
+# For license information, see LICENSE.TXT
+
+from deptree import *
