9df01149949ac8a8011393813b42daa45d0013e9
kdenver <kdenver@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2009-03-09 12:42:40 +0000

    Fixing a bug where the xml report was written twice to the output file.

--- plugins/output/xmlFile.py
+++ plugins/output/xmlFile.py
@@ -68,7 +68,6 @@ class xmlFile(baseOutputPlugin):
         self._topElement.setAttribute("start", self._timestampString)
         self._topElement.setAttribute("startstr", self._longTimestampString)
         self._topElement.setAttribute("xmloutputversion", "1.00")
-        
         self._scanInfo = self._xmldoc.createElement("scaninfo")
                                               
     def _init( self ):
@@ -197,38 +196,37 @@ class xmlFile(baseOutputPlugin):
         '''
         This method is called when the scan has finished.
         '''
-        
         if not self._initialized:
-            self._init()
+          self._init()
+          
+          # Add the vulnerability results
+          vulns = kb.kb.getAllVulns()
+          for i in vulns:
+            messageNode = self._xmldoc.createElement("vulnerability")
+            messageNode.setAttribute("severity", str(i.getSeverity()))
+            messageNode.setAttribute("method", str(i.getMethod()))
+            messageNode.setAttribute("url", str(i.getURL()))
+            messageNode.setAttribute("var", str(i.getVar()))
+            description = self._xmldoc.createTextNode(i.getDesc())
+            messageNode.appendChild(description)
+            self._topElement.appendChild(messageNode)
         
-        # Add the vulnerability results
-        vulns = kb.kb.getAllVulns()
-        for i in vulns:
-          messageNode = self._xmldoc.createElement("vulnerability")
-          messageNode.setAttribute("severity", str(i.getSeverity()))
-          messageNode.setAttribute("method", str(i.getMethod()))
-          messageNode.setAttribute("url", str(i.getURL()))
-          messageNode.setAttribute("var", str(i.getVar()))
-          description = self._xmldoc.createTextNode(i.getDesc())
-          messageNode.appendChild(description)
-          self._topElement.appendChild(messageNode)
+          # Add the information results
+          infos = kb.kb.getAllInfos()
+          for i in infos:
+            messageNode = self._xmldoc.createElement("information")
+            messageNode.setAttribute("url", str(i.getURL()))
+            description = self._xmldoc.createTextNode(i.getDesc())
+            messageNode.appendChild(description)
+            self._topElement.appendChild(messageNode)
         
-        # Add the information results
-        infos = kb.kb.getAllInfos()
-        for i in infos:
-          messageNode = self._xmldoc.createElement("information")
-          messageNode.setAttribute("url", str(i.getURL()))
-          description = self._xmldoc.createTextNode(i.getDesc())
-          messageNode.appendChild(description)
-          self._topElement.appendChild(messageNode)
-        
-        # Add additional information results
-        for node in self._errorXML:
+          # Add additional information results
+          for node in self._errorXML:
             self._topElement.appendChild(node)
         
-        # Write xml report
-        self._xmldoc.appendChild(self._topElement)
-        self._xmldoc.writexml(self._file, "", "", "\n", "UTF-8")
+          # Write xml report
+          self._xmldoc.appendChild(self._topElement)
+          self._xmldoc.writexml(self._file, "", "", "\n", "UTF-8")
               
     def getLongDesc( self ):
         '''
