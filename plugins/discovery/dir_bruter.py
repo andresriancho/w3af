156b6abc30d479c1a34bbd574b5f494226dbd726
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2009-07-12 18:00:09 +0000

    Commiting some interesting changes which make the dir_bruter a lot faster.

--- plugins/discovery/dir_bruter.py
+++ plugins/discovery/dir_bruter.py
@@ -32,6 +32,8 @@ import core.data.parsers.urlParser as urlParser
 import os
 
 import core.data.kb.knowledgeBase as kb
+from core.controllers.misc.levenshtein import relative_distance
+from core.data.fuzzer.fuzzer import createRandAlNum
 
 
 class dir_bruter(baseDiscoveryPlugin):
@@ -48,9 +50,16 @@ class dir_bruter(baseDiscoveryPlugin):
         # User configured parameters
         self._dir_list = 'plugins' + os.path.sep + 'discovery' + os.path.sep + 'dir_bruter'
         self._dir_list += os.path.sep + 'common_dirs_small.db'
+        self._be_recursive = True
 
         # Internal variables
         self._fuzzable_requests = []
+        self.is_404 = None
+        self._200_as_404 = False
+        self._404_as_404 = False
+        self._failed_to_fingerprint = False
+        self._404_content = None
+        self._tested_base_url = False
 
     def discover(self, fuzzableRequest ):
         '''
@@ -61,36 +70,146 @@ class dir_bruter(baseDiscoveryPlugin):
         if not self._exec:
             raise w3afRunOnce()
         else:
-            # Only run once
-            self._exec = False
-            is_404 = kb.kb.getData( 'error404page', '404' )
+            
+            if not self._be_recursive:
+                # Only run once
+                self._exec = False
+                
+            if self.is_404 == None:
+                self.is_404 = kb.kb.getData( 'error404page', '404' )
 
             self._fuzzable_requests = []
-
+            
+            domain_path = urlParser.getDomainPath( fuzzableRequest.getURL() )
             base_url = urlParser.baseUrl( fuzzableRequest.getURL() )
             
-            for directory_name in file(self._dir_list):
-                dir_url = urlParser.urlJoin(  base_url , directory_name.strip() )  
+            to_test = []
+            if not self._tested_base_url:
+                to_test.append( base_url )
+                self._tested_base_url = True
+                
+            if domain_path != base_url:
+                to_test.append( domain_path )
+            
+            for base_path in to_test:
+                # Perform some requests to see how the server responds to "404"
+                self._fingerprint_404( base_path )
+                self._bruteforce_directories( base_path )
+
+        return self._fuzzable_requests
+    
+    def _bruteforce_directories(self, base_path):
+        '''
+        @parameter base_path: The base path to use in the bruteforcing process, can be something
+        like http://host.tld/ or http://host.tld/images/ .
+        '''
+        for directory_name in file(self._dir_list):
+            directory_name = directory_name.strip()
+            
+            # ignore comments and empty lines
+            if directory_name and not directory_name.startswith('#'):
+                dir_url = urlParser.urlJoin(  base_path , directory_name)
                 dir_url +=  '/'
-                #om.out.debug( '[dir_bruter] Trying ' + dir_url )
-                http_response = self._urlOpener.GET( dir_url, useCache=True )
-                    
-                if not is_404( http_response ):
+
+                http_response = self._urlOpener.GET( dir_url, useCache=False )
+                
+                if not self._internal_is_404( http_response ):
                     fuzzable_reqs = self._createFuzzableRequests( http_response )
                     self._fuzzable_requests.extend( fuzzable_reqs )
-        
-        return self._fuzzable_requests
+                    
+                    msg = 'Directory bruteforcer plugin found directory "'
+                    msg += http_response.getURL()  + '"'
+                    
+                    if self._404_as_404:
+                        msg += ' with HTTP response code ' + str(http_response.getCode())
+                        msg += ' and Content-Length: ' + str(len(http_response.getBody()))
+                    
+                    if self._200_as_404:
+                        msg +=  ' with Content-Length: ' + str(len(http_response.getBody()))
+                        
+                    msg += '.'
+                    
+                    om.out.information( msg )
 
+    def _fingerprint_404(self, base_path):
+        '''
+        Perform some requests to see what kind of 404 the server has.
+        
+        @return: None
+        '''
+        #
+        # Generate a list of 404's
+        #
+        response_list = []
+        for i in xrange(5):
+            rnd_str = createRandAlNum( i + 6)
+            
+            dir_url = urlParser.urlJoin( base_path, rnd_str + '/')
+            http_response = self._urlOpener.GET( dir_url, useCache=False )
+            response_list.append(http_response)
+            
+        #
+        # Analyze it
+        #
+        count_200 = 0
+        count_404 = 0
+        for response in response_list:
+            # TODO: create a dict here, and work with that, this simplistic approach fails when errors 403, 401, 500 are returned!!
+            if response.getCode() == 200:
+                count_200 += 1
+            if response.getCode() == 404:
+                count_404 += 1
+        
+        if len(response_list) == count_200:
+            # Server uses 200 as 404
+            self._200_as_404 = True
+            self._404_as_404 = False
+            self._404_content = response_list[0].getBody()
+            
+        elif len(response_list) == count_404:
+            # Server uses 200 as 404
+            self._404_as_404 = True
+            self._200_as_404 = False
+            
+        else:
+            self._failed_to_fingerprint = True
+        
+    def _internal_is_404(self, http_response):
+        '''
+        @parameter http_response: The http_response that we want to know if it's a 404 or not
+        @return: True if it's a 404
+        '''
+        if self._404_as_404:
+            if http_response.getCode() == 404:
+                return True
+            else:
+                return False
+                
+        elif self._200_as_404:
+            # relative_distance() == 1 , they are equal
+            # relative_distance() == 0 , they are completely different
+            if relative_distance( self._404_content, http_response.getBody() ) < 0.75:
+                return False
+            else:
+                return True
+                
+        elif self._failed_to_fingerprint:
+            return self.is_404( http_response )
+            
     def getOptions( self ):
         '''
         @return: A list of option objects for this plugin.
         '''    
-        
         d1 = 'Wordlist to use in directory bruteforcing process.'
         o1 = option('wordlist', self._dir_list , d1, 'string')
+        
+        d2 = 'If set to True, this plugin will bruteforce all directories, not only the root'
+        d2 += ' directory.'
+        o2 = option('be_recursive', self._be_recursive , d2, 'boolean')
 
         ol = optionList()
         ol.add(o1)
+        ol.add(o2)
 
         return ol
         
@@ -106,6 +225,8 @@ class dir_bruter(baseDiscoveryPlugin):
         dir_list = OptionList['wordlist'].getValue()
         if os.path.exists( dir_list ):
             self._dir_list = dir_list
+            
+        self._be_recursive = OptionList['be_recursive'].getValue()
 
     def getPluginDeps( self ):
         '''
@@ -121,6 +242,8 @@ class dir_bruter(baseDiscoveryPlugin):
         return '''
         This plugin finds directories on a web server by bruteforcing the names using a list.
 
-        One configurable parameter exists:
+        Two configurable parameters exist:
             - wordlist: The wordlist to be used in the directory bruteforce process.
+            - be_recursive: If set to True, this plugin will bruteforce all directories, not only
+            the root directory.
         '''
