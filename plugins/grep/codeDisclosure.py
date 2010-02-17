e8ef2e2e3a53328b8e86eb287c46f4257312de4b
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2010-02-17 19:32:39 +0000

    Moving an interesting function to the core.

--- plugins/grep/codeDisclosure.py
+++ plugins/grep/codeDisclosure.py
@@ -33,6 +33,7 @@ import core.data.kb.vuln as vuln
 import core.data.constants.severity as severity
 
 from core.controllers.coreHelpers.fingerprint_404 import is_404
+from core.controllers.misc.is_source_file import is_source_file
 from core.data.db.temp_persist import disk_list
 
 import re
@@ -47,48 +48,15 @@ class codeDisclosure(baseGrepPlugin):
 
     def __init__(self):
         baseGrepPlugin.__init__(self)
-        # This regex means: "find all tags that are of the form <? something ?> 
-        # but if that something is "xml .*" ignore it completely. This is to 
-        # remove the false positive in the detection of code disclosure
-        # that is added when the web application uses something like
-        # <?xml version="1.0" encoding="UTF-8"?>
-        # This was added to fix bug #1989056
-        php = re.compile( '<\?(?! *xml).*\?>', re.IGNORECASE | re.DOTALL)
-
-        # The rest of the regex are ok, because this patterns aren't used in html / xhtml
-        asp = re.compile( '<%.*%>', re.IGNORECASE | re.DOTALL)
-        jsp = re.compile( '<%.*%>', re.IGNORECASE | re.DOTALL)
-        jsp2 = re.compile( '<jsp:.*>', re.IGNORECASE | re.DOTALL)
-
-        # I've also seen some devs think like this:
-        #
-        # 1- I have my code that says <? print 'something' ?>
-        # 2- I want to comment that code
-        # 3- I comment it like this!  <!--? print 'something' ?-->
-        # or like this:  <!--? print 'something' ?>
-        #
-        # Not a bad idea, huh?
-        commented_asp = re.compile( '<!--\s*%.*%(--)?>', re.IGNORECASE | re.DOTALL)
-        commented_php = re.compile( '<!--\s*\?.*\?(--)?>', re.IGNORECASE | re.DOTALL)
-        commented_jsp = re.compile( '<!--\s*%.*%(--)?>', re.IGNORECASE | re.DOTALL)
-        commented_jsp2 = re.compile( '<!--\s*jsp:.*(--)?>', re.IGNORECASE | re.DOTALL)
         
-        self._regexs = []
-        self._regexs.append( (php, 'PHP') )
-        self._regexs.append( (asp, 'ASP') )
-        self._regexs.append( (jsp, 'JSP') )
-        self._regexs.append( (jsp2, 'JSP') )
-        self._regexs.append( (commented_php, 'PHP') )
-        self._regexs.append( (commented_asp, 'ASP') )
-        self._regexs.append( (commented_jsp, 'JSP') )
-        self._regexs.append( (commented_jsp2, 'JSP') )
-        
+        #   Internal variables
         self._already_added = disk_list()
         self._first_404 = True
 
     def grep(self, request, response):
         '''
         Plugin entry point, search for the code disclosures.
+        
         @parameter request: The HTTP request object.
         @parameter response: The HTTP response object
         @return: None
@@ -96,32 +64,34 @@ class codeDisclosure(baseGrepPlugin):
 
         if response.is_text_or_html() and response.getURL() not in self._already_added:
             
-            for regex, lang in self._regexs:
-                res = regex.search( response.getBody() )
-
+            match, lang  = is_source_file( response.getBody() )
+            
+            if match:
+                
                 # Check also for 404
-                if res and not is_404( response ):
+                if not is_404( response ):
                     v = vuln.vuln()
                     v.setURL( response.getURL() )
                     v.setId( response.id )
                     v.setSeverity(severity.LOW)
                     v.setName( lang + ' code disclosure vulnerability' )
-                    v.addToHighlight(res.group())
-                    msg = 'The URL: "' + v.getURL() + '" has a code disclosure vulnerability.'
+                    v.addToHighlight(match.group())
+                    msg = 'The URL: "' + v.getURL() + '" has a '+lang+' code disclosure vulnerability.'
                     v.setDesc( msg )
                     kb.kb.append( self, 'codeDisclosure', v )
                     self._already_added.append( response.getURL() )
-
-                # It's a 404!
-                if res and is_404( response ) and self._first_404:
+                
+                else:
+                    
                     self._first_404 = False
                     v = vuln.vuln()
                     v.setURL( response.getURL() )
                     v.setId( response.id )
                     v.setSeverity(severity.LOW)
-                    v.addToHighlight(res.group())
+                    v.addToHighlight(match.group())
                     v.setName( lang + ' code disclosure vulnerability in 404 page' )
-                    msg = 'The URL: "' + v.getURL() + '" has a code disclosure vulnerability in the customized 404 script.'
+                    msg = 'The URL: "' + v.getURL() + '" has a '+lang+' code disclosure vulnerability in'
+                    msg += ' the customized 404 script.'
                     v.setDesc( msg )
                     kb.kb.append( self, 'codeDisclosure', v )
     
