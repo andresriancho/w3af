4c22c644ea0b1a983f04fe72a0bafcc83224a5de
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2010-11-04 13:39:09 +0000

    Fixed bug https://sourceforge.net/apps/trac/w3af/ticket/160106 . Code by Sérgio Pelissari - Primehaxor.

--- plugins/grep/findComments.py
+++ plugins/grep/findComments.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 '''
 findComments.py
 
@@ -52,7 +54,9 @@ class findComments(baseGrepPlugin):
         self._interestingWords = ['user', 'pass', 'xxx', 'fix', 'bug', 'broken', 'oops', 'hack', 
         'caution', 'todo', 'note', 'warning', '!!!', '???', 'shit','stupid', 'tonto', 'porqueria',
         'ciudado', 'usuario', 'contrase', 'puta',
-        'secret','@', 'email','security','captcha'
+        'secret','@', 'email','security','captcha',
+        # some in Portuguese
+        'banco', 'bradesco', 'itau', 'visa', 'bancoreal', 'transfêrencia', 'depósito', 'cartão', 'crédito', 'dados pessoais'
         ]
         self._already_reported_interesting = []
 
@@ -142,16 +146,23 @@ class findComments(baseGrepPlugin):
     def end(self):
         '''
         This method is called when the plugin wont be used anymore.
+        @return: None
         '''
         inform = []
         for comment in self._comments.keys():
             urls_with_this_comment = self._comments[comment]
-            om.out.information('The comment : "' + comment + '" was found on this URL(s):')
+            stick_comment = ' '.join(comment.split())
+            if len(stick_comment) > 40:
+                msg = 'A comment with the string "%s..." (and %s more bytes) was found on these URL(s):' % (stick_comment[:40], str(len(stick_comment) - 40))
+                om.out.information( msg )
+            else:
+                msg = 'A comment containing "%s" was found on these URL(s):' % (stick_comment)
+                om.out.information( msg )
+             
             for url , request_id in urls_with_this_comment:
-                inform.append('- ' + url + ' (request with id:'+str(request_id)+')' )
+                inform.append('- ' + url + ' (request with id: '+str(request_id)+')' )
         
             inform.sort()
-            inform = list(set(inform))
             for i in inform:
                 om.out.information( i )
 
