1faa1e5f05dc3d217ca6853eb635165cb99a10ff
javierandalia <javierandalia@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2010-11-08 11:55:58 +0000

    unit tests for form

---
+++ core/data/dc/tests/test_form.py
@@ -0,0 +1,15 @@
+
+
+import unittest
+from core.data.dc import form
+
+class test_form(unittest.TestCase):
+    pass
+
+def create_form_helper(form_dict):
+    '''
+    Create a dc.form object from a dict container
+    '''
+
+if __name__ == "__main__":
+    unittest.main()
