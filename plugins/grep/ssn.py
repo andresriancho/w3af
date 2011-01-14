88ee9b39b0c530a992751a403e5534cbbda9814f
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2011-01-14 20:32:26 +0000

    Modified the doctest to run with url_object().

--- plugins/grep/ssn.py
+++ plugins/grep/ssn.py
@@ -67,10 +67,11 @@ class ssn(baseGrepPlugin):
 
         >>> from core.data.url.httpResponse import httpResponse
         >>> from core.data.url.HTTPRequest import HTTPRequest
-
+        >>> from core.data.parsers.urlParser import url_object
+        
         Simple test, empty string.
         >>> body = ''
-        >>> url = 'http://www.w3af.com/'
+        >>> url = url_object('http://www.w3af.com/')
         >>> headers = {'content-type': 'text/html'}
         >>> response = httpResponse(200, body , headers, url, url)
         >>> request = HTTPRequest(url)
