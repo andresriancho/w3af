6543095706b098c65330a74e4643f86a9adb28e2
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2011-05-05 01:43:36 +0000

    Added more doctests for importResults.

--- plugins/discovery/importResults.py
+++ plugins/discovery/importResults.py
@@ -113,6 +113,22 @@ class importResults(baseDiscoveryPlugin):
     def _obj_from_csv( self, csv_row ):
         '''
         @return: A fuzzableRequest based on the csv_line.
+        
+        >>> i = importResults()
+        
+        >>> i._obj_from_csv( ('GET','http://www.w3af.com/',''))
+        <QS fuzzable request | GET | http://www.w3af.com/ >
+        
+        >>> i._obj_from_csv( ('GET','http://www.w3af.com/?id=1',''))
+        <QS fuzzable request | GET | http://www.w3af.com/?id=1 >
+
+        >>> pdr = i._obj_from_csv( ('GET','http://www.w3af.com/','id=1'))
+        >>> pdr
+        <postdata fuzzable request | GET | http://www.w3af.com/ >
+        >>> pdr.getData()
+        {'id': ['1']}
+        >>> str(pdr.getData())
+        'id=1'
         '''
         try:
             (method, uri, postdata) = csv_row
