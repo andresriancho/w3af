2f55cb152502f104e4c207607e4a2504b370862d
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2011-05-22 02:48:14 +0000

    Adding unit-test for oracle grep plugin.

--- plugins/grep/oracle.py
+++ plugins/grep/oracle.py
@@ -51,6 +51,54 @@ class oracle(baseGrepPlugin):
         @parameter request: The HTTP request object.
         @parameter response: The HTTP response object
         @return: None
+
+        Init
+        >>> from core.data.url.httpResponse import httpResponse
+        >>> from core.data.request.fuzzableRequest import fuzzableRequest
+        >>> from core.data.parsers.urlParser import url_object
+        >>> from core.controllers.coreHelpers.fingerprint_404 import fingerprint_404_singleton
+        >>> f = fingerprint_404_singleton( [False, False, False] )
+
+        Simple test, empty string.
+        >>> body = ''
+        >>> url = url_object('http://www.w3af.com/')
+        >>> headers = {'content-type': 'text/html'}
+        >>> response = httpResponse(200, body , headers, url, url)
+        >>> request = fuzzableRequest()
+        >>> request.setURL(url)
+        >>> request.setMethod('GET')
+        >>> o = oracle()
+        >>> o.grep(request, response)
+        >>> len(kb.kb.getData('oracle', 'oracle'))
+        0
+
+        One long string
+        >>> body = 'ABC ' * 10000
+        >>> url = url_object('http://www.w3af.com/')
+        >>> headers = {'content-type': 'text/html'}
+        >>> response = httpResponse(200, body , headers, url, url)
+        >>> request = fuzzableRequest()
+        >>> request.setURL(url)
+        >>> request.setMethod('GET')
+        >>> o = oracle()
+        >>> o.grep(request, response)
+        >>> len(kb.kb.getData('oracle', 'oracle'))
+        0
+
+        Something interesting to match
+        >>> body = 'ABC ' * 100
+        >>> body += '<!-- Created by Oracle '
+        >>> body += '</br> ' * 50
+        >>> url = url_object('http://www.w3af.com/')
+        >>> headers = {'content-type': 'text/html'}
+        >>> response = httpResponse(200, body , headers, url, url)
+        >>> request = fuzzableRequest()
+        >>> request.setURL(url)
+        >>> request.setMethod('GET')
+        >>> o = oracle()
+        >>> o.grep(request, response)
+        >>> len(kb.kb.getData('oracle', 'oracle'))
+        1
         '''
         url = response.getURL()
         if response.is_text_or_html() and url not in self._already_analyzed:
