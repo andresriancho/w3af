1c9b7c4bdfd3674aa8a399efab807686f2686ceb
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2011-05-31 23:39:56 +0000

    Added string for detecting error shown in ticket #164219

--- plugins/grep/errorPages.py
+++ plugins/grep/errorPages.py
@@ -114,6 +114,7 @@ class errorPages(baseGrepPlugin):
         mesg.append('class java.lang.')
         mesg.append('java.lang.NullPointerException')
         mesg.append('java.rmi.ServerException')
+        mesg.append('at java.lang.')
         
         mesg.append('onclick="toggle(\'full exception chain stacktrace\')"')
         mesg.append('at org.apache.catalina')
