ac7b91b02dbba6ae6386afaf62719383b38245ae
oxdef <oxdef@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-01-18 12:25:11 +0000

    Fixed Ticket #170287 [Unicode] Yet another "UnicodeDecodeError: 'ascii' codec can't decode byte" in fuzzableRequest.py

--- core/data/request/fuzzableRequest.py
+++ core/data/request/fuzzableRequest.py
@@ -239,6 +239,7 @@ class fuzzableRequest(object):
                 # Because of repeated parameter names, we need to add this:
                 #
                 for the_value in values:
+                    the_value = the_value.decode(self._dc.encoding)
                     # the_value is always a string
                     if len(the_value) > 10:
                         the_value = the_value[:10] + '...'
@@ -248,7 +249,6 @@ class fuzzableRequest(object):
                     
             result_string = result_string[:-2]
             result_string += ')'
-        
         return result_string.encode(DEFAULT_ENCODING)
     
     def __repr__(self):
