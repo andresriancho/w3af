93a3402b40b90532a94825729c6cca809524efea
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-02-17 18:19:12 +0000

    Changing FSF address

--- extlib/cluster/clusterTests.py
+++ extlib/cluster/clusterTests.py
@@ -12,7 +12,7 @@
 # details.
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+# 51 Franklin Street, Suite 500, Boston, MA  02110-1335  USA
 #
 
 from cluster import *
