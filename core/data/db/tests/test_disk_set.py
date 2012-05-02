0b3b20bcefed8885e94d12b28e1e8053a6a7e076
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-05-02 12:47:06 +0000

    Added test for POST data fuzzable request (after seeing some automated bug reports related to it).

--- core/data/db/tests/test_disk_set.py
+++ core/data/db/tests/test_disk_set.py
@@ -7,6 +7,7 @@ from core.data.db.disk_set import disk_set
 
 from core.data.parsers.urlParser import url_object
 from core.data.request.httpQsRequest import HTTPQSRequest
+from core.data.request.httpPostDataRequest import httpPostDataRequest
 
 
 class test_disk_set(unittest.TestCase):
@@ -61,8 +62,35 @@ class test_disk_set(unittest.TestCase):
         
         # This forces an internal change in the URL object
         qsr2.getURL().url_string
-        self.assertTrue( qsr2 in ds )        
+        self.assertTrue( qsr2 in ds )
+
+    def test_add_httpPostDataRequest(self):
+        ds = disk_set()
         
+        uri = url_object('http://w3af.org/?id=2')
+        pdr1 = httpPostDataRequest(uri, method='GET', headers={'Referer': 'http://w3af.org/'})
+
+        uri = url_object('http://w3af.org/?id=3')
+        pdr2 = httpPostDataRequest(uri, method='GET', headers={'Referer': 'http://w3af.com/'})
+        
+        uri = url_object('http://w3af.org/?id=7')
+        pdr3 = httpPostDataRequest(uri, method='FOO', headers={'Referer': 'http://w3af.com/'})
+        
+        ds.add( pdr1 )
+        ds.add( pdr2 )
+        ds.add( pdr2 )
+        ds.add( pdr1 )
+        
+        self.assertEqual( ds[0] , pdr1)
+        self.assertEqual( ds[1] , pdr2)
+        self.assertFalse( pdr3 in ds )
+        self.assertTrue( pdr2 in ds )
+        self.assertEqual( len(ds) , 2)
+        
+        # This forces an internal change in the URL object
+        pdr2.getURL().url_string
+        self.assertTrue( pdr2 in ds )
+                
     def test_update(self):
         ds = disk_set()
         ds.add(1)
