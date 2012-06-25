4453615e0f952ff5c872e521cd91ea4d69216ee4
oxdef <oxdef@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-06-25 19:59:51 +0000

    1. skeleton of web20Spider; 2. CasperJS script spider.js

---
+++ plugins/discovery/web20Spider.py
@@ -0,0 +1,63 @@
+'''
+web20Spider.py
+
+Copyright 2006 Andres Riancho
+
+This file is part of w3af, w3af.sourceforge.net .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+'''
+
+from core.controllers.basePlugin.baseDiscoveryPlugin import baseDiscoveryPlugin
+from core.controllers.misc.decorators import runonce
+from core.controllers.w3afException import w3afException, w3afRunOnce
+from core.data.options.option import option
+from core.data.options.optionList import optionList
+from core.data.parsers.urlParser import url_object
+import core.controllers.outputManager as om
+import core.data.constants.w3afPorts as w3afPorts
+
+from plugins.discovery.spiderMan import TERMINATE_URL, spiderMan
+
+class web20Spider(baseDiscoveryPlugin):
+
+    def __init__(self):
+        # Internal variables
+        self._run = True
+
+    @runonce(exc_class=w3afRunOnce)
+    def discover(self, freq ):
+        print 'web20Spider is running!'
+        return []
+
+    def getOptions(self):
+        '''
+        @return: A list of option objects for this plugin.
+        '''
+        ol = optionList()
+        return ol
+
+    def setOptions(self, optionsMap):
+        pass
+
+    def getPluginDeps( self ):
+        return []
+
+    def getLongDesc( self ):
+        '''
+        @return: A DETAILED description of the plugin functions and features.
+        '''
+        return '''Purpose of this plugins is crawling of modern web app with help of integrated browser.
+        '''
---
+++ plugins/discovery/web20Spider/spider.js
@@ -0,0 +1,110 @@
+// 
+// TODO
+// 1. Cookie suuport for auth protected web apps
+// 2. forms
+var casper, current_deep, max_deep, process_page, target, target_domain, states, to_visit;
+
+casper = require('casper').create();
+current_deep = 0;
+max_deep = 3;
+states = [];
+to_visit = []
+
+var casper = require('casper').create({
+    verbose: false,
+    logLevel: "debug"
+});
+target = 'http://localhost/';
+to_visit.push(target);
+
+function get_domain(url) {
+    chunks = url.split('/');
+    return chunks[2];
+}
+
+target_domain = get_domain(target);
+
+function getClickable() {
+    // 
+    // http://stackoverflow.com/questions/4588119/get-elements-css-selector-without-element-id
+    //
+    function fullPath(el) {
+      var names = [];
+      while (el.parentNode) {
+        if (el.id) {
+          names.unshift('#'+el.id);
+          break;
+        } else {
+          for (var c=1,e=el; e.previousElementSibling; e=e.previousElementSibling, c++);
+          names.unshift(el.tagName+":nth-child("+c+")");
+          el = el.parentNode;
+        }
+      }
+      return names.join(" > ");
+    }
+
+    var links =  __utils__.findAll('a, img');
+    var paths = [];
+    for (var i=0; i<links.length; i++) {
+        var tmp = {
+            url: null, 
+            href: links[i].getAttribute('href'),
+            path: fullPath(links[i])
+        };
+        paths.push(tmp);
+    }
+    return paths;
+}
+
+casper.start(target, function() {});
+
+process_page = function() {
+    var url;
+    if (to_visit.length > 0) {
+        url = to_visit.pop();
+        states.push(url)
+    } else {
+        return;
+    }
+    this.thenOpen(url, function() {
+        var links = this.evaluate(getClickable);
+        for (var i=0; i<links.length; i++) {
+            this.echo('Click on "' + links[i].path + '" with href "' + links[i].href + '"');
+            if (this.exists(links[i].path)) {
+                this.thenClick(links[i].path).then(function(){
+                    this.wait(300, function() {
+                        var tmp_url = this.getCurrentUrl();
+                        if (url !== tmp_url) {
+                            this.echo('We are inside "' + tmp_url + '". Lets go back!' );
+                            if (get_domain(tmp_url) == target_domain) {
+                                if (states.indexOf(tmp_url) == -1) {
+                                    to_visit.push(tmp_url)
+                                }
+                            }
+                            this.back();
+                        }
+                    });
+                });
+            } else {
+                this.echo(links[i].path + ' doesnt exist on ' + this.getCurrentUrl());
+            }
+        }
+    });
+};
+
+for (var i=0; i<=max_deep; i++) {
+    casper.then(process_page);
+}
+
+casper.then(function() {
+    this.echo('=====================')
+    this.echo('States:');
+    this.each(states, function(self, state) {this.echo(' '+state)});
+    if (to_visit.length) {
+        this.echo('To visit:');
+        this.each(to_visit, function(self, state) {this.echo(' '+state)});
+    }
+});
+casper.run(function() {
+    this.echo('Quit').exit();
+});
