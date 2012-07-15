5282df517c93af13587166d5d4ca8ce9b04f9ad8
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-07-15 17:44:14 +0000

    Added one more unittest for DAV.

--- plugins/tests/audit/test_dav.py
+++ plugins/tests/audit/test_dav.py
@@ -25,13 +25,15 @@ from ..helper import PluginTest, PluginConfig
 class TestDav(PluginTest):
     
     target_vuln_all = 'http://moth/w3af/audit/dav/write-all/'
+    target_no_privs = 'http://moth/w3af/audit/dav/no-privileges/'
     target_safe_all = 'http://moth/w3af/audit/eval/'
     
     _run_configs = {
         'cfg': {
             'target': None,
             'plugins': {
-                 'audit': (PluginConfig('dav',),),                 }
+                 'audit': (PluginConfig('dav',),),                 
+                 }
             },
         }
     
@@ -40,14 +42,38 @@ class TestDav(PluginTest):
         self._scan( self.target_vuln_all, cfg['plugins'])
         
         vulns = self.kb.getData('dav', 'dav')
-        self.assertEquals(2, len(vulns))
         
-        self.assertTrue( all(['Insecure DAV configuration' == v.getName() for v in vulns]) )
+        EXPECTED_NAMES = set(['Insecure DAV configuration'] * 2)
+        
+        self.assertEquals( EXPECTED_NAMES,
+                           set([v.getName() for v in vulns])
+                          )
         
         self.assertEquals( set(['PUT', 'PROPFIND']),
                            set([v.getMethod() for v in vulns]))
         
         self.assertTrue( all([self.target_vuln_all == str(v.getURL().getDomainPath()) for v in vulns]))
+    
+    def test_no_privileges(self):
+        '''
+        DAV is configured but the directory doesn't have the file-system permissions
+        to allow the Apache process to write to it.
+        '''
+        cfg = self._run_configs['cfg']
+        self._scan( self.target_no_privs, cfg['plugins'])
+        
+        vulns = self.kb.getData('dav', 'dav')
+        
+        self.assertTrue( len(vulns), 1)
+        
+        vuln = vulns[0]
+        
+        self.assertEquals( 'DAV insufficient privileges',
+                           vuln.getName() )
+        
+        self.assertEquals( self.target_no_privs, 
+                           str(vuln.getURL().getDomainPath() ) )
+        
     
     def test_not_found_dav(self):
         cfg = self._run_configs['cfg']
