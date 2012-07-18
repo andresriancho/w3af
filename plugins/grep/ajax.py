426a28b2913234a634557a7faf3a6969dfcb4ff8
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-07-18 19:27:55 +0000

    Migrated doctest to unittest for grep.ajax

--- plugins/grep/ajax.py
+++ plugins/grep/ajax.py
@@ -19,19 +19,14 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
-
-# options
-from core.data.options.optionList import optionList
-
-from core.controllers.basePlugin.baseGrepPlugin import baseGrepPlugin
+import re
+from lxml import etree
 
 import core.data.kb.knowledgeBase as kb
 import core.data.kb.info as info
 
+from core.controllers.basePlugin.baseGrepPlugin import baseGrepPlugin
 from core.data.bloomfilter.bloomfilter import scalable_bloomfilter
-
-import re
-from lxml import etree
 
 
 class ajax(baseGrepPlugin):
@@ -62,79 +57,6 @@ class ajax(baseGrepPlugin):
         @parameter request: The HTTP request object.
         @parameter response: The HTTP response object
         @return: None, all results are saved in the kb.
-        
-        Init
-        >>> from core.data.url.httpResponse import httpResponse
-        >>> from core.data.request.fuzzableRequest import fuzzableRequest
-        >>> from core.controllers.misc.temp_dir import create_temp_dir
-        >>> from core.data.parsers.urlParser import url_object
-        >>> o = create_temp_dir()
-
-        Simple test, empty string.
-        >>> body = ''
-        >>> url = url_object('http://www.w3af.com/')
-        >>> headers = {'content-type': 'text/html'}
-        >>> response = httpResponse(200, body , headers, url, url)
-        >>> request = fuzzableRequest(url, method='GET')
-        >>> a = ajax()
-        >>> a.grep(request, response)
-        >>> assert len(kb.kb.getData('ajax', 'ajax')) == 0
-
-        Discover ajax!
-        >>> body = '<html><head><script>xhr = new XMLHttpRequest(); xhr.open(GET, "data.txt",  true); </script></head><html>'
-        >>> url = url_object('http://www.w3af.com/')
-        >>> headers = {'content-type': 'text/html'}
-        >>> response = httpResponse(200, body , headers, url, url)
-        >>> request = fuzzableRequest(url, method='GET')
-        >>> a = ajax()
-        >>> a.grep(request, response)
-        >>> assert len(kb.kb.getData('ajax', 'ajax')) == 1
-
-        Discover ajax with a broken script tag that doesn't close
-        >>> kb.kb.save('ajax','ajax',[])
-        >>> body = '<html><head><script>xhr = new XMLHttpRequest(); xhr.open(GET, "data.txt",  true); </head><html>'
-        >>> url = url_object('http://www.w3af.com/')
-        >>> headers = {'content-type': 'text/html'}
-        >>> response = httpResponse(200, body , headers, url, url)
-        >>> request = fuzzableRequest(url, method='GET')
-        >>> a = ajax()
-        >>> a.grep(request, response)
-        >>> assert len(kb.kb.getData('ajax', 'ajax')) == 1
-
-        Discover ajax with a broken script, head and html tags.
-        >>> kb.kb.save('ajax','ajax',[])
-        >>> body = '<html><head><script>xhr = new XMLHttpRequest(); xhr.open(GET, "data.txt",  true);'
-        >>> url = url_object('http://www.w3af.com/')
-        >>> headers = {'content-type': 'text/html'}
-        >>> response = httpResponse(200, body , headers, url, url)
-        >>> request = fuzzableRequest(url, method='GET')
-        >>> a = ajax()
-        >>> a.grep(request, response)
-        >>> assert len(kb.kb.getData('ajax', 'ajax')) == 1
-
-        Another ajax function, no broken html.
-        >>> kb.kb.save('ajax','ajax',[])
-        >>> body = '<html><head><script> ... xhr = new ActiveXObject("Microsoft.XMLHTTP"); ... </script></head><html>'
-        >>> url = url_object('http://www.w3af.com/')
-        >>> headers = {'content-type': 'text/html'}
-        >>> response = httpResponse(200, body , headers, url, url)
-        >>> request = fuzzableRequest(url, method='GET')
-        >>> a = ajax()
-        >>> a.grep(request, response)
-        >>> assert len(kb.kb.getData('ajax', 'ajax')) == 1
-
-        Two functions, I only want one report for this page.
-        >>> kb.kb.save('ajax','ajax',[])
-        >>> body = '<script> ... xhr = new XMLHttpRequest(); ... xhr = new ActiveXObject("Microsoft.XMLHTTP"); ... </script>'
-        >>> url = url_object('http://www.w3af.com/')
-        >>> headers = {'content-type': 'text/html'}
-        >>> response = httpResponse(200, body , headers, url, url)
-        >>> request = fuzzableRequest(url, method='GET')
-        >>> a = ajax()
-        >>> a.grep(request, response)
-        >>> len(kb.kb.getData('ajax', 'ajax'))
-        1
-
         '''
         url = response.getURL()
         if response.is_text_or_html() and url not in self._already_inspected:
@@ -164,29 +86,11 @@ class ajax(baseGrepPlugin):
                             i.addToHighlight(res.group(0))
                             kb.kb.append(self, 'ajax', i)
 
-    
-    def setOptions( self, OptionList ):
-        pass
-    
-    def getOptions( self ):
-        '''
-        @return: A list of option objects for this plugin.
-        '''    
-        ol = optionList()
-        return ol
-        
     def end(self):
         '''
         This method is called when the plugin wont be used anymore.
         '''
         self.print_uniq( kb.kb.getData( 'ajax', 'ajax' ), 'URL' )
-
-    def getPluginDeps( self ):
-        '''
-        @return: A list with the names of the plugins that should be run before the
-        current one.
-        '''
-        return []
     
     def getLongDesc( self ):
         '''
---
+++ plugins/tests/grep/test_ajax.py
@@ -0,0 +1,100 @@
+'''
+test_ajax.py
+
+Copyright 2012 Andres Riancho
+
+This file is part of w3af, w3af.sourceforge.net .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+'''
+import unittest
+
+import core.data.kb.knowledgeBase as kb
+
+from core.data.url.httpResponse import httpResponse
+from core.data.request.fuzzableRequest import fuzzableRequest
+from core.controllers.misc.temp_dir import create_temp_dir
+from core.data.parsers.urlParser import url_object
+from plugins.grep.ajax import ajax
+
+
+class test_ajax(unittest.TestCase):
+    
+    def setUp(self):
+        create_temp_dir()
+        kb.kb.cleanup()
+        self.plugin = ajax()
+        self.plugin._already_inspected = set()        
+        self.url = url_object('http://www.w3af.com/')
+        self.request = fuzzableRequest(self.url)
+        
+    def test_ajax_empty(self):
+        body = ''
+        url = url_object('http://www.w3af.com/')
+        headers = {'content-type': 'text/html'}
+        response = httpResponse(200, body , headers, url, url)
+        request = fuzzableRequest(url, method='GET')
+        self.plugin.grep(request, response)
+        self.assertEquals( len(kb.kb.getData('ajax', 'ajax')) , 0 )
+    
+    def test_ajax_find(self):
+        body = '<html><head><script>xhr = new XMLHttpRequest(); xhr.open(GET, "data.txt",  true); </script></head><html>'
+        url = url_object('http://www.w3af.com/')
+        headers = {'content-type': 'text/html'}
+        response = httpResponse(200, body , headers, url, url)
+        request = fuzzableRequest(url, method='GET')
+        self.plugin.grep(request, response)
+        self.assertEquals( len(kb.kb.getData('ajax', 'ajax')) , 1 )
+    
+    def test_ajax_broken_html(self):
+        kb.kb.save('ajax','ajax',[])
+        body = '<html><head><script>xhr = new XMLHttpRequest(); xhr.open(GET, "data.txt",  true); </head><html>'
+        url = url_object('http://www.w3af.com/')
+        headers = {'content-type': 'text/html'}
+        response = httpResponse(200, body , headers, url, url)
+        request = fuzzableRequest(url, method='GET')
+        self.plugin.grep(request, response)
+        self.assertEquals( len(kb.kb.getData('ajax', 'ajax')) , 1 )
+    
+    def test_ajax_broken_2(self):
+        kb.kb.save('ajax','ajax',[])
+        body = '<html><head><script>xhr = new XMLHttpRequest(); xhr.open(GET, "data.txt",  true);'
+        url = url_object('http://www.w3af.com/')
+        headers = {'content-type': 'text/html'}
+        response = httpResponse(200, body , headers, url, url)
+        request = fuzzableRequest(url, method='GET')
+        self.plugin.grep(request, response)
+        self.assertEquals( len(kb.kb.getData('ajax', 'ajax')) , 1 )
+    
+    def test_ajax_find_2(self):
+        kb.kb.save('ajax','ajax',[])
+        body = '<html><head><script> ... xhr = new ActiveXObject("Microsoft.XMLHTTP"); ... </script></head><html>'
+        url = url_object('http://www.w3af.com/')
+        headers = {'content-type': 'text/html'}
+        response = httpResponse(200, body , headers, url, url)
+        request = fuzzableRequest(url, method='GET')
+        a = ajax()
+        a.grep(request, response)
+        self.assertEquals( len(kb.kb.getData('ajax', 'ajax')) , 1 )
+    
+    def test_ajax_two(self):
+        kb.kb.save('ajax','ajax',[])
+        body = '<script> ... xhr = new XMLHttpRequest(); ... xhr = new ActiveXObject("Microsoft.XMLHTTP"); ... </script>'
+        url = url_object('http://www.w3af.com/')
+        headers = {'content-type': 'text/html'}
+        response = httpResponse(200, body , headers, url, url)
+        request = fuzzableRequest(url, method='GET')
+        self.plugin.grep(request, response)
+        self.assertEquals( len(kb.kb.getData('ajax', 'ajax')) , 1 )
