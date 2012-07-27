aa6719310d24fdf40be4847b8cacc838898334fb
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-07-27 21:27:04 +0000

    Adding (the strangest ever) unittest to email_report.

--- plugins/output/email_report.py
+++ plugins/output/email_report.py
@@ -17,37 +17,40 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
-
 import smtplib
+
 from email.mime.text import MIMEText
 
-from core.controllers.basePlugin.baseOutputPlugin import baseOutputPlugin
-from core.controllers.w3afException import w3afException
 import core.controllers.outputManager as om
 import core.data.kb.knowledgeBase as kb
 import core.data.kb.config as cf
+
+from core.controllers.basePlugin.baseOutputPlugin import baseOutputPlugin
 from core.data.options.option import option
 from core.data.options.optionList import optionList
-import core.data.constants.severity as severity
 
-MSG_TMPL = '''Hello!
-Target: %s has some vulnerabilities.
-
-'''
 
 class email_report(baseOutputPlugin):
     '''Email report to specified addresses.
+    
+    @author: Taras (oxdef@oxdef.info)
     '''
-
+    
+    MSG_TMPL = '''Hello!
+    Target: %s has some vulnerabilities.
+    
+    '''
+    
     def __init__(self):
         baseOutputPlugin.__init__(self)
+        
         self.targets = []
-        self.tpl = MSG_TMPL
+        self._exec = False
+        
         self.smtpServer = 'localhost'
         self.smtpPort = 25
         self.toAddrs = ''
         self.fromAddr = ''
-        self._exec = False
 
     def log_enabled_plugins(self, pluginsDict, optionsDict):
         self.targets = cf.cf.getData('targets')
@@ -60,18 +63,23 @@ class email_report(baseOutputPlugin):
 
     def getOptions(self):
         ol = optionList()
+        
         d = 'SMTP server ADDRESS to send notifications through, e.g. smtp.yourdomain.com'
         o = option('smtpServer', self.smtpServer, d, 'string')
         ol.add(o)
+        
         d = 'SMTP server PORT'
         o = option('smtpPort', self.smtpPort, d, 'integer')
         ol.add(o)
+        
         d = 'Recipient email address'
         o = option('toAddrs', self.toAddrs, d, 'list')
         ol.add(o)
+        
         d = '"From" email address'
         o = option('fromAddr', self.fromAddr, d, 'string')
         ol.add(o)
+        
         return ol
 
     def end(self):
@@ -79,7 +87,7 @@ class email_report(baseOutputPlugin):
             return
         self._exec = True
 
-        data = self.tpl % (self.targets[0])
+        data = self.MSG_TMPL % (self.targets[0])
         vulns = kb.kb.getAllVulns()
 
         for v in vulns:
@@ -95,36 +103,22 @@ class email_report(baseOutputPlugin):
             server.sendmail(self.fromAddr, self.toAddrs, msg.as_string())
             server.quit()
         except Exception, e:
-            msg = 'The SMTP settings in email_report plugin seem to be incorrect. Original error: "'
-            msg += str(e) + '".'
+            msg = 'The SMTP settings in email_report plugin seem to be incorrect.'
+            msg += ' Original error: "%s".' % e
             om.out.error( msg )
 
     def getLongDesc(self):
         return '''
-            This plugin sends short report (only vulnerabilities)
-            by email to specified addresses.
+            This plugin sends short report (only vulnerabilities) by email to
+            specified addresses.
 
             There are some configurable parameters:
-            - smtpServer
-            - smtpPort
-            - toAddrs
-            - fromAddr
+                - smtpServer
+                - smtpPort
+                - toAddrs
+                - fromAddr
             '''
-
-    def debug(self, message, newLine = True):
-        pass
-
-    def information(self, message , newLine = True):
-        pass
-
-    def error(self, message , newLine = True):
-        pass
-
-    def vulnerability(self, message , newLine=True, severity=severity.MEDIUM):
-        pass
-
-    def console(self, message, newLine = True):
-        pass
-
-    def logHttp(self, request, response):
-        pass
+    
+    def do_nothing(self, *args, **kwds): pass
+    
+    debug = information = error = vulnerability = console = do_nothing
