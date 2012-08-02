758f93aacdd40b52df6d501b54ac70c8a443ac20
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-08-02 17:14:01 +0000

    Adding test case for demo.testfire.net login

--- plugins/tests/auth/test_generic.py
+++ plugins/tests/auth/test_generic.py
@@ -18,14 +18,17 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
+from nose.plugins.skip import SkipTest
 
 from ..helper import PluginTest, PluginConfig
 from core.data.parsers.urlParser import url_object
+from core.data.url.xUrllib import xUrllib
 
 
 class TestGeneric(PluginTest):
     
     base_url = 'http://moth/w3af/auth/generic/'
+    demo_testfire = 'http://demo.testfire.net/bank/'
     
     _run_config = {
             'target': base_url,
@@ -50,6 +53,31 @@ class TestGeneric(PluginTest):
              }
         }
     
+    demo_testfire_net = {
+            'target': demo_testfire,
+            'plugins': {
+                'discovery': (
+                    PluginConfig('web_spider',
+                                 ('onlyForward', True, PluginConfig.BOOL),
+                                 ('ignoreRegex', '.*logout.*', PluginConfig.STR),
+                                 ('followRegex', '.*queryxpath.*', PluginConfig.STR)),
+                              
+                ),
+                'auth':  (PluginConfig('generic',
+                                 ('username', 'admin', PluginConfig.STR),
+                                 ('password', 'admin', PluginConfig.STR),
+                                 ('username_field', 'uid', PluginConfig.STR),
+                                 ('password_field', 'passw', PluginConfig.STR),
+                                 ('auth_url', url_object(demo_testfire + 'login.aspx') , PluginConfig.URL),
+                                 ('check_url', url_object(demo_testfire + 'main.aspx') , PluginConfig.URL),
+                                 ('check_string', 'View Recent Transactions', PluginConfig.STR),
+                            ),
+                         ),
+             }
+        }
+    
+    
+    
     def test_post_auth_xss(self):
         self._scan(self._run_config['target'], self._run_config['plugins'])
 
@@ -60,4 +88,25 @@ class TestGeneric(PluginTest):
         vuln = vulns[0]
         self.assertEquals( vuln.getName(), 'Cross site scripting vulnerability', vuln.getName() )
         self.assertEquals( vuln.getVar(), 'section', vuln.getVar() )
+        
+    def test_demo_testfire_net(self):
+        # We don't control the demo.testfire.net domain, so we'll check if its
+        # up before doing anything else
+        uri_opener = xUrllib()
+        login_url = url_object(self.demo_testfire + 'login.aspx')
+        try:
+            res = uri_opener.GET(login_url)
+        except:
+            raise SkipTest('demo.testfire.net is unreachable!')
+        else:
+            if not 'Online Banking Login' in res.body: 
+                raise SkipTest('demo.testfire.net has changed!')
+    
+        self._scan(self.demo_testfire_net['target'], self.demo_testfire_net['plugins'])
+
+        urls = self.kb.getData('urls', 'url_objects')
+        url_strings = set(str(u) for u in urls)
+        
+        self.assertTrue( self.demo_testfire + 'queryxpath.aspx' in url_strings ) 
+        self.assertTrue( self.demo_testfire + 'queryxpath.aspx.cs' in url_strings )
         
