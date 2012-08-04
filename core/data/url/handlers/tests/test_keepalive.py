52904db3c43189180affc2401da4e775a194d239
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-08-04 23:40:59 +0000

    Cosmetic

--- core/data/url/handlers/tests/test_keepalive.py
+++ core/data/url/handlers/tests/test_keepalive.py
@@ -1,13 +1,37 @@
-from pymock import PyMockTestCase, method, override
-from time import time
+'''
+test_keepalive.py
+
+Copyright 2012 Andres Riancho
+
+This file is part of w3af, w3af.sourceforge.net .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+'''
 import socket
+import random
+
+from time import time
+from pymock import PyMockTestCase, method, override
 
 from core.controllers.w3afException import w3afException, w3afMustStopException
-from core.data.url.HTTPRequest import HTTPRequest
-from core.data.url.handlers.keepalive import KeepAliveHandler, ConnectionManager,\
-HTTPResponse, URLTimeoutError
+from core.data.url.handlers.keepalive import (KeepAliveHandler, ConnectionManager,
+                                              HTTPResponse, URLTimeoutError)
+
 
 class dummy: pass
+
 
 class test_keepalive(PyMockTestCase):
 
@@ -110,8 +134,8 @@ class test_keepalive(PyMockTestCase):
         We only want to use a single instance of the ConnectionManager.
         '''
         conn_mgr_id = id(self.kahdler._cm)
-        import random
-        for i in xrange(random.randint(1, 10)):
+        
+        for _ in xrange(random.randint(1, 10)):
             self.assertTrue(conn_mgr_id == id(KeepAliveHandler()._cm))
         
         
@@ -175,11 +199,4 @@ class test_connection_mgr(PyMockTestCase):
         # curr_len = old_len - 1
         self.assertTrue(old_len-1 == self.cm.get_connections_total() == 0)
         
-        
-        
 
-        
-
-if __name__=="__main__":
-    import unittest
-    unittest.main()
