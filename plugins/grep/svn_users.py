0cf7c5239a33f4f5f624cf44c6355099b307cb64
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-08-08 23:00:00 +0000

    Removing test script, adding unittest, minor changes to svn_users. Finished! All grep plugins have unittests now!

--- plugins/grep/svn_users.py
+++ plugins/grep/svn_users.py
@@ -19,21 +19,14 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
-
-import core.controllers.outputManager as om
-
-# options
-from core.data.options.option import option
-from core.data.options.optionList import optionList
-
-from core.controllers.basePlugin.baseGrepPlugin import baseGrepPlugin
-from core.data.bloomfilter.bloomfilter import scalable_bloomfilter
+import re
 
 import core.data.kb.knowledgeBase as kb
 import core.data.kb.vuln as vuln
 import core.data.constants.severity as severity
 
-import re
+from core.controllers.basePlugin.baseGrepPlugin import baseGrepPlugin
+from core.data.bloomfilter.bloomfilter import scalable_bloomfilter
 
 
 class svn_users(baseGrepPlugin):
@@ -53,7 +46,7 @@ class svn_users(baseGrepPlugin):
         #
         regex = '\$.{1,12}: .*? .*? \d{4}[-/]\d{1,2}[-/]\d{1,2}'
         regex += ' \d{1,2}:\d{1,2}:\d{1,2}.*? (.*?) (Exp )?\$'
-        self._regex_list = [ re.compile(regex) ]
+        self._regex_list = [ re.compile(regex), ]
         
     def grep(self, request, response):
         '''
@@ -85,35 +78,20 @@ class svn_users(baseGrepPlugin):
                     kb.kb.append(self, 'users', v)
 
         
-    def setOptions( self, OptionList ):
-        pass
-    
-    def getOptions( self ):
-        '''
-        @return: A list of option objects for this plugin.
-        '''    
-        ol = optionList()
-        return ol
-
     def end(self):
         '''
         This method is called when the plugin wont be used anymore.
         '''
         self.print_uniq( kb.kb.getData( 'svn_users', 'users' ), 'URL' )
     
-    def getPluginDeps( self ):
-        '''
-        @return: A list with the names of the plugins that should be run before the
-        current one.
-        '''
-        return []
-    
     def getLongDesc( self ):
         '''
         @return: A DETAILED description of the plugin functions and features.
         '''
         return '''
-        This plugin greps every page for users of the versioning system. Sometimes the HTML pages are
-        versioned using CVS or SVN, if the header of the versioning system is saved as a comment in this page,
-        the user that edited the page will be saved on that header and will be added to the knowledgeBase.
+        This plugin greps every page for users of the versioning system. Sometimes
+        the HTML pages are versioned using CVS or SVN, if the header of the
+        versioning system is saved as a comment in this page, the user that edited
+        the page will be saved on that header and will be added to the knowledge
+        base.
         '''
