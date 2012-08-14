71d29d45ce27421646e73764aab69975c69e3251
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-08-14 03:20:32 +0000

    Adding dot_net_errors unittest, rewrite plugin.

---
+++ plugins/infrastructure/dot_net_errors.py
@@ -0,0 +1,125 @@
+'''
+dot_net_errors.py
+
+Copyright 2006 Andres Riancho
+
+This file is part of w3af, w3af.sourceforge.net .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+'''
+import core.data.kb.knowledgeBase as kb
+import core.data.kb.vuln as vuln
+import core.data.constants.severity as severity
+
+from core.controllers.basePlugin.baseInfrastructurePlugin import baseInfrastructurePlugin
+from core.data.bloomfilter.bloomfilter import scalable_bloomfilter
+
+
+class dot_net_errors(baseInfrastructurePlugin):
+    '''
+    Request specially crafted URLs that generate ASP.NET errors in order
+    to gather information.
+    
+    @author: Andres Riancho (andres.riancho@gmail.com)
+    '''
+
+    def __init__(self):
+        baseInfrastructurePlugin.__init__(self)
+
+        # Internal variables
+        self._already_tested = scalable_bloomfilter()
+        # On real web applications, if we can't trigger an error in the first
+        # MAX_TESTS tests, it simply won't happen and we have to stop testing.
+        self.MAX_TESTS = 25
+
+    def discover(self, fuzzable_request):
+        '''
+        Requests the special filenames.
+        
+        @parameter fuzzable_request: A fuzzable_request instance that contains
+                                    (among other things) the URL to test.
+        '''
+        if len(self._already_tested) < self.MAX_TESTS \
+        and fuzzable_request.getURL() not in self._already_tested:
+            self._already_tested.add( fuzzable_request.getURL() )
+
+            test_generator = self._generate_URLs( fuzzable_request.getURL() )
+
+            self._tm.threadpool.map(self._send_and_check,
+                                    test_generator,
+                                    chunksize=1)                
+
+    def _generate_URLs(self, original_url):
+        '''
+        Generate new URLs based on original_url.
+
+        @parameter original_url: The original url that has to be modified in
+                                 order to trigger errors in the remote application.
+        '''
+        special_chars = ['|', '~']
+
+        filename = original_url.getFileName()
+        if filename != '' and '.' in filename:
+            splitted_filename = filename.split('.')
+            extension = splitted_filename[-1:][0]
+            name = '.'.join( splitted_filename[0:-1] )
+
+            for char in special_chars:
+                new_filename = name + char + '.' + extension
+                new_url = original_url.urlJoin(new_filename)
+                yield new_url
+
+    def _send_and_check(self, url):
+        '''
+        @parameter response: The httpResponse object that holds the content of
+                             the response to analyze.
+        '''
+        response = self._uri_opener.GET( url, cache=True )
+        
+        viewable_remote_machine = '<b>Details:</b> To enable the details of this'
+        viewable_remote_machine += ' specific error message to be viewable on'
+        viewable_remote_machine += ' remote machines'
+        
+        if viewable_remote_machine not in response.body\
+        and '<h2> <i>Runtime Error</i> </h2></span>' in response.body:
+            v = vuln.vuln( response )
+            v.setPluginName(self.getName())
+            v.setId( response.id )
+            v.setSeverity(severity.LOW)
+            v.setName( 'Information disclosure via .NET errors' )
+            msg = 'Detailed information about ASP.NET error messages can be'
+            msg += ' viewed from remote sites. The URL: "%s" discloses detailed'
+            msg += ' error messages.'
+            v.setDesc( msg % response.getURL() )
+            kb.kb.append( self, 'dot_net_errors', v )
+                
+    def getPluginDeps( self ):
+        '''
+        @return: A list with the names of the plugins that should be run before the
+        current one.
+        '''
+        return ['grep.error_pages']
+        
+    def getLongDesc( self ):
+        '''
+        @return: A DETAILED description of the plugin functions and features.
+        '''
+        return '''
+        Request specially crafted URLs that generate ASP.NET errors in order to
+        gather information like the ASP.NET version. Some examples of URLs that
+        generate errors are:
+            - default|.aspx
+            - default~.aspx
+        '''
---
+++ plugins/tests/infrastructure/test_dot_net_errors.py
@@ -0,0 +1,47 @@
+'''
+test_dot_net_errors.py
+
+Copyright 2012 Andres Riancho
+
+This file is part of w3af, w3af.sourceforge.net .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+'''
+from ..helper import PluginTest, PluginConfig
+
+class TestDotNetErrors(PluginTest):
+    
+    moth_url = 'http://moth/w3af/infrastructure/dot_net_errors/'
+    
+    _run_configs = {
+        'cfg': {
+            'target': moth_url,
+            'plugins': {'infrastructure': (PluginConfig('dot_net_errors'),),
+                        'crawl': (PluginConfig('web_spider',
+                                               ('onlyForward', True, PluginConfig.BOOL),),)}
+            }
+        }
+    
+    def test_dot_net_errors(self):
+        cfg = self._run_configs['cfg']
+        self._scan(cfg['target'], cfg['plugins'])
+        
+        infos = self.kb.getData('dot_net_errors', 'dot_net_errors')
+        
+        self.assertEqual( len(infos), 1, infos )
+        
+        info = infos[0]
+        
+        self.assertEqual( info.getName(), 'Information disclosure via .NET errors')
+                
