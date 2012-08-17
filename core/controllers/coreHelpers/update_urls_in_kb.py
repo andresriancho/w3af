39803c4e33713fc336fada6b6b0d3ac1217d5298
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-08-17 03:05:44 +0000

    Added some utility functions.

--- core/controllers/coreHelpers/update_urls_in_kb.py
+++ core/controllers/coreHelpers/update_urls_in_kb.py
@@ -19,12 +19,15 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
+import threading
 import core.data.kb.knowledgeBase as kb
 
+update_lock = threading.RLock()
 
-def update_URLs_in_KB( fuzzable_request_list ):
+
+def update_kb( fuzzable_request_list ):
     '''
-    Updates the URL list in the kb for other plugins to use.
+    Updates the URL and fuzzable request list in the kb for other plugins to use.
     
     >>> from core.data.parsers.urlParser import url_object
     >>> from core.data.request.fuzzableRequest import fuzzableRequest
@@ -45,18 +48,31 @@ def update_URLs_in_KB( fuzzable_request_list ):
     [<url_object for "http://w3af.org/">, <url_object for "http://w3af.org/blog/">]
     
     '''
-    url_object_list = kb.kb.getData( 'url', 'url_objects' )
-    new_list = [ fr.getURL() for fr in fuzzable_request_list \
-                 if fr.getURL() not in url_object_list ]
+    with update_lock:
+        # Update the list of URLs that is used world wide, it is VERY
+        # important to notice that we always need to append stuff to the
+        # end of this list and avoid things like sorting, inserting in
+        # positions different than the tail, etc. This helps with the
+        # implementation of scanrun.IteratedURLList .
+        #
+        # TODO: Force this somehow so that this isn't just a warning but
+        # something that fails if developers change it.
+        url_object_list = get_urls_from_kb()
+        new_list = [ fr.getURL() for fr in fuzzable_request_list \
+                     if fr.getURL() not in url_object_list ]
+    
+        url_object_list.extend( new_list )
+        kb.kb.save( 'urls', 'url_objects' ,  url_object_list )
+    
+        # Update the list of fuzzable requests that lives in the KB
+        # TODO: Move the whole KB to a sqlite database in order to save
+        #       some memory usage.
+        kb_fr_set = get_fuzzable_requests_from_kb()
+        kb_fr_set.update( fuzzable_request_list )
 
-    # Update the list of URLs that is used world wide, it is VERY
-    # important to notice that we always need to append stuff to the
-    # end of this list and avoid things like sorting, inserting in
-    # positions different than the tail, etc. This helps with the
-    # implementation of scanrun.IteratedURLList .
-    #
-    # TODO: Force this somehow so that this isn't just a warning but
-    # something that fails if developers change it.
-    url_object_list.extend( new_list )
-    kb.kb.save( 'urls', 'url_objects' ,  url_object_list )
-
+def get_urls_from_kb():
+    return kb.kb.getData( 'urls', 'url_objects' )
+    
+def get_fuzzable_requests_from_kb():
+    return kb.kb.getData( 'urls', 'fuzzable_requests' )
+        
