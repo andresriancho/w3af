7f5b0fb3211a14d76087f0ac9c95ba039a0fe67e
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-08-27 17:58:44 +0000

    Adding new grep plugin that finds cross-domain script src Adding unittest for new plugin

---
+++ plugins/grep/cross_domain_js.py
@@ -0,0 +1,109 @@
+'''
+cross_domain_js.py
+
+Copyright 2006 Andres Riancho
+
+This file is part of w3af, w3af.sourceforge.net .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+'''
+from lxml import etree
+
+import core.data.kb.knowledgeBase as kb
+import core.data.kb.info as info
+
+from core.controllers.plugins.grep_plugin import GrepPlugin
+from core.data.bloomfilter.bloomfilter import scalable_bloomfilter
+from core.data.parsers.urlParser import url_object
+
+SCRIPT_SRC_XPATH = ".//script[@src]"
+
+
+class cross_domain_js(GrepPlugin):
+    '''
+    Find script tags with src attributes that point to a different domain. 
+      
+    @author: Andres Riancho (andres.riancho@gmail.com)
+    '''
+
+    def __init__(self):
+        GrepPlugin.__init__(self)
+        
+        # Internal variables
+        self._already_inspected = scalable_bloomfilter()
+        self._script_src_xpath = etree.XPath( SCRIPT_SRC_XPATH )
+
+    def grep(self, request, response):
+        '''
+        Plugin entry point, verify if the HTML has a form with file uploads.
+        
+        @parameter request: The HTTP request object.
+        @parameter response: The HTTP response object
+        @return: None
+        '''
+        url = response.getURL()
+
+        if response.is_text_or_html() and not url in self._already_inspected:
+
+            self._already_inspected.add(url)
+            dom = response.getDOM()
+
+            # In some strange cases, we fail to normalize the document
+            if dom is not None:
+                
+                # Loop through file inputs tags                
+                for script_src_tag in self._script_src_xpath( dom ):
+                    
+                    # This should be always False due to the XPATH we're using
+                    # but you never know...
+                    if not 'src' in script_src_tag.attrib:
+                        continue
+                    
+                    script_src = script_src_tag.attrib['src']
+                    script_full_url = response.getURL().urlJoin( script_src )
+                    script_domain = script_full_url.getDomain()
+                    
+                    if script_domain != response.getURL().getDomain():
+                        i = info.info()
+                        i.setPluginName(self.getName())
+                        i.setName('Cross-domain javascript source')
+                        i.setURL(url)
+                        i.setId(response.id)
+                        msg = 'The URL: "%s" has script tag with a source that points' \
+                              ' to a third party site ("%s"). This practice is not' \
+                              ' recommended as security of the current site is being' \
+                              ' delegated to that entity.'
+                        i.setDesc(msg)
+                        to_highlight = etree.tostring(script_src_tag)
+                        i.addToHighlight(to_highlight)
+                        kb.kb.append(self, 'cross_domain_js', i)
+
+    def end(self):
+        '''
+        This method is called when the plugin wont be used anymore.
+        '''
+        self.print_uniq( kb.kb.getData( 'cross_domain_js', 'cross_domain_js' ), 'URL' )
+    
+    def get_long_desc( self ):
+        '''
+        @return: A DETAILED description of the plugin functions and features.
+        '''
+        return '''
+        Find script tags with src attributes that point to a different domain.
+        
+        It is important to notice that websites that depend on external javascript
+        sources are delegating part of their security to those entities, so
+        it is imperative to be aware of such code.
+        '''
---
+++ plugins/tests/grep/test_cross_domain_js.py
@@ -0,0 +1,57 @@
+'''
+test_cross_domain_js.py
+
+Copyright 2012 Andres Riancho
+
+This file is part of w3af, w3af.sourceforge.net .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+'''
+from nose.plugins.attrib import attr
+
+from ..helper import PluginTest, PluginConfig
+
+
+@attr('smoke')
+class TestErrorPages(PluginTest):
+    
+    target_url = 'http://moth/w3af/grep/cross_domain_js/'
+    
+    _run_configs = {
+        'cfg': {
+            'target': target_url,
+            'plugins': {
+                'grep': (PluginConfig('cross_domain_js'),),
+                'crawl': (
+                          PluginConfig('web_spider',
+                                       ('onlyForward', True, PluginConfig.BOOL)),)
+            }
+        }
+    }
+    
+    def test_found_vuln(self):
+        cfg = self._run_configs['cfg']
+        self._scan(cfg['target'], cfg['plugins'])
+
+        infos = self.kb.getData('cross_domain_js', 'cross_domain_js')
+        self.assertEquals(3, len(infos))
+        
+        EXPECTED = set(['cross_domain_script_mixed.html',
+                        'cross_domain_script_with_type.html',
+                        'cross_domain_script.html'])
+        found_fnames = set([i.getURL().getFileName() for i in infos])
+        
+        self.assertEquals(EXPECTED,
+                          found_fnames)
+
