163d2d3832f42c2b359ecf39a3910f17aa7fe8a2
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-09-07 21:43:20 +0000

    Awful merge bug that made it to the svn server.

--- plugins/infrastructure/find_vhosts.py
+++ plugins/infrastructure/find_vhosts.py
@@ -182,26 +182,7 @@ class find_vhosts(InfrastructurePlugin):
                     # socket.gaierror: (-5, 'No address associated with hostname')
                     socket.gethostbyname( domain )
                 except:
-<<<<<<< .mine
-                    i = info.info()
-                    i.setPluginName(self.getName())
-                    i.setName('Internal hostname in HTML link')
-                    i.setURL( fuzzable_request.getURL() )
-                    i.setMethod( 'GET' )
-                    i.set_id( original_response.id )
-                    msg = 'The content of "'+ fuzzable_request.getURL() +'" references a non '
-                    msg += 'existant domain: "' + link + '". This may be a broken link, or an'
-                    msg += ' internal domain name.'
-                    i.setDesc( msg )
-                    kb.kb.append( self, 'find_vhosts', i )
-                    om.out.information( i.getDesc() )
-        
-        res = [ r for r in res if r != '']
-        
-        return res 
-=======
                     yield domain
->>>>>>> .r5785
     
     def _generic_vhosts( self, fuzzable_request ):
         '''
