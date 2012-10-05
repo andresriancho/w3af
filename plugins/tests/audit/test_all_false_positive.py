9dadfda060a5d3ec6f243af01d6f99253e63381a
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-10-05 22:41:31 +0000

    Cosmetic

--- plugins/tests/audit/test_all_false_positive.py
+++ plugins/tests/audit/test_all_false_positive.py
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
 from ..helper import PluginTest, PluginConfig
-from nose.plugins.skip import Skip, SkipTest
+from nose.plugins.skip import SkipTest
 
 
 class TestAllFP(PluginTest):
