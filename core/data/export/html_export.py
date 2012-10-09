f7eba27b197f3cb2f2ee227a572477c120ca6a00
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-10-09 18:43:47 +0000

    Moving in order to work on a diff workstation

--- core/data/export/html_export.py
+++ core/data/export/html_export.py
@@ -21,34 +21,32 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
-import urllib
-import cgi
-
-import core.data.parsers.urlParser as urlParser
 from core.data.parsers.httpRequestParser import httpRequestParser
+
 
 def html_export(request_string):
     '''
     @parameter request_string: The string of the request to export
     @return: A HTML that will perform the same HTTP request.
     '''
-    requestLines = request_string.split('\n\n')
-    header = requestLines[0]
-    body = '\n\n'.join(requestLines[1:])
-    httpRequest = httpRequestParser( header, body)
+    request_lines = request_string.split('\n\n')
+    header = request_lines[0]
+    body = '\n\n'.join(request_lines[1:])
+    http_request = httpRequestParser( header, body)
     res = '''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
     <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Exported HTTP Request from W3AF</title>
+        <title>Exported HTTP Request from w3af</title>
     </head>
-    <body>'''
-    res += '<form action="' + httpRequest.getURI() +'" method="' + httpRequest.get_method() + '">\n'
-    if httpRequest.getData() and httpRequest.getData() != '\n':
-        postData = httpRequest.getDc()
-        for i in postData:
-            res += '<label>' + i + '</label>\n'
-            res += '<input type="text" name="' + i.strip() + '" value="' + postData[i][0] + '">\n'
+    <body>\n'''
+    res += '<form action="' + http_request.getURI() +'" method="' + http_request.get_method() + '">\n'
+    if http_request.getData() and http_request.getData() != '\n':
+        post_data = http_request.getDc()
+        for param_name in post_data:
+            for value in post_data[param_name]:
+                res += '<label>' + param_name + '</label>\n'
+                res += '<input type="text" name="' + param_name.strip() + '" value="' + value + '">\n'
     res += '<input type="submit">\n'
     res += '</form>\n'
     res += '''</body>\n</html>'''
