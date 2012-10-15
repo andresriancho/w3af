e48a84aebf789b170d6d271650c7666681180c80
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-10-15 19:24:21 +0000

    Cosmetic.

--- core/data/kb/info.py
+++ core/data/kb/info.py
@@ -19,9 +19,9 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
+import core.data.constants.severity as severity
 
 from core.data.parsers.urlParser import url_object
-import core.data.constants.severity as severity
 
 
 class info(dict):
