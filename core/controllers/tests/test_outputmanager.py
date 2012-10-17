88bb5e3bd6123a5d61b94176bf3c87a4781d748e
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-10-17 11:47:17 +0000

    Migrated to mock module! Almost there :)

--- core/controllers/tests/test_outputmanager.py
+++ core/controllers/tests/test_outputmanager.py
@@ -19,7 +19,9 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
-from pymock import PyMockTestCase, method
+import unittest
+
+from mock import MagicMock, Mock
 from nose.plugins.attrib import attr
 
 import core.controllers.outputManager as om
@@ -27,13 +29,10 @@ import core.controllers.outputManager as om
 from core.controllers.w3afCore import w3afCore
 
 
-class TestOutputManager(PyMockTestCase):
+class TestOutputManager(unittest.TestCase):
     
     OUTPUT_PLUGIN_ACTIONS = ('debug', 'information', 'error',
                              'console', 'vulnerability')
-
-    def setUp(self):
-        PyMockTestCase.setUp(self)
 
     @attr('smoke')
     def test_output_plugins_actions(self):
@@ -42,45 +41,38 @@ class TestOutputManager(PyMockTestCase):
         msg = '<< SOME OUTPUT MESS@GE!! <<'
         
         for action in TestOutputManager.OUTPUT_PLUGIN_ACTIONS:
-            plugin = self.mock()
-            om.out._output_plugin_list = [plugin,]
-            om_action = getattr(om.out, action)
-            method(plugin, action).expects(msg, True) 
-            
-            ## Stop recording. Play!
-            self.replay()
+            plugin = Mock()
+            plugin_action = MagicMock()
+            setattr(plugin, action, plugin_action)
             
             # Invoke action
+            om.out._output_plugin_list = [plugin,]
+            om_action = getattr(om.out, action)
             om_action(msg, True)
+            
             om.out.process_all_messages()
             
-            # Verify and reset
-            self.verify()
-            self.reset()
-    
+            plugin_action.assert_called_once_with(msg, True)
+            
     @attr('smoke')
     def test_output_plugins_actions_with_unicode_message(self):
         '''Call all actions on output plugins using a unicode message'''
-        
         msg = u'<< ÑñçÇyruZZ!! <<'
         utf8_encoded_msg = msg.encode('utf8')
 
         for action in TestOutputManager.OUTPUT_PLUGIN_ACTIONS:
-            plugin = self.mock()
-            om.out._output_plugin_list = [plugin,]
-            om_action = getattr(om.out, action)
-            method(plugin, action).expects(utf8_encoded_msg, True) 
-            
-            ## Stop recording. Play!
-            self.replay()
+            plugin = Mock()
+            plugin_action = MagicMock()
+            setattr(plugin, action, plugin_action)
             
             # Invoke action
+            om.out._output_plugin_list = [plugin,]
+            om_action = getattr(om.out, action)
             om_action(msg, True)
+            
             om.out.process_all_messages()
             
-            # Verify and reset
-            self.verify()
-            self.reset()        
+            plugin_action.assert_called_once_with(msg, True)     
     
     @attr('smoke')
     def test_method_that_not_exists(self):
@@ -88,36 +80,31 @@ class TestOutputManager(PyMockTestCase):
         catch-all, just the ones I define!'''
         try:
             self.assertRaises(AttributeError, om.out.foobar, ('abc',))
-        except AttributeError:
-            pass
+        except AttributeError, ae:
+            self.assertTrue(True, ae)
 
     @attr('smoke')
     def test_kwds(self):
         '''The output manager implements __getattr__ with some added
         functools.partial magic. This verifies that it works well with kwds'''
-        
         msg = 'foo bar spam eggs'
         action = 'information'
-        
-        plugin = self.mock()
-        om.out._output_plugin_list = [plugin,]
-        om_action = getattr(om.out, action)
-        method(plugin, action).expects(msg, newLine=False) 
-        
-        ## Stop recording. Play!
-        self.replay()
+
+        plugin = Mock()
+        plugin_action = MagicMock()
+        setattr(plugin, action, plugin_action)
         
         # Invoke action
-        om_action(msg, newLine=False)
+        om.out._output_plugin_list = [plugin,]
+        om_action = getattr(om.out, action)
+        om_action(msg, False)
+        
         om.out.process_all_messages()
         
-        # Verify and reset
-        self.verify()
-        self.reset()
-    
+        plugin_action.assert_called_once_with(msg, False)
+            
     @attr('smoke')
     def test_error_handling(self):
-        
         class InvalidPlugin(object):
             def information(self, msg, newLine=True):
                 raise Exception('Test')
@@ -142,5 +129,4 @@ class TestOutputManager(PyMockTestCase):
         edata = exc_list[0]
         self.assertEqual( str(edata.exception) , 'Test' )
         
-        self.reset()
         
