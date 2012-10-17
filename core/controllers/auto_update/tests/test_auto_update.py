596ae368f7d16fcecc9f22f8a3790dfdfec2d38a
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-10-17 11:30:48 +0000

    Incomplete migration to mock

--- core/controllers/auto_update/tests/test_auto_update.py
+++ core/controllers/auto_update/tests/test_auto_update.py
@@ -18,38 +18,32 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
-
+import datetime
 import pysvn
-import sys
+import unittest
+import os
 
+from mock import MagicMock, Mock, patch
+from pysvn import wc_notify_action as wcna
+from pysvn import Revision
+from pysvn import wc_status_kind as wcsk
 from collections import namedtuple
+from nose.plugins.skip import SkipTest
 
-from pymock import PyMockTestCase, method, override, dontcare, set_count
+import core.controllers.auto_update.auto_update as autoupdmod
 
-from ..auto_update import (
+from core.controllers.auto_update.auto_update import (
     w3afSVNClient, Revision, VersionMgr, SVNFilesList, StartUpConfig,
     FILE_UPD, FILE_NEW, FILE_DEL, ST_CONFLICT, ST_MODIFIED, ST_UNKNOWN,
     W3AF_LOCAL_PATH, get_svnversion, SVNError, SVNUpdateError
     )
-
-# Remove magic method as it generates some conficts with pymock
-del w3afSVNClient.__getattribute__
-del VersionMgr.__getattribute__
 
 REPO_URL = 'http://localhost/svn/w3af'
 LOCAL_PATH = '/home/user/w3af'
 INF = pysvn.depth.infinity
 
 
-def dummy(*args, **kwargs):
-    pass
-
-def get_autoupdate_os_module():
-    from ..auto_update import os
-    return os
-    
-
-class Testw3afSVNClient(PyMockTestCase):
+class Testw3afSVNClient(unittest.TestCase):
     
     rev = Revision(112, None)
     upd_files = SVNFilesList(
@@ -60,12 +54,18 @@ class Testw3afSVNClient(PyMockTestCase):
         )
 
     def setUp(self):
-        PyMockTestCase.setUp(self)
+        w3afSVNClient._get_repourl = MagicMock(return_value=None)
         
-        w3afSVNClient._get_repourl = dummy
         self.client = w3afSVNClient(LOCAL_PATH)
         self.client._repourl = REPO_URL
-        self.client._svnclient = self.mock()
+        self.client._svnclient = Mock()
+        
+        self.pysvn_client_patcher_side_effect = None
+        self.pysvn_client_patcher = patch('pysvn.Client', side_effect=self.pysvn_client_patcher_side_effect)
+        self.pysvn_client_mock = self.pysvn_client_patcher.start()
+    
+    def tearDown(self):
+        self.pysvn_client_patcher.stop()
     
     def test_has_repourl(self):
         self.assertTrue(self.client._repourl is not None)
@@ -78,39 +78,24 @@ class Testw3afSVNClient(PyMockTestCase):
 
     def test_upd(self):
         client = self.client
-        override(pysvn, 'Revision', self.mock())
-        pysvnhead = pysvn.Revision(pysvn.opt_revision_kind.head)
-        method(client._svnclient, 'update').expects(
-                                                LOCAL_PATH,
-                                                revision=pysvnhead,
-                                                depth=INF
-                                                ).returns([self.rev])
-        override(client, '_filter_files').expects(
-                                    client.UPD_ACTIONS).returns(self.upd_files)
         
-        ## Stop recording. Play!
-        self.replay()
+        pysvnhead = pysvn.Revision(pysvn.opt_revision_kind.head)
+        
+        client._svnclient.update = MagicMock(return_value=self.rev)
+        client._filter_files = MagicMock(return_value=self.upd_files)
+        
         self.assertEquals(self.upd_files, client.update(rev=None))
+        client._svnclient.update.assert_called_once_with(LOCAL_PATH, revision=pysvnhead, depth=INF)
+        client._filter_files.assert_called_once_with(client.UPD_ACTIONS)
         
-        ## Verify ##
-        self.verify()
-
     def test_upd_fail(self):
-        override(pysvn, 'Revision', self.mock())
         pysvnhead = pysvn.Revision(pysvn.opt_revision_kind.head)
+        
         client = self.client
-        method(client._svnclient, 'update').expects(
-                                    LOCAL_PATH,
-                                    revision=pysvnhead,
-                                    depth=INF
-                                    ).raises(pysvn.ClientError('file locked'))
-        
-        ## Stop recording. Play!
-        self.replay()
+        client._svnclient.update = MagicMock(side_effect=pysvn.ClientError('file locked'))
+ 
         self.assertRaises(SVNUpdateError, client.update)
-        
-        ## Verify ##
-        self.verify()
+        client._svnclient.update.assert_called_once_with(LOCAL_PATH, revision=pysvnhead, depth=INF)
         
     def test_upd_conflict(self):
         '''
@@ -124,18 +109,14 @@ class Testw3afSVNClient(PyMockTestCase):
         pass
 
     def test_filter_files(self):
-        from pysvn import wc_notify_action as wcna
-        from pysvn import Revision
-        os = get_autoupdate_os_module()
         client = self.client
-        override(os.path, 'isdir').expects(dontcare()).returns(False)
-        set_count(exactly=2)
-        ## Stop recording. Play!
-        self.replay()
+        os.path.isdir = MagicMock(return_value=False)
+        
         # Call client's callback function several times
         f1 = '/path/to/file/foo.py'
         ev = {'action': wcna.update_delete,
-               'error': None, 'mime_type': None, 'path': f1,
+               'error': None, 'mime_type': None,
+               'path': f1,
                'revision': Revision(pysvn.opt_revision_kind.number, 11)}
         client._register(ev)
 
@@ -149,70 +130,47 @@ class Testw3afSVNClient(PyMockTestCase):
         expected_res = SVNFilesList([(f1, FILE_DEL), (f2, FILE_UPD)])
         self.assertEquals(expected_res, 
             client._filter_files(filterbyactions=w3afSVNClient.UPD_ACTIONS))
-        ## Verify ##
-        self.verify()
 
     def test_status(self):
-        from pysvn import wc_status_kind as wcsk
-        client = self.client
         # Mock pysvnstatus objects
-        smock = self.mock()
-        smock.path
-        self.setReturn('/some/path/foo')
-        smock.text_status
-        self.setReturn(wcsk.modified)
+        smock1 = Mock()
+        smock1.path = '/some/path/foo'
+        smock1.text_status = wcsk.modified
         
-        smock2 = self.mock()
-        smock2.path
-        self.setReturn('/some/path/foo2')
-        smock2.text_status
-        self.setReturn(wcsk.conflicted)
+        smock2 = Mock()
+        smock2.path = '/some/path/foo2'
+        smock2.text_status = wcsk.conflicted
         
-        smock3 = self.mock()
-        smock3.path
-        self.setReturn('/some/path/foo3')
-        smock3.text_status
-        self.setReturn('some_weird_status')
+        smock3 = Mock()
+        smock3.path = '/some/path/foo3'
+        smock3.text_status = 'some_weird_status'
         
-        status_files = [smock, smock2, smock3]
-        method(client._svnclient, 'status').expects(LOCAL_PATH, recurse=False)
-        self.returns(status_files)
-        ## Stop recording - Replay ##
-        self.replay()
-        expected_res = \
-            SVNFilesList([
-                ('/some/path/foo', ST_MODIFIED),
-                ('/some/path/foo2', ST_CONFLICT),
-                ('/some/path/foo3', ST_UNKNOWN)])
+        status_files = [smock1, smock2, smock3]
+        client = w3afSVNClient(LOCAL_PATH)
+        client._svnclient.status = MagicMock(return_value=status_files)
+        
+        expected_res = SVNFilesList([('/some/path/foo', ST_MODIFIED),
+                                     ('/some/path/foo2', ST_CONFLICT),
+                                     ('/some/path/foo3', ST_UNKNOWN)])
         self.assertEquals(expected_res, client.status())
-        ## Verify ##
-        self.verify()
     
-    def test_working_copy(self):
-
-        cli = self.mock()
-        override(pysvn, 'Client').expects().returns(cli)
-        
-        # NOT A WORKING COPY
-        method(cli, 'status').expects(LOCAL_PATH, recurse=False).raises(Exception())
-        self.replay()
-        self.assertFalse(w3afSVNClient.is_working_copy(LOCAL_PATH))
-        
-        # IS A WORKING COPY
-        self.reset()
-        cli = self.mock()
-        override(pysvn, 'Client').expects().returns(cli)
-        method(cli, 'status').expects(LOCAL_PATH, recurse=False).returns(1)
-        self.replay()
+    def test_not_working_copy(self):
+        cli = Mock()
+        cli.status = MagicMock(side_effect=Exception())
+        self.pysvn_client_patcher_side_effect = [cli,]
+        self.assertFalse( w3afSVNClient.is_working_copy(LOCAL_PATH) )
+    
+    def test_a_working_copy(self):
+        cli = Mock()
+        cli.status = MagicMock()
+        self.pysvn_client_patcher_side_effect = [cli,]
         self.assertTrue(w3afSVNClient.is_working_copy(LOCAL_PATH))
 
     def test_commit(self):
         pass
 
 
-from nose.plugins.skip import Skip, SkipTest
-
-class TestVersionMgr(PyMockTestCase):
+class TestVersionMgr(unittest.TestCase):
     
     def setUp(self):
         '''
@@ -226,88 +184,73 @@ class TestVersionMgr(PyMockTestCase):
         I haven't been able to fix this issue... so I'm skipping these two
         tests!
         '''
-        raise SkipTest('FIXME: See above comment.')
-        
-        PyMockTestCase.setUp(self)
-        
         # Override auto_update module variable
-        import core.controllers.auto_update.auto_update as autoupdmod
-        autoupdmod.SVNClientClass = self.mock()
-        self.vmgr = VersionMgr(LOCAL_PATH, dummy)
+        autoupdmod.SVNClientClass = Mock()
+        self.vmgr = VersionMgr(LOCAL_PATH, MagicMock(return_value=None))
     
-    def test_has_to_update(self):
-        
+    def test_no_need_update(self):
         vmgr = self.vmgr
-        start_cfg_mock = self.mock()
-        vmgr._start_cfg = start_cfg_mock
+        vmgr._start_cfg = StartUpConfig()
+        vmgr._start_cfg._autoupd = False
         
         # Test no auto-update
-        start_cfg_mock.auto_upd
-        self.setReturn(False)
-        self.replay()
         self.assertFalse(vmgr._has_to_update())
-
-        # Test [D]aily, [W]eekly and [M]onthly auto-update
-        import datetime
+    
+    def test_has_to_update(self):
+        '''
+        Test [D]aily, [W]eekly and [M]onthly auto-update
+        '''
         SC = StartUpConfig        
+        vmgr = self.vmgr
+        
         for freq, diffdays in ((SC.FREQ_DAILY, 1), (SC.FREQ_WEEKLY, 8), \
                                (SC.FREQ_MONTHLY, 34)):
-            self.reset()
-            start_cfg_mock.auto_upd
-            self.setReturn(True)
-            start_cfg_mock.freq
-            self.setReturn(freq)
-            start_cfg_mock.last_upd
-            self.setReturn(datetime.date.today() - datetime.timedelta(days=diffdays))
-            self.replay()
+            
+            vmgr._start_cfg = start_cfg = StartUpConfig()
+            start_cfg._autoupd = True
+            start_cfg._freq = freq
+            
+            last_upd = datetime.date.today() - datetime.timedelta(days=diffdays)
+            start_cfg._lastupd = last_upd
+            
             self.assertTrue(vmgr._has_to_update())
     
     def test_added_new_dependencies(self):
         pass
 
 
-class TestSVNVersion(PyMockTestCase):
+class TestSVNVersion(unittest.TestCase):
     
     Rev = namedtuple('Rev', ('number',))
     
     def setUp(self):
-        PyMockTestCase.setUp(self)
-        from ..auto_update import pysvn
-        self.cli = self.mock()
-        override(pysvn, 'Client').expects().returns(self.cli)
+        self.cli = Mock()
+        pysvn.Client = MagicMock(return_value=self.cli)
     
     def test_get_svnversion_with_non_svn_path(self):
-        os = get_autoupdate_os_module()
-        override(os, 'walk').expects(dontcare()).generates(
-                                                    *([('x', 'y', 'z')] * 3)
-                                                    )
+        def side_effect(path):
+            for fake_data in [('x', 'y', 'z'),
+                              ('x', 'y', 'z'),
+                              ('x', 'y', 'z')]:
+                yield fake_data
+        os.walk = MagicMock(side_effect=side_effect)
         
         cli = self.cli
         Rev = TestSVNVersion.Rev
-        method(cli, 'info').expects(dontcare()).returns({'revision': Rev(22)})
-        method(cli, 'info').expects(dontcare()).returns({'revision': Rev(23)})
-        # If at least a 2-level depth non svn subdirectory is
-        # found the pysvn client raises an exception
-        method(cli, 'info').expects(dontcare()).raises(pysvn.ClientError)
+        cli.info = MagicMock(side_effect=[{'revision': Rev(22)},
+                                          {'revision': Rev(23)},
+                                          pysvn.ClientError()])
         
-        ## Stop recording - Replay ##
-        self.replay()
         self.assertEquals('22:23', get_svnversion(W3AF_LOCAL_PATH))
-        
-        ## Verify ##
-        self.verify()
     
     def test_non_svn_install(self):
         '''
         Ensure that SVNError is raised when `get_svnversion` is called
         in a non svn copy.
         '''
-        os = get_autoupdate_os_module()
-        override(os, 'walk').expects(dontcare()).returns(())
-        self.replay()
-        self.assertRaises(SVNError, get_svnversion, (W3AF_LOCAL_PATH,))
-## TODO: Uncomment next lines when py >= 2.7 and delete the previous one ##        
-##        with self.assertRaises(SVNError) as cm:
-##            get_svnversion(W3AF_LOCAL_PATH)
-##        self.assertTrue("is not a svn working copy" in cm.exception.message)
+        os.walk = MagicMock(return_value=[])
+        with self.assertRaises(SVNError) as cm:
+            get_svnversion(W3AF_LOCAL_PATH)
+        self.assertTrue("is not a svn working copy" in cm.exception.message)
+        
     
