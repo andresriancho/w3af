bde72b25a700491db28692e94bd278ea223a06de
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-10-26 20:47:56 +0000

    Removing unused import

--- core/controllers/dependency_check/dependency_check.py
+++ core/controllers/dependency_check/dependency_check.py
@@ -22,7 +22,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 import sys
 import platform
 import warnings
-import logging
 
 import core.controllers.outputManager as om
 
