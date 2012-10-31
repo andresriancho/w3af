07537fb2a8a9319114447c051f104ad3277d00b2
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-10-31 15:02:31 +0000

    Cosmetic

--- core/ui/console/tests/test_accept_disclaimer.py
+++ core/ui/console/tests/test_accept_disclaimer.py
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 import unittest
 
-from mock import patch, Mock, PropertyMock
+from mock import patch, Mock
 
 from core.ui.console.console_ui import ConsoleUI
 
