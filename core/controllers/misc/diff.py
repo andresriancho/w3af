2936db8385fee6c0453ccbf0e008f442e5a0702e
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-11-07 12:14:09 +0000

    Cosmetic

--- core/controllers/misc/diff.py
+++ core/controllers/misc/diff.py
@@ -19,7 +19,6 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
-
 import difflib
 
 
@@ -57,4 +56,3 @@ def diff(a, b):
         removed_b += size
     
     return a, b
-         
