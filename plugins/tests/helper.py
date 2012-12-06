0a2f04e03ca1a452d1592b600ed024ea60a8160e
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-12-06 01:46:50 +0000

    PEP8

--- plugins/tests/helper.py
+++ plugins/tests/helper.py
@@ -109,10 +109,10 @@ class PluginTest(unittest.TestCase):
 
     def tearDown(self):
         #
-        # I want to make sure that we don't have *any hidden* exceptions in our tests.
+        # I want to make sure that we don't have *any hidden* exceptions in our
+        # tests.
         #
-        caught_exceptions = self.w3afcore.exception_handler.get_all_exceptions(
-        )
+        caught_exceptions = self.w3afcore.exception_handler.get_all_exceptions()
         msg = [e.get_summary() for e in caught_exceptions]
         self.assertEqual(len(caught_exceptions), 0, msg)
 
