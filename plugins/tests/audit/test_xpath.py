96021e74699c0de3ebdce69976118354e5b3d3d1
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2012-12-13 15:25:46 +0000

    Cosmetic while checking why it was failing.

--- plugins/tests/audit/test_xpath.py
+++ plugins/tests/audit/test_xpath.py
@@ -48,8 +48,11 @@ class TestXPATH(PluginTest):
         # Assert the general results
         expected_vuln_number = 4
         vulns = self.kb.get('xpath', 'xpath')
-        self.assertEquals(expected_vuln_number, len(vulns))
-        self.assertEquals(all(["XPATH injection vulnerability" == vuln.get_name() for vuln in vulns]), True)
+        self.assertEquals(expected_vuln_number, len(vulns), vulns)
+        
+        vtitle = "XPATH injection vulnerability"
+        all_titles = all([vtitle == vuln.get_name() for vuln in vulns])
+        self.assertTrue(all_titles, vulns)
 
         # Verify the specifics about the vulnerabilities
         expected = [
