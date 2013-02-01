c35bffae5300f5fb38cf45283be34d2d2992c62f
Andres Riancho <andres.riancho@gmail.com>
2013-02-01 12:20:11 -0300

    Refactoring of w3afcore tests, created core directory and splitted them into different files Also, inherit from CountTestMixin instead of TestW3afCorePause to reduce test time.

--- core/controllers/tests/core/test_pause_stop.py
+++ core/controllers/tests/core/test_pause_stop.py
@@ -1,5 +1,5 @@
 '''
-test_w3afcore.py
+test_pause_stop.py
 
 Copyright 2011 Andres Riancho
 
@@ -18,38 +18,21 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
+import unittest
 import os
 import shutil
 import time
-import unittest
 import threading
 import pprint
 
 from multiprocessing.dummy import Process
-from nose.plugins.attrib import attr
-from mock import MagicMock, patch, call
-
-import core.controllers.output_manager as om
+from mock import MagicMock
 
 from core.controllers.w3afCore import w3afCore
-from core.controllers.exceptions import (w3afMustStopException,
-                                         w3afMustStopByUnknownReasonExc,
-                                         w3afMustStopByUserRequest)
 from plugins.tests.helper import create_target_option_list
 
 
-@attr('smoke')
-class TestW3afCore(unittest.TestCase):
-
-    def test_multiple_instances(self):
-        '''Just making sure nothing crashes if I have more than 1 instance
-        of w3afCore'''
-        instances = []
-        for _ in xrange(5):
-            instances.append(w3afCore())
-
-class TestW3afCorePause(unittest.TestCase):
-
+class CountTestMixin(unittest.TestCase):
     def setUp(self):
         '''
         This is a rather complex setUp since I need to move the count.py
@@ -83,6 +66,8 @@ class TestW3afCorePause(unittest.TestCase):
         for fname in (self.dst, self.dst + 'c'):
             if os.path.exists(fname):
                 os.remove(fname)
+
+class TestW3afCorePause(CountTestMixin):
                 
     def test_pause_unpause(self):
         '''
@@ -184,92 +169,6 @@ class TestW3afCorePause(unittest.TestCase):
         self.w3afcore.status.set_current_fuzzable_request = mock_call
         
         self.w3afcore.start()
-
-class TestExceptionHandler(TestW3afCorePause):
-    '''
-    Inherit from TestW3afCorePause to get the nice setUp().
-    '''
-    def test_same_id(self):
-        '''
-        Verify that the exception handler is the same before and after the scan
-        '''
-        before_id_ehandler = id(self.w3afcore.exception_handler)
-        
-        self.w3afcore.start()
-        
-        after_id_ehandler = id(self.w3afcore.exception_handler)
-        
-        self.assertEqual(before_id_ehandler, after_id_ehandler)
-        
-
-class TestCoreExceptions(unittest.TestCase):
-
-    def setUp(self):
-        '''
-        This is a rather complex setUp since I need to move the
-        exception_raise.py plugin to the plugin directory in order to be able
-        to run it afterwards.
-
-        In the tearDown method, I'll remove the file.
-        '''
-        self.src = os.path.join('core', 'controllers', 'tests', 'exception_raise.py')
-        self.dst = os.path.join('plugins', 'crawl', 'exception_raise.py')
-        shutil.copy(self.src, self.dst)
-
-        self.w3afcore = w3afCore()
-        
-        target_opts = create_target_option_list('http://moth/')
-        self.w3afcore.target.set_options(target_opts)
-
-        self.w3afcore.plugins.set_plugins(['exception_raise',], 'crawl')
-
-        # Verify env and start the scan
-        self.w3afcore.plugins.init_plugins()
-        self.w3afcore.verify_environment()
-        
-        self.exception_plugin = self.w3afcore.plugins.plugins['crawl'][0]
-        
-    
-    def tearDown(self):
-        self.w3afcore.quit()
-        
-        # py and pyc file
-        for fname in (self.dst, self.dst + 'c'):
-            if os.path.exists(fname):
-                os.remove(fname)
-                
-    def test_stop_on_must_stop_exception(self):
-        '''
-        Verify that the w3afMustStopException stops the scan.
-        '''
-        self.exception_plugin.exception_to_raise = w3afMustStopException
-        
-        with patch('core.controllers.w3afCore.om.out') as om_mock:
-            self.w3afcore.start()
-            
-            error = "\n**IMPORTANT** The following error was detected by w3af"\
-                    " and couldn't be resolved:\nTest exception.\n"
-            self.assertIn(call.error(error), om_mock.mock_calls)
-
-    def test_stop_unknown_exception(self):
-        '''
-        Verify that the w3afMustStopByUnknownReasonExc stops the scan.
-        '''
-        self.exception_plugin.exception_to_raise = w3afMustStopByUnknownReasonExc
-        self.assertRaises(w3afMustStopByUnknownReasonExc, self.w3afcore.start)
-                
-    def test_stop_by_user_request(self):
-        '''
-        Verify that the w3afMustStopByUserRequest stops the scan.
-        '''
-        self.exception_plugin.exception_to_raise = w3afMustStopByUserRequest
-        
-        with patch('core.controllers.w3afCore.om.out') as om_mock:
-            self.w3afcore.start()
-            
-            message = 'Test exception.'
-            self.assertIn(call.information(message), om_mock.mock_calls)
-
 
 def nice_repr(alive_threads):
     repr_alive = [repr(x) for x in alive_threads][:20]
