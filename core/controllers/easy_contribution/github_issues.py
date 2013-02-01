b0029d58b625d069d36ad08fc7085c5b7c3f42d2
Andres Riancho <andres.riancho@gmail.com>
2013-02-01 17:36:23 -0300

    Adding missing imports

--- core/controllers/easy_contribution/github_issues.py
+++ core/controllers/easy_contribution/github_issues.py
@@ -20,6 +20,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
 import string
+import hashlib
+import time
 
 from github import Github
 from github import GithubException
