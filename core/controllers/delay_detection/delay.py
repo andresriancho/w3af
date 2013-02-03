e1d4082a0465ccab23f7a3e91fadfab53d88ce89
Andres Riancho <andres.riancho@gmail.com>
2013-02-03 18:07:29 -0300

    * Minor refactoring for delay detection (send less requests during setup phase) * Adding (still failing) unittests
    This is just a preparation for #41 which is the redos rewrite and unittest

--- core/controllers/delay_detection/delay.py
+++ core/controllers/delay_detection/delay.py
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
 
-class delay(object):
+class Delay(object):
     '''
     A simple representation of a delay string like "sleep(%s)"
     '''
