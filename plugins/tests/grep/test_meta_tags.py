8d83dc5657edcc60859ded5c9cd2fefc2f058d65
Andres Riancho <andres.riancho@gmail.com>
2013-02-04 11:02:58 -0300

    Cosmetic

--- plugins/tests/grep/test_meta_tags.py
+++ plugins/tests/grep/test_meta_tags.py
@@ -18,7 +18,6 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
-
 from plugins.tests.helper import PluginTest, PluginConfig
 import core.data.constants.severity as severity
 
