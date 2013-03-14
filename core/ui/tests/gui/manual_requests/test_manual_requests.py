7ab9943f99cb1498e43de5012018f9b6caa4b8ed
Andres Riancho <andres.riancho@gmail.com>
2013-03-14 15:21:39 -0300

    More progress for #18: "Send GET request to a local web server, assert at the web server level that the information is correct"

--- core/ui/tests/gui/manual_requests/test_manual_requests.py
+++ core/ui/tests/gui/manual_requests/test_manual_requests.py
@@ -22,13 +22,30 @@ import os
 
 from core.ui.tests.wrappers.xpresser_unittest import XpresserUnittest
 
+from core.data.url.tests.helpers.http_daemon import HTTPDaemon
+from core.data.parsers.HTTPRequestParser import HTTPRequestParser
+
+# TODO: Code duplication
+#
+# Tried to do this the right way, by adding the import at the beginning
+# of the file:
+#
+#from core.ui.gui.tools.manual_requests import MANUAL_REQUEST_EXAMPLE
+#
+# But it fails because of the pygtk vs. gi stuff... 
+MANUAL_REQUEST_EXAMPLE = """\
+GET http://localhost/script.php HTTP/1.0
+Host: www.some_host.com
+User-Agent: w3af.org
+Pragma: no-cache
+Content-Type: application/x-www-form-urlencoded
+"""
+
 
 class TestManualRequests(XpresserUnittest):
     
     IMAGES = os.path.join('core', 'ui', 'tests', 'gui', 'manual_requests', 'images')
     EXTRA_IMAGES = os.path.join('core', 'ui', 'tests', 'gui', 'tools_menu', 'images')
-    
-    TEST_PORT = 8081
     
     def setUp(self):
         XpresserUnittest.setUp(self)
@@ -47,4 +64,41 @@ class TestManualRequests(XpresserUnittest):
         
         # Close the error dialog
         self.type(['<Enter>',], False)
-    
+
+    def test_GET_request(self):
+        self.http_daemon = HTTPDaemon()
+        self.http_daemon.start()
+        self.http_daemon.wait_for_start()
+        
+        #
+        #    Send the request to our server using the GUI
+        #
+        self.double_click('localhost')
+        self.type('127.0.0.1:%s' % self.http_daemon.get_port(), False)
+        
+        self.click('send')
+        
+        # Wait until we actually get the response, and verify we got the
+        # response body we expected:
+        self.find('abcdef')
+        self.find('200_OK')
+        
+        #
+        #    Assert that it's what we really expected
+        #
+        requests = self.http_daemon.requests
+        self.assertEqual(len(requests), 1)
+        
+        request = requests[0]
+
+        head, postdata = MANUAL_REQUEST_EXAMPLE, ''
+        http_request = HTTPRequestParser(head, postdata)
+        
+        self.assertEqual(http_request.get_url().get_path(), request.path)
+        self.assertEqual(http_request.get_method(), request.command)
+        
+        for header_name, header_value in http_request.get_headers().iteritems():
+            self.assertIn(header_name.lower(), request.headers)
+            self.assertEqual(header_value, request.headers[header_name.lower()])
+            
+        self.http_daemon.server.shutdown()
