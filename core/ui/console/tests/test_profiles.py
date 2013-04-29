cb1149931a664c4850b9fdebf5d58372c8f22480
Andres Riancho <andres.riancho@gmail.com>
2013-04-29 14:00:37 -0300

    * Closes #291 * Added save_as for profiles in the console * Unittested * Added help for save_as in console, removed assert from help.xml

--- core/ui/console/tests/test_profiles.py
+++ core/ui/console/tests/test_profiles.py
@@ -22,6 +22,7 @@ from nose.plugins.attrib import attr
 
 from core.ui.console.console_ui import ConsoleUI
 from core.ui.console.tests.helper import ConsoleTestHelper
+from core.data.profile.profile import profile
 
 
 @attr('smoke')
@@ -29,7 +30,27 @@ class TestProfilesConsoleUI(ConsoleTestHelper):
     '''
     Load profiles from the console UI.
     '''
-
+    def setUp(self):
+        super(TestProfilesConsoleUI, self).setUp()
+        self._remove_if_exists('unittest')
+    
+    def tearDown(self):
+        super(TestProfilesConsoleUI, self).tearDown()
+        self._remove_if_exists('unittest')
+    
+    def _remove_if_exists(self, profile_name):
+        try:
+            profile_inst = profile(profile_name)
+            profile_inst.remove()
+        except:
+            pass
+    
+    def _assert_exists(self, profile_name):
+        try:
+            profile(profile_name)
+        except:
+            assert False, 'The %s profile does NOT exist!' % profile_name
+        
     def test_load_profile_exists(self):
         commands_to_run = ['profiles',
                            'help',
@@ -59,4 +80,54 @@ class TestProfilesConsoleUI(ConsoleTestHelper):
         self.console.sh()
 
         assert_result, msg = self.startswith_expected_in_output(expected)
+        self.assertTrue(assert_result, msg)
+
+    def test_save_as_profile(self):
+        commands_to_run = ['profiles',
+                           'use OWASP_TOP10',
+                           'save_as unittest',
+                           'exit']
+
+        expected = ('Profile saved.',)
+
+        self.console = ConsoleUI(commands=commands_to_run, do_upd=False)
+        self.console.sh()
+
+        assert_result, msg = self.startswith_expected_in_output(expected)
+        self.assertTrue(assert_result, msg)
+        
+        self._assert_exists('unittest')
+
+    def test_save_load_misc_settings(self):
+        # Save the settings
+        commands_to_run = ['misc-settings set msf_location /etc/',
+                           'profiles save_as unittest',
+                           'exit']
+
+        expected = ('Profile saved.',)
+
+        self.console = ConsoleUI(commands=commands_to_run, do_upd=False)
+        self.console.sh()
+
+        assert_result, msg = self.startswith_expected_in_output(expected)
+        self.assertTrue(assert_result, msg)
+        
+        self._assert_exists('unittest')
+        
+        # Clean the mocked stdout
+        self._mock_stdout.clear()
+        
+        # Load the settings
+        commands_to_run = ['profiles',
+                           'use unittest',
+                           'back',
+                           'misc-settings view',
+                           'exit']
+
+        expected = ('/etc/',)
+
+        self.console = ConsoleUI(commands=commands_to_run, do_upd=False)
+        self.console.sh()
+
+        assert_result, msg = self.all_expected_substring_in_output(expected)
         self.assertTrue(assert_result, msg)
