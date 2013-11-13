e4e67b0de3e3fd65e9762e2bd8b2edba73273a57
Andres Riancho <andres.riancho@gmail.com>
2013-11-13 10:13:03 +0900

    We want to be able to easily change our test runner, write for loops, etc.

---
+++ w3af/core/controllers/ci/nosetests_wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+# The order in which these are run doesn't really matter, but I do need to
+# take care of "grouping" (which directory is run) because of an incompatibility
+# between "w3af/core/ui/gui/" and "w3af/core/ui/tests/" which comes from
+# Gtk2 vs. Gtk3.
+
+$PARAMS = '--with-doctest --doctest-tests -A'
+$SELECTORS = 'smoke and not internet and not moth and not root'
+
+nosetests $PARAMS -A "$SELECTORS" w3af/core/controllers/
+nosetests $PARAMS -A "$SELECTORS" w3af/core/data/
+nosetests $PARAMS -A "$SELECTORS" w3af/core/ui/tests/
+nosetests $PARAMS -A "$SELECTORS" w3af/core/ui/console/
+nosetests $PARAMS -A "$SELECTORS" w3af/core/ui/gui/
+nosetests $PARAMS -A "$SELECTORS" w3af/plugins/
+        
+# TODO: Run the tests which require moth
