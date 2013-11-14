10c39a003bee671c3b9fa05a8532ab9e40dbcd47
Andres Riancho <andres.riancho@gmail.com>
2013-11-14 18:50:11 +0900

    Adding coverage information and sending to coveralls

--- w3af/core/controllers/ci/nosetests_wrapper.py
+++ w3af/core/controllers/ci/nosetests_wrapper.py
@@ -11,7 +11,7 @@ from concurrent import futures
 
 MAX_WORKERS = multiprocessing.cpu_count()
 NOSETESTS = 'nosetests'
-NOSE_PARAMS = '--with-yanc --with-doctest --doctest-tests'
+NOSE_PARAMS = '--with-yanc --with-doctest --doctest-tests --with-cov --cov-report=xml'
 SELECTORS = ["smoke and not internet and not moth and not root",]
 TEST_DIRECTORIES = [
     # The order in which these are run doesn't really matter, but I do need to
@@ -86,8 +86,7 @@ def summarize_exit_codes(exit_codes):
 
 def print_info_console(cmd, stdout, stderr, exit_code):
     print colored(cmd, 'green')
-    print '=' * len(cmd)
-
+    
     print clean_noise(stdout)
     print clean_noise(stderr)
 
