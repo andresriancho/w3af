58baca74be3bf8c4eb51e4b5b16e94c2093d858a
Andres Riancho <andres.riancho@gmail.com>
2013-11-24 19:24:00 -0300

    Starting to believe that this is a circleci bug...

--- w3af/core/controllers/ci/nosetests_wrapper.py
+++ w3af/core/controllers/ci/nosetests_wrapper.py
@@ -17,7 +17,7 @@ from fabric.colors import red, yellow, green
 ARTIFACT_DIR = os.environ.get('CIRCLE_ARTIFACTS', '/tmp/')
 LOG_FILE = os.path.join(ARTIFACT_DIR, 'nosetests.log')
 
-MAX_WORKERS = multiprocessing.cpu_count()
+MAX_WORKERS = 1 #multiprocessing.cpu_count()
 NOSETESTS = 'nosetests'
 NOSE_PARAMS = '--with-yanc --with-doctest --doctest-tests --with-cov --cov-report=xml'
 
