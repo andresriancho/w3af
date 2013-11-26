8e976d76e1350acbaaf216086e1f51a5fd0a5f8a
Andres Riancho <andres.riancho@gmail.com>
2013-11-26 11:17:58 -0300

    Better tests with DELTA for result length

--- w3af/core/data/search_engines/tests/test_bing.py
+++ w3af/core/data/search_engines/tests/test_bing.py
@@ -30,6 +30,8 @@ from w3af.core.data.url.extended_urllib import ExtendedUrllib
 @attr('internet')
 class test_bing(unittest.TestCase):
 
+    DELTA = 10
+
     def setUp(self):
         self.bing_se = bing(ExtendedUrllib())
 
@@ -45,12 +47,12 @@ class test_bing(unittest.TestCase):
         results = self.bing_se.get_n_results(self.query, self.limit)
         
         # Len of results must be le. than limit
-        self.assertLessEqual(len(results), self.limit)
+        self.assertGreaterEqual(len(results), self.limit)
+        self.assertLessEqual(len(results) + self.DELTA, self.limit)
 
-        # I want to get some results...
-        self.assertTrue(len(results) >= 10, results)
-        self.assertTrue(
-            len(set([r.URL.get_domain() for r in results])) >= 3, results)
+        # I want to get real results
+        domains = set([r.URL.get_domain() for r in results])
+        self.assertGreater(len(domains), 3, results)
 
         # URLs should be unique
         urls = [r.URL for r in results]
