7e860d0ebc4fec17d6844f00892cd1700293dc0f
Andres Riancho <andres.riancho@gmail.com>
2013-12-03 14:30:33 -0300

    Print unique items

--- w3af/core/controllers/ci/nosetests_wrapper/utils/output.py
+++ w3af/core/controllers/ci/nosetests_wrapper/utils/output.py
@@ -2,9 +2,28 @@ from __future__ import print_function
 
 import logging
 
+from itertools import ifilterfalse
+
 from w3af.core.controllers.ci.nosetests_wrapper.utils.nosetests import clean_noise
 from w3af.core.controllers.ci.nosetests_wrapper.constants import NOSE_IGNORE_SELECTOR
 
+
+def unique_everseen(iterable, key=None):
+    "List unique elements, preserving order. Remember all elements ever seen."
+    # unique_everseen('AAAABBBCCDAABBB') --> A B C D
+    # unique_everseen('ABBCcAD', str.lower) --> A B C D
+    seen = set()
+    seen_add = seen.add
+    if key is None:
+        for element in ifilterfalse(seen.__contains__, iterable):
+            seen_add(element)
+            yield element
+    else:
+        for element in iterable:
+            k = key(element)
+            if k not in seen:
+                seen_add(k)
+                yield element
 
 def print_info_console(cmd, stdout, stderr, exit_code, output_fname):
     fmt = '%s (%s)'
@@ -40,9 +59,10 @@ def print_summary(all_tests, run_tests, ignored_tests):
     logging.info('%s out of %s tests run' % (len(run_tests._tests),
                                              len(all_tests._tests)))
     
-    missing = sorted([test.id() for test in ignored_tests._tests])
+    missing = unique_everseen(sorted([test.id() for test in ignored_tests._tests]))
     missing_str = '\n'.join(missing)
     
-    msg = 'The following tests were not run due to selector "%s":\n%s'
-    logging.debug(msg % (NOSE_IGNORE_SELECTOR, missing_str))
+    msg = 'The following %s tests were not run due to selector "%s":\n%s'
+    logging.debug(msg % (len(ignored_tests._tests), NOSE_IGNORE_SELECTOR,
+                         missing_str))
     
