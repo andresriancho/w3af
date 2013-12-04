adc7781807d9df80894b72d65364ce08ff7071e5
Andres Riancho <andres.riancho@gmail.com>
2013-12-04 18:54:48 -0300

    Migrated test to django-moth

--- w3af/plugins/tests/infrastructure/test_server_header.py
+++ w3af/plugins/tests/infrastructure/test_server_header.py
@@ -18,14 +18,13 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
-
-from nose.plugins.attrib import attr
+from w3af.core.controllers.ci.moth import get_moth_http
 from w3af.plugins.tests.helper import PluginTest, PluginConfig
 
 
 class TestServerHeader(PluginTest):
 
-    base_url = 'http://moth/'
+    base_url = get_moth_http()
 
     _run_configs = {
         'cfg': {
@@ -34,7 +33,6 @@ class TestServerHeader(PluginTest):
         }
     }
 
-    @attr('ci_fails')
     def test_find_server_power(self):
         cfg = self._run_configs['cfg']
         self._scan(cfg['target'], cfg['plugins'])
