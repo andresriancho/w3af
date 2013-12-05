ba44dc0277d9f41d1d3473610244da7adf7738f5
Andres Riancho <andres.riancho@gmail.com>
2013-12-05 15:03:11 -0300

    Also inspect SSL log

--- w3af/core/controllers/ci/teardown_moth.py
+++ w3af/core/controllers/ci/teardown_moth.py
@@ -5,8 +5,9 @@ import sys
 import tempfile
 
 ARTIFACTS_DIR = os.environ.get('CIRCLE_ARTIFACTS', tempfile.gettempdir())
-LOG_NAME = 'django-moth.log'
-LOG_FILE = os.path.join(ARTIFACTS_DIR, LOG_NAME)
+
+HTTP_LOG = 'django-moth.log'
+SSL_LOG = 'django-moth-ssl.log'
 
 TRACEBACK = 'Traceback (most recent call last)'
 
@@ -17,13 +18,18 @@ def check_traceback_in_log():
     in this vulnerable application, should be handled properly. They usually
     tell me that I forgot something.
     '''
-    for line in file(LOG_FILE):
-        if TRACEBACK in line:
-            msg = 'Found a traceback in %s, check the build artifacts at CircleCI.'
-            print(msg % LOG_NAME)
-            sys.exit(1)
+    for log_file in (HTTP_LOG, SSL_LOG):
+        
+        log_path = os.path.join(ARTIFACTS_DIR, log_file)
+        
+        for line in file(log_path):
+            if TRACEBACK in line:
+                msg = 'Found a traceback in %s, check the build artifacts'\
+                      ' at CircleCI.'
+                print(msg % log_file)
+                sys.exit(1)
             
-    print('No tracebacks found in %s' % LOG_NAME)
+    print('No tracebacks found in django moth logs.')
 
 if __name__ == '__main__':
     check_traceback_in_log()
