2b041b462763f8c1d041153beeb1192b7ff71dea
Andres Riancho <andres.riancho@gmail.com>
2014-01-29 10:57:33 -0300

    First step towards CentOS support for dependency check, still needs more testing.

--- w3af/core/controllers/dependency_check/platforms/current_platform.py
+++ w3af/core/controllers/dependency_check/platforms/current_platform.py
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
 from w3af.core.controllers.dependency_check.requirements import PIP_PACKAGES
-from ..os_detection import is_mac, is_openbsd, is_fedora
+from ..os_detection import is_mac, is_openbsd, is_fedora, is_centos
 
 if is_mac():
     from .mac import (SYSTEM_NAME, PKG_MANAGER_CMD,
@@ -39,6 +39,12 @@ elif is_fedora():
                         SYSTEM_PACKAGES, PIP_CMD,
                         os_package_is_installed,
                         after_hook)
+
+elif is_centos():
+    from .centos import (SYSTEM_NAME, PKG_MANAGER_CMD,
+                         SYSTEM_PACKAGES, PIP_CMD,
+                         os_package_is_installed,
+                         after_hook)
 
 else:
     from .linux import (SYSTEM_NAME, PKG_MANAGER_CMD,
