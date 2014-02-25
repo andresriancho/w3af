b41a60558d570175d23f4e5e5e659874658b1a7b
Andres Riancho <andres.riancho@gmail.com>
2014-02-25 10:48:47 -0300

    Removing unused import

--- w3af/plugins/tests/output/test_export_requests.py
+++ w3af/plugins/tests/output/test_export_requests.py
@@ -22,7 +22,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 import os
 import urllib
 
-from nose.plugins.attrib import attr
 from w3af.core.controllers.ci.moth import get_moth_http
 from w3af.plugins.tests.helper import PluginTest, PluginConfig
 from w3af.core.data.parsers.url import URL
