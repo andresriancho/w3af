f9cd58ae164fcc9ce050779609f5fa0a51121efa
Andres Riancho <andres.riancho@gmail.com>
2014-02-26 16:09:38 -0300

    Better error message

--- w3af/core/controllers/auto_update/git_client.py
+++ w3af/core/controllers/auto_update/git_client.py
@@ -1,4 +1,4 @@
-'''
+"""
 auto_update.py
 
 Copyright 2011 Andres Riancho
@@ -17,7 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-'''
+"""
 import threading
 import git
 
@@ -35,9 +35,9 @@ class GitClientError(Exception):
 
 
 class GitClient(object):
-    '''
+    """
     Our wrapper for performing actions on the git repository.
-    '''
+    """
     UPD_ERROR_MSG = ('An error occurred while fetching from the remote'
                      ' Git repository! Please update manually using'
                      ' "git pull".')
@@ -48,10 +48,10 @@ class GitClient(object):
         self._progress = GitRemoteProgress()
     
     def add_observer(self, observer):
-        '''
+        """
         :param observer: Function which takes four parameters,
                             op_code, cur_count, max_count, message
-        '''
+        """
         self._progress.add_observer(observer)
     
     @property
@@ -73,9 +73,10 @@ class GitClient(object):
             # or which exception would be raised. So I'm catching all and
             # verifying if there are conflicts in an exception and in the
             # case were no exceptions were raised
-            except Exception:
+            except Exception, e:
                 self.handle_conflicts(latest_before_pull)
-                raise GitClientError(self.UPD_ERROR_MSG)
+                msg = self.UPD_ERROR_MSG + ' The original exception was: "%s"'
+                raise GitClientError(msg % e)
             else:
                 self.handle_conflicts(latest_before_pull)
                 changelog = ChangeLog(latest_before_pull, after_pull)
@@ -92,13 +93,13 @@ class GitClient(object):
         return True
     
     def handle_conflicts(self, reset_commit_id):
-        '''
+        """
         This method verifies if the repository is in conflict and resolved it
         by performing a reset() to the previous commit-id.
         
         :param reset_commit_id: The commit id to reset to
         @raise GitClientError: To let the user know that the update failed
-        '''
+        """
         if repo_has_conflicts():
             self.reset_to_previous_state(reset_commit_id)
             raise GitClientError('A merge conflict was generated while trying'
@@ -106,18 +107,18 @@ class GitClient(object):
                                  ' update manually by using a git client.')
     
     def reset_to_previous_state(self, reset_commit_id):
-        '''
+        """
         Does a "git reset --hard <reset_commit_id>".
         
         :param reset_commit_id: The commit id to reset to
-        '''
+        """
         self._repo.head.reset(commit=reset_commit_id, index=True,
                               working_tree=True)
         
     def get_remote_head_id(self):
-        '''
+        """
         :return: The ID for the latest commit in the REMOTE repo.
-        '''
+        """
         # Get the latest changes from the remote end
         self.fetch()
         
@@ -128,9 +129,9 @@ class GitClient(object):
         return origin_master.commit.hexsha
         
     def get_local_head_id(self):
-        '''
+        """
         :return: The ID for the latest commit in the LOCAL repo.
-        '''
+        """
         branch_name = get_current_branch()
         repo_refs = self._repo.refs
         origin_master = [ref for ref in repo_refs if ref.name == branch_name][0]
@@ -138,9 +139,9 @@ class GitClient(object):
         return origin_master.commit.hexsha
     
     def get_parent_for_revision(self, child_hexsha):
-        '''
+        """
         :return: The parent revision ID for the given hexsha.
-        '''
+        """
         result = []
         
         for commit in self._repo.iter_commits():
@@ -151,11 +152,11 @@ class GitClient(object):
                 
         
 class GitRemoteProgress(RemoteProgress):
-    '''
+    """
     PythonGit will call update() on this object if I pass them to the progress
     parameter of pull or fetch in order to report progress on the long, network-
     bound task.
-    '''
+    """
     observers = []
     
     def add_observer(self, observer):
