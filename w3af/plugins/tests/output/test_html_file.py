2c5d23a883aa7b7616b05962d95781ae9188aeec
Andres Riancho <andres.riancho@gmail.com>
2014-02-26 18:59:52 -0300

    Enabling to see what fails for this test

--- w3af/plugins/tests/output/test_html_file.py
+++ w3af/plugins/tests/output/test_html_file.py
@@ -1,4 +1,4 @@
-'''
+"""
 test_html_file.py
 
 Copyright 2012 Andres Riancho
@@ -17,12 +17,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-'''
+"""
 import os
 import re
 
 from lxml import etree
-from nose.plugins.attrib import attr
 
 from w3af.core.controllers.ci.moth import get_moth_http
 from w3af.core.data.kb.tests.test_vuln import MockVuln
@@ -30,7 +29,6 @@ from w3af.core.data.parsers.url import URL
 from w3af.plugins.tests.helper import PluginTest, PluginConfig
 
 
-@attr('ci_fails')
 class TestHTMLOutput(PluginTest):
 
     target_url = get_moth_http('/audit/xss/')
