ecfe8894a20e63bb1ccfae02ac3ec86c8ebe8919
Andres Riancho <andres.riancho@gmail.com>
2014-02-27 19:57:07 -0300

    Migrated to httpretty

--- w3af/plugins/tests/infrastructure/test_server_status.py
+++ w3af/plugins/tests/infrastructure/test_server_status.py
@@ -18,23 +18,23 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
-
-from nose.plugins.attrib import attr
-from w3af.plugins.tests.helper import PluginTest, PluginConfig
+from w3af.plugins.tests.helper import PluginTest, PluginConfig, MockResponse
 
 
 class TestServerStatus(PluginTest):
 
-    base_url = 'http://moth/'
+    target_url = 'http://httpretty-mock/'
+
+    MOCK_RESPONSES = [MockResponse('/server-status',
+                                   '<dl><dt>Server Version: Apache/2.2.9 (Unix)</dt>'),]
 
     _run_configs = {
         'cfg': {
-        'target': base_url,
+        'target': target_url,
         'plugins': {'infrastructure': (PluginConfig('server_status'),)}
         }
     }
 
-    @attr('ci_fails')
     def test_find_server(self):
         cfg = self._run_configs['cfg']
         self._scan(cfg['target'], cfg['plugins'])
