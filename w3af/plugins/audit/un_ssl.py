0dd69e14436120f1402605600f59ae16ec0840b3
Andres Riancho <andres.riancho@gmail.com>
2014-03-10 18:44:38 -0300

    unssl audit plugin generated errors stop scan #1450

--- w3af/plugins/audit/un_ssl.py
+++ w3af/plugins/audit/un_ssl.py
@@ -25,7 +25,8 @@ import w3af.core.data.constants.severity as severity
 
 from w3af.core.controllers.plugins.audit_plugin import AuditPlugin
 from w3af.core.controllers.misc.levenshtein import relative_distance_boolean
-from w3af.core.controllers.exceptions import BaseFrameworkException
+from w3af.core.controllers.exceptions import (BaseFrameworkException,
+                                              ScanMustStopException)
 from w3af.core.data.kb.vuln import Vuln
 
 
@@ -66,21 +67,31 @@ class un_ssl(AuditPlugin):
             secure_fr = freq.copy()
             secure_fr.set_url(secure_uri)
 
+            # Make sure that we ignore errors during this test
             send_mutant = self._uri_opener.send_mutant
+            kwargs = {'grep': False, 'ignore_errors': True}
 
             try:
-                insecure_response = send_mutant(insecure_fr, grep=False)
-                secure_response = send_mutant(secure_fr, grep=False)
-            except BaseFrameworkException:
+                insecure_response = send_mutant(insecure_fr, **kwargs)
+                secure_response = send_mutant(secure_fr,  **kwargs)
+            except (BaseFrameworkException, ScanMustStopException):
                 # No vulnerability to report since one of these threw an error
                 # (because there is nothing listening on that port). It makes
                 # no sense to keep running since we already got an error
                 self._run = False
-                
+
             else:
+                if insecure_response is None or secure_response is None:
+                    # No vulnerability to report since one of these threw an
+                    # error (because there is nothing listening on that port).
+                    # It makes no sense to keep running since we already got an
+                    # error
+                    self._run = False
+                    return
+
                 if self._redirects_to_secure(insecure_response, secure_response):
                     return
-                
+
                 if insecure_response.get_code() == secure_response.get_code()\
                 and relative_distance_boolean(insecure_response.get_body(),
                                               secure_response.get_body(),
@@ -104,6 +115,16 @@ class un_ssl(AuditPlugin):
                     # In most cases, when one resource is available, all are
                     # so we just stop searching for this vulnerability
                     self._run = False
+
+    def handle_url_error(self, url_error):
+        """
+        Override the url error handler because in most cases we'll be connecting
+        to a URL which is offline.
+
+        :return: (True: to avoid this exception to be re-raised
+                  None: There is no response)
+        """
+        return True, None
 
     def _redirects_to_secure(self, insecure_response, secure_response):
         """
