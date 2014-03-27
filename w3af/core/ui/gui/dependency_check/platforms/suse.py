dd19a0a981acafe22a7f97b0326ace09cfc17950
Andres Riancho <andres.riancho@gmail.com>
2014-03-27 17:33:51 -0300

    Fixing import

--- w3af/core/ui/gui/dependency_check/platforms/suse.py
+++ w3af/core/ui/gui/dependency_check/platforms/suse.py
@@ -19,15 +19,15 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 '''
-from core.controllers.dependency_check.pip_dependency import PIPDependency
-from core.controllers.dependency_check.platforms.suse import (SYSTEM_NAME,
-                                                               PKG_MANAGER_CMD,
-                                                               SYSTEM_PACKAGES,
-                                                               PIP_CMD,
-                                                               PHPLY_GIT,
-                                                               PIP_PACKAGES,
-                                                               os_package_is_installed,
-                                                               after_hook)
+from w3af.core.controllers.dependency_check.pip_dependency import PIPDependency
+from w3af.core.controllers.dependency_check.platforms.suse import (SYSTEM_NAME,
+                                                               	   PKG_MANAGER_CMD,
+                                                               	   SYSTEM_PACKAGES,
+                                                               	   PIP_CMD,
+                                                               	   PHPLY_GIT,
+                                                               	   PIP_PACKAGES,
+                                                               	   os_package_is_installed,
+                                                               	   after_hook)
 
 # I imported the information from the core, now I need to append the GUI
 # requirements to those lists!
