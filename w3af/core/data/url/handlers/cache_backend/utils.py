5421d0cf3d1444c9d17d780451e18ffc88ea3018
Andres Riancho <andres.riancho@gmail.com>
2014-04-03 17:51:04 -0300

    Commented out the encode call to fix #1917

--- w3af/core/data/url/handlers/cache_backend/utils.py
+++ w3af/core/data/url/handlers/cache_backend/utils.py
@@ -9,13 +9,18 @@ def gen_hash(request):
     headers_1 = ''.join('%s%s' % (h, v) for h, v in req.headers.iteritems())
     headers_2 = ''.join('%s%s' % (h, v) for h, v in req.unredirected_hdrs.iteritems())
     
-    the_str = '%s%s%s%s%s' % (
-                             req.get_method(),
-                             req.get_full_url(),
-                             headers_1,
-                             headers_2,
-                             req.get_data() or ''
-                             )
-    
-    the_str = the_str.encode('utf-8', 'ignore')
+    the_str = '%s%s%s%s%s' % (req.get_method(),
+                              req.get_full_url(),
+                              headers_1,
+                              headers_2,
+                              req.get_data() or '')
+
+    # Commented out the encode call to fix #1917
+    # https://github.com/andresriancho/w3af/issues/1917
+    #
+    # But, as with everything else related with encoding, I'm unsure and that's
+    # why I won't remove the line for some time.
+    #
+    #the_str = the_str.encode('utf-8', 'ignore')
+
     return hashlib.md5(the_str).hexdigest()
