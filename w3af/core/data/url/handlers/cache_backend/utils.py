1c9e052fd70f679d1bbdf9ff53501a4fa042ad35
Andres Riancho <andres.riancho@gmail.com>
2014-04-03 18:36:47 -0300

    Fixing https://github.com/andresriancho/w3af/issues/1917 in a better way, which doesn't break test_xurllib.py

--- w3af/core/data/url/handlers/cache_backend/utils.py
+++ w3af/core/data/url/handlers/cache_backend/utils.py
@@ -4,23 +4,32 @@ import hashlib
 def gen_hash(request):
     """
     Generate an unique ID for a request
+
+    Note that we use safe_str function in order to avoid errors like:
+        * Encoding error #1917
+        * https://github.com/andresriancho/w3af/issues/1917
     """
     req = request
-    headers_1 = ''.join('%s%s' % (h, v) for h, v in req.headers.iteritems())
-    headers_2 = ''.join('%s%s' % (h, v) for h, v in req.unredirected_hdrs.iteritems())
+    headers_1 = ''.join('%s%s' % (safe_str(h), safe_str(v)) for h, v in req.headers.iteritems())
+    headers_2 = ''.join('%s%s' % (safe_str(h), safe_str(v)) for h, v in req.unredirected_hdrs.iteritems())
     
-    the_str = '%s%s%s%s%s' % (req.get_method(),
-                              req.get_full_url(),
+    the_str = '%s%s%s%s%s' % (safe_str(req.get_method()),
+                              safe_str(req.get_full_url()),
                               headers_1,
                               headers_2,
-                              req.get_data() or '')
-
-    # Commented out the encode call to fix #1917
-    # https://github.com/andresriancho/w3af/issues/1917
-    #
-    # But, as with everything else related with encoding, I'm unsure and that's
-    # why I won't remove the line for some time.
-    #
-    #the_str = the_str.encode('utf-8', 'ignore')
+                              safe_str(req.get_data() or ''))
 
     return hashlib.md5(the_str).hexdigest()
+
+
+def safe_str(obj):
+    """
+    http://code.activestate.com/recipes/466341-guaranteed-conversion-to-unicode-or-byte-string/
+
+    :return: The byte string representation of obj
+    """
+    try:
+        return str(obj)
+    except UnicodeEncodeError:
+        # obj is unicode
+        return unicode(obj).encode('unicode_escape')
