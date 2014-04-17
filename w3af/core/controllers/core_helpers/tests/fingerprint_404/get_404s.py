e33e8da7d6d32edd47198fd0964fedd5fef7f15c
Andres Riancho <andres.riancho@gmail.com>
2014-04-17 13:36:49 -0300

    Don't run this on CI

--- w3af/core/controllers/core_helpers/tests/fingerprint_404/get_404s.py
+++ w3af/core/controllers/core_helpers/tests/fingerprint_404/get_404s.py
@@ -9,45 +9,46 @@ ALEXA_TOP1M = 'http://s3.amazonaws.com/alexa-static/top-1m.csv.zip'
 ALEXA_FILE = 'top-1m.csv'
 ALEXA_FILE_COMPRESSED = 'top-1m.csv.zip'
 
-if not os.path.exists(ALEXA_FILE_COMPRESSED):
-    resp = urllib2.urlopen(ALEXA_TOP1M)
-    file(ALEXA_FILE, 'w').write(resp.read())
+if __name__ == '__main__':
+    if not os.path.exists(ALEXA_FILE_COMPRESSED):
+        resp = urllib2.urlopen(ALEXA_TOP1M)
+        file(ALEXA_FILE, 'w').write(resp.read())
 
-if not os.path.exists(ALEXA_FILE):
-    zfile = zipfile.ZipFile(ALEXA_FILE_COMPRESSED)
-    zfile.extract(ALEXA_FILE, '.')
+    if not os.path.exists(ALEXA_FILE):
+        zfile = zipfile.ZipFile(ALEXA_FILE_COMPRESSED)
+        zfile.extract(ALEXA_FILE, '.')
 
-s = shelve.open('data.shelve')
+    s = shelve.open('data.shelve')
 
-# This is a "resume" feature
-last = len(s)
-print 'c(%s)' % last,
+    # This is a "resume" feature
+    last = len(s)
+    print 'c(%s)' % last,
 
-for i, line in enumerate(file(ALEXA_FILE)):
-    if i <= last:
-        continue
+    for i, line in enumerate(file(ALEXA_FILE)):
+        if i <= last:
+            continue
 
-    line = line.strip()
-    _, domain = line.split(',')
+        line = line.strip()
+        _, domain = line.split(',')
 
-    try:
-        ok = urllib2.urlopen('http://%s/' % domain).read()
         try:
-            bad = urllib2.urlopen('http://%s/not-ex1st.html' % domain).read()
-        except urllib2.HTTPError, error:
-            bad = error.read()
-    except KeyboardInterrupt:
-        break
-    except urllib2.HTTPError:
-        sys.stdout.write('4')
-        sys.stdout.flush()
-    except Exception:
-        sys.stdout.write('E')
-        sys.stdout.flush()
-    else:
-        s[domain] = (ok, bad)
-        sys.stdout.write('.')
-        sys.stdout.flush()
+            ok = urllib2.urlopen('http://%s/' % domain).read()
+            try:
+                bad = urllib2.urlopen('http://%s/not-ex1st.html' % domain).read()
+            except urllib2.HTTPError, error:
+                bad = error.read()
+        except KeyboardInterrupt:
+            break
+        except urllib2.HTTPError:
+            sys.stdout.write('4')
+            sys.stdout.flush()
+        except Exception:
+            sys.stdout.write('E')
+            sys.stdout.flush()
+        else:
+            s[domain] = (ok, bad)
+            sys.stdout.write('.')
+            sys.stdout.flush()
 
-sys.stdout.write('\n')
-s.close()
+    sys.stdout.write('\n')
+    s.close()
