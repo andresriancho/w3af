d5ccb6f0743790802cbe61be818a8234bdbabaec
Andres Riancho <andres.riancho@gmail.com>
2014-04-23 14:51:16 -0300

    Minor perf improvement

--- w3af/core/data/dc/headers.py
+++ w3af/core/data/dc/headers.py
@@ -19,6 +19,8 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
+import string
+
 from w3af.core.data.constants.encodings import UTF8
 from w3af.core.data.dc.data_container import DataContainer
 from w3af.core.data.misc.encoding import smart_unicode
@@ -80,8 +82,10 @@ class Headers(DataContainer):
         :param header_name: The name of the header we want the value for
         :return: The value for a header given a name (be case insensitive)
         """
+        lower_header_name = header_name.lower()
+        
         for stored_header_name in self:
-            if header_name.lower() == stored_header_name.lower():
+            if lower_header_name == stored_header_name.lower():
                 return self[stored_header_name], stored_header_name
 
         return default, None
