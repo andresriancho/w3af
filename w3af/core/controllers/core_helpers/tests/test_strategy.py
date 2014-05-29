b963ba622f67b0bd81930b999d25a1fec4011476
Andres Riancho <andres.riancho@gmail.com>
2014-05-29 10:02:21 -0300

    Test case which reproduces Pseudo-random number of vulnerabilities found in audit phase (xss) #1557

--- w3af/core/controllers/core_helpers/tests/test_strategy.py
+++ w3af/core/controllers/core_helpers/tests/test_strategy.py
@@ -18,10 +18,40 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
+import subprocess
+import sys
+import os
+
 from nose.plugins.attrib import attr
 
 from w3af.core.controllers.ci.moth import get_moth_http
+from w3af.core.controllers.ci.wavsep import get_wavsep_http
 from w3af.plugins.tests.helper import PluginTest, PluginConfig
+
+SCRIPT_PATH = '/tmp/script-1557.w3af'
+TEST_SCRIPT_1557 = """\
+plugins
+output console
+output config console
+set verbose False
+back
+
+audit xss
+
+crawl web_spider
+crawl config web_spider
+set only_forward True
+back
+
+back
+target
+set target %s/active/RXSS-Detection-Evaluation-GET/index.jsp
+back
+
+start
+
+exit
+"""
 
 
 class TestStrategy(PluginTest):
@@ -52,3 +82,42 @@ class TestStrategy(PluginTest):
 
         self.assertEquals(id_before_fr, id_after_fr)
         self.assertEquals(id_before_ur, id_after_ur)
+
+    def tearDown(self):
+        if os.path.exists(SCRIPT_PATH):
+            os.unlink(SCRIPT_PATH)
+
+    def test_1557_random_number_of_results(self):
+        """
+        Pseudo-random number of vulnerabilities found in audit phase (xss)
+
+        https://github.com/andresriancho/w3af/issues/1557
+        """
+        file(SCRIPT_PATH, 'w').write(TEST_SCRIPT_1557 % get_wavsep_http())
+
+        python_executable = sys.executable
+
+        VULN_STRING = 'A Cross Site Scripting vulnerability'
+        vuln_count = []
+
+        for i in xrange(25):
+            print('Start run #%s' % i)
+
+            p = subprocess.Popen([python_executable, 'w3af_console',
+                                  '-n', '-s', script_path],
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE,
+                                  stdin=subprocess.PIPE,
+                                  shell=False,
+                                  universal_newlines=True)
+
+            stdout, stderr = p.communicate()
+            i_vuln_count = stdout.count(VULN_STRING)
+            print('%s vulnerabilities found' % i_vuln_count)
+
+            self.assertNotEqual(i_vuln_count, 0)
+
+            for previous_count in vuln_count:
+                self.assertEqual(previous_count, i_vuln_count)
+
+            vuln_count.append(i_vuln_count)
