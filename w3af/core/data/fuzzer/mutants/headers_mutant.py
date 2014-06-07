985f8b3378faf61d25ee8330397cb72d68c45e03
Andres Riancho <andres.riancho@gmail.com>
2014-06-06 23:07:03 -0300

    Migrated HeadersMutant to new architecture (this is one of the greatest examples on the great refactoring I'm doing!) #580

--- w3af/core/data/fuzzer/mutants/headers_mutant.py
+++ w3af/core/data/fuzzer/mutants/headers_mutant.py
@@ -20,33 +20,21 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 """
 from w3af.core.data.fuzzer.mutants.mutant import Mutant
-from w3af.core.data.dc.headers import Headers
 
 
 class HeadersMutant(Mutant):
     """
     This class is a headers mutant.
     """
-    def __init__(self, freq):
-        Mutant.__init__(self, freq)
-
-    def get_mutant_type(self):
+    @staticmethod
+    def get_mutant_type():
         return 'headers'
 
     def get_dc(self):
-        return self._headers
+        return self._freq.get_headers()
 
-    def set_dc(self, dc):
-        # See comment below (search for __HERE__).
-        fixed_headers = Headers()
-
-        for key, value in dc.iteritems():
-            if isinstance(value, list):
-                value = value[0]
-
-            fixed_headers[key] = value
-
-        self._headers = fixed_headers
+    def set_dc(self, new_headers):
+        return self._freq.set_headers(new_headers)
 
     def found_at(self):
         """
@@ -58,23 +46,6 @@ class HeadersMutant(Mutant):
         return fmt % (self.get_url(), self.get_method(), self.get_var(),
                       self.get_token_value())
 
-    def set_token_value(self, val):
-        """
-        Set the value of the variable that this mutant modifies.
-        """
-        try:
-            self._freq._headers[self.get_var()] = val
-        except:
-            msg = 'The headers mutant object wasn\'t  correctly initialized.'
-            raise ValueError(msg)
-
-    def get_token_value(self):
-        try:
-            return self._freq._headers[self.get_var()]
-        except:
-            msg = 'The headers mutant object wasn\'t  correctly initialized.'
-            raise ValueError(msg)
-
     @staticmethod
     def create_mutants(freq, mutant_str_list, fuzzable_param_list,
                        append, fuzzer_config, data_container=None):
@@ -82,31 +53,16 @@ class HeadersMutant(Mutant):
         This is a very important method which is called in order to create
         mutants. Usually called from fuzzer.py module.
         """
-        if not fuzzer_config['fuzzable_headers']:
+        fuzzable_headers = fuzzer_config['fuzzable_headers']
+
+        if not fuzzable_headers:
             return []
 
         # Generate a list with the headers we'll fuzz
-        fuzzable_param_list = fuzzable_param_list + fuzzer_config[
-            'fuzzable_headers']
+        fuzzable_param_list = fuzzable_param_list + fuzzable_headers
 
-        # Generate a dummy object that we'll use for fixing the "impedance mismtach"
-        # between the Headers() object that doesn't have the same form as a
-        # generic DataContainer. Headers look like:
-        #    {'a':'b'}
-        # While data containers look like
-        #    {'a': ['b',]}
-        #
-        # Note that I'm undoing this in the set_dc method above.
-        # (search for __HERE__)
-        #
-        orig_headers = freq.get_headers()
-        headers_copy = orig_headers.copy()
-        for header_name in fuzzer_config['fuzzable_headers']:
-            headers_copy[header_name] = ''
-        cloned_headers = headers_copy.clone_with_list_values()
-
-        return Mutant._create_mutants_worker(
-            freq, HeadersMutant, mutant_str_list,
-            fuzzable_param_list,
-            append, fuzzer_config,
-            data_container=cloned_headers)
+        return Mutant._create_mutants_worker(freq, HeadersMutant,
+                                             mutant_str_list,
+                                             fuzzable_param_list,
+                                             append, fuzzer_config,
+                                             data_container=freq.get_headers())
