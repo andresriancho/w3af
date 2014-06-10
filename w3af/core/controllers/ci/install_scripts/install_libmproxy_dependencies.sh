bae1c3ee5dfa34479bda19df510026b9fbabec20
Vyacheslav Bakhmutov <m0sth8@yandex-team.ru>
2014-06-10 19:56:41 +0700

    Add installation script for circle CI for ibmproxy dependencies

---
+++ w3af/core/controllers/ci/install_scripts/install_libmproxy_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+
+
+pip install git+git://github.com/mitmproxy/netlib.git@52c6ba8880363ba5d82b5e767559afbc72371272
+pip install git+git://github.com/mitmproxy/mitmproxy.git@00fd243810954b1ea7c108482513a7e92f2e8000
