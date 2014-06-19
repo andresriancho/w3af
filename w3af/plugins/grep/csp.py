f0ab1f23e2b4699ac5fb7b121fef527fef553e4b
Andres Riancho <andres.riancho@gmail.com>
2014-06-19 11:40:16 -0300

    * Fixing unittest to match django-moth * Made the code in CSP plugin beautiful

--- w3af/plugins/grep/csp.py
+++ w3af/plugins/grep/csp.py
@@ -19,18 +19,18 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 """
-import w3af.core.data.constants.severity as severity
-
 from w3af.core.data.db.disk_list import DiskList
 from w3af.core.data.db.disk_item import DiskItem
 from w3af.core.data.kb.vuln import Vuln
 from w3af.core.controllers.plugins.grep_plugin import GrepPlugin
 from w3af.core.controllers.csp.utils import find_vulns
 
+
 class csp(GrepPlugin):
     """
     This plugin identifies incorrect or too permissive CSP (Content Security Policy) headers returned by the web application under analysis.
     """
+    VULN_NAME = 'CSP vulnerability'
 
     def __init__(self):
         """
@@ -44,37 +44,40 @@ class csp(GrepPlugin):
                 
     def get_long_desc(self):
         return """
-        This plugin identifies incorrect or too permissive CSP (Content Security Policy) headers
-        returned by the web application under analysis.
+        This plugin identifies incorrect or too permissive CSP (Content Security Policy)
+        headers returned by the web application under analysis.
 
         Additional information: 
-        https://www.owasp.org/index.php/Content_Security_Policy
-        http://www.w3.org/TR/CSP
+         * https://www.owasp.org/index.php/Content_Security_Policy
+         * http://www.w3.org/TR/CSP
         """        
 
     def grep(self, request, response):
         """
         Perform search on current HTTP request/response exchange.
-        Store informations about vulns for further global processing.
+        Store information about vulns for further global processing.
         
         @param request: HTTP request
         @param response: HTTP response  
         """
-        #Check that current URL has not been already analyzed
-        response_url = str(response.get_url().uri2url())
+        # Check that current URL has not been already analyzed
+        response_url = response.get_url().uri2url()
         if response_url in self._urls:
             return        
-        else:
-            self._urls.append(response_url)    
+
+        self._urls.append(response_url)
                 
-        #Search issues using dedicated module
+        # Search issues using dedicated module
         csp_vulns = find_vulns(response)
         
-        #Analyze issue list
+        # Analyze issue list
         if len(csp_vulns) > 0:
-            vuln_store_item = DiskCSPVulnStoreItem(response_url, response.id, csp_vulns)   
+            vuln_store_item = DiskCSPVulnStoreItem(response_url,
+                                                   response.id,
+                                                   csp_vulns)
             self._vulns.append(vuln_store_item)
-            #Increment the vulnerabilities counter 
+
+            # Increment the vulnerabilities counter
             for csp_directive_name in csp_vulns:
                 self._total_count += len(csp_vulns[csp_directive_name])
                 
@@ -82,44 +85,50 @@ class csp(GrepPlugin):
         """
         Perform global analysis for all vulnerabilities found.
         """
-        #Check if vulns has been found
+        # Check if vulns have been found
         if self._total_count == 0:
             return
         
-        #Parse vulns collection
+        # Parse vulns collection
         vuln_already_reported = []
-        total_url_processed_count = len(self._urls)
+
         for vuln_store_item in self._vulns:
             for csp_directive_name, csp_vulns_list in vuln_store_item.csp_vulns.iteritems():
                 for csp_vuln in csp_vulns_list:
-                    #Check if the current vuln is common (shared) to several url processed 
-                    #and has been already reported
+                    # Check if the current vuln is common (shared) to several
+                    # url processed and has been already reported
                     if csp_vuln.desc in vuln_already_reported:
                         continue
-                    #Search for current vuln occurences in order to know if 
-                    #the vuln is common (shared) to several url processed                                    
-                    occurences = self._find_occurences(csp_vuln.desc)
-                    v = None
-                    if len(occurences) > 1:
-                        #Shared vuln case
-                        v = Vuln('CSP vulnerability', csp_vuln.desc,
-                            csp_vuln.severity, occurences, self.get_name())
+
+                    # Search for current vuln occurrences in order to know if
+                    # the vuln is common (shared) to several url processed
+                    occurrences = self._find_occurrences(csp_vuln.desc)
+
+                    if len(occurrences) > 1:
+                        # Shared vuln case
+                        v = Vuln(self.VULN_NAME, csp_vuln.desc,
+                                 csp_vuln.severity, occurrences,
+                                 self.get_name())
+                        v.set_url(vuln_store_item.url.base_url())
+
                         vuln_already_reported.append(csp_vuln.desc)
                     else:
-                        #Isolated vuln case
-                        v = Vuln('CSP vulnerability', csp_vuln.desc,
-                            csp_vuln.severity, vuln_store_item.resp_id, self.get_name())
-                    #Report vuln
+                        # Isolated vuln case
+                        v = Vuln(self.VULN_NAME, csp_vuln.desc,
+                                 csp_vuln.severity, vuln_store_item.resp_id,
+                                 self.get_name())
+                        v.set_url(vuln_store_item.url)
+
+                    # Report vuln
                     self.kb_append(self, 'csp', v)
                 
-        #Cleanup
+        # Cleanup
         self._urls.cleanup()
         self._vulns.cleanup()
 
-
-    def _find_occurences(self, vuln_desc):
+    def _find_occurrences(self, vuln_desc):
         """
-        Internal utility function to find all occurences of a vuln 
+        Internal utility function to find all occurrences of a vuln
         into the global collection of vulns found by the plugin.
         
         @param vuln_desc: Vulnerability description.
@@ -127,41 +136,41 @@ class csp(GrepPlugin):
         """
         list_resp_id = []
 
-        #Check input for quick exit
+        # Check input for quick exit
         if vuln_desc is None or vuln_desc.strip() == "":
             return list_resp_id
        
-        #Parse vulns collection
+        # Parse vulns collection
         ref = vuln_desc.lower().strip()        
         for vuln_store_item in self._vulns:
             for csp_directive_name, csp_vulns_list in vuln_store_item.csp_vulns.iteritems():
                 for csp_vuln in csp_vulns_list:        
                     if csp_vuln.desc.strip().lower() == ref:
-                        if vuln_store_item.resp_id  not in list_resp_id:
+                        if vuln_store_item.resp_id not in list_resp_id:
                             list_resp_id.append(vuln_store_item.resp_id)
 
         return list_resp_id
 
 
-
 class DiskCSPVulnStoreItem(DiskItem):
     """
-    This is a class to store CSP vulnerabilities found for a URL (URL+ID) in a DiskList or DiskSet.
+    This is a class to store CSP vulnerabilities found for a URL (URL+ID) in a
+    DiskList or DiskSet.
     """
 
-    """
-    Constructor.
-    @param r_url: HTTP reponse url
-    @param r_id: HTTP reponse ID
-    @param r_vulns: CSP vulnerabilities found
-    """
     def __init__(self, r_url, r_id, r_vulns):
+        """
+        Constructor.
+        @param r_url: HTTP reponse url
+        @param r_id: HTTP reponse ID
+        @param r_vulns: CSP vulnerabilities found
+        """
         self.url = r_url
         self.resp_id = r_id
         self.csp_vulns = r_vulns
 
-    """
-    Implements method from base class.
-    """
     def get_eq_attrs(self):
-        return ['url','resp_id']        
+        """
+        Implements method from base class.
+        """
+        return ['url','resp_id']
--- w3af/plugins/tests/grep/test_csp.py
+++ w3af/plugins/tests/grep/test_csp.py
@@ -18,24 +18,17 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
-from nose.plugins.attrib import attr
-
 from w3af.core.controllers.ci.moth import get_moth_http
 from w3af.plugins.tests.helper import PluginTest, PluginConfig
  
  
-@attr('ci_ready')
 class TestCSP(PluginTest):
  
-    # Test scripts URLs
-    base_url = get_moth_http()
-    csp_with_error_url = base_url + '/grep/csp/index.php'    
-    csp_without_error_url = base_url + '/grep/csp/csp_without_error.php'
+    target_url = get_moth_http('/grep/csp/')
 
-    #Test configurations 
-    _run_configs = {          
+    _run_configs = {
         'cfg_with_error': {
-            'target': csp_with_error_url,
+            'target': target_url,
             'plugins': {
                 'grep': (PluginConfig('csp'),),
                 'crawl': (
@@ -44,50 +37,32 @@ class TestCSP(PluginTest):
                 )
             }
         },        
-        'cfg_without_error': {
-            'target': csp_without_error_url,
-            'plugins': {
-                'grep': (PluginConfig('csp'),)
-            }
-        }
     }
 
     def test_found_vuln(self):
         """
         Test to validate case in which error are found:
-        One vuln is common to several pages and others are isoled.
+        One vuln is common to several pages and others are isolated.
         """
-        #Prepare expectation
-        expected_vulns_desc = []
-        #---This vuln is shared by several pages
-        expected_vulns_desc.append("Directive 'default-src' allow all sources.")
-        #---Theses vulns are isolated
-        expected_vulns_desc.append("Directive 'script-src' allow all javascript sources.")
-        expected_vulns_desc.append("Directive 'script-src' is defined but no directive 'script-nonce' is defined to protect javascript resources.")
-        expected_vulns_desc.append("Directive 'object-src' allow all plugin sources.")
-        expected_vulns_desc.append("Somes directives are misspelled: def-src,sript-src.")
-
-        #Configure and run test case
         cfg = self._run_configs['cfg_with_error']
         self._scan(cfg['target'], cfg['plugins'])
 
-        #Apply validation
         vulns = self.kb.get('csp', 'csp')
-        self.assertEquals(len(expected_vulns_desc), len(vulns))        
-        counter = 0
-        for v in vulns:
-            if v.get_desc(False) in expected_vulns_desc:
-                counter += 1
-        self.assertEquals(counter, len(expected_vulns_desc))        
 
-    def test_no_vuln(self):
-        """
-        Test to validate case in which no error is found.
-        """
-        #Configure and run test case
-        cfg = self._run_configs['cfg_without_error']
-        self._scan(cfg['target'], cfg['plugins'])
-        
-        #Apply validation
-        vulns = self.kb.get('csp', 'csp')
-        self.assertEquals(0, len(vulns))        
+        #---This vuln is shared by several pages
+        EXPECTED = ["Directive 'default-src' allow all sources."]
+        #---Theses vulns are isolated
+        EXPECTED.append("Directive 'script-src' allow all javascript sources.")
+        EXPECTED.append("Directive 'script-src' is defined but no directive"
+                        " 'script-nonce' is defined to protect javascript"
+                        " resources.")
+        EXPECTED.append("Directive 'object-src' allow all plugin sources.")
+        EXPECTED.append("Somes directives are misspelled: def-src,sript-src.")
+
+        vuln_descs = set([v.get_desc(with_id=False) for v in vulns])
+        self.assertEqual(set(EXPECTED), vuln_descs)
+        self.assertAllVulnNamesEqual('CSP vulnerability', vulns)
+
+        NOT_IN_FILENAME = 'csp_without_error.html'
+        vuln_fnames = set([v.get_url().get_file_name() for v in vulns])
+        self.assertNotIn(NOT_IN_FILENAME, vuln_fnames)
