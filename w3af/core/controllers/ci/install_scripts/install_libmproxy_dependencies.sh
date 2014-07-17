b8212596a4bdbe63db120357da93f2168df4ad20
Vyacheslav Bakhmutov <m0sth8@yandex-team.ru>
2014-07-17 14:49:38 +0700

    oh, requirements magic

--- w3af/core/controllers/ci/install_scripts/install_libmproxy_dependencies.sh
+++ w3af/core/controllers/ci/install_scripts/install_libmproxy_dependencies.sh
@@ -3,4 +3,4 @@
 pip install pyOpenSSL==0.14
 pip install jinja2==2.7.3
 pip install -U --force-reinstall git+git://github.com/mitmproxy/netlib.git@55c2133b69bc39ad43c6ce1ab14b32019878e56a
-pip install -U --force-reinstall git+git://github.com/mitmproxy/mitmproxy.git@05a8c52f8f6c4fb5f1820475b9682da5a1eeadda
+pip install  git+git://github.com/mitmproxy/mitmproxy.git@05a8c52f8f6c4fb5f1820475b9682da5a1eeadda
