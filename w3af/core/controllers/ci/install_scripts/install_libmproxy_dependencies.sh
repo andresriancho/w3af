0182eb881e1f0f1e78f5f1869269ab7f9f5872be
Vyacheslav Bakhmutov <m0sth8@yandex-team.ru>
2014-07-17 14:31:12 +0700

    update netlib version

--- w3af/core/controllers/ci/install_scripts/install_libmproxy_dependencies.sh
+++ w3af/core/controllers/ci/install_scripts/install_libmproxy_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
-pip install pyOpenSSL==0.14 jinja2
-pip install git+git://github.com/mitmproxy/netlib.git@52c6ba8880363ba5d82b5e767559afbc72371272
+pip install pyOpenSSL==0.14
+pip install jinja2==2.7.3
+pip install git+git://github.com/mitmproxy/netlib.git@55c2133b69bc39ad43c6ce1ab14b32019878e56a
 pip install git+git://github.com/mitmproxy/mitmproxy.git@05a8c52f8f6c4fb5f1820475b9682da5a1eeadda
