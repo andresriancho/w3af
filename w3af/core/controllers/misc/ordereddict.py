869ad13eba2e331c2d70fbb680f31069dfd035b3
Andres Riancho <andres.riancho@gmail.com>
2014-08-28 13:35:29 -0300

    Cosmetic

--- w3af/core/controllers/misc/ordereddict.py
+++ w3af/core/controllers/misc/ordereddict.py
@@ -45,7 +45,7 @@ class OrderedDict(dict, MutableMapping):
         items = [[k, self[k]] for k in self]
         inst_dict = vars(self).copy()
         inst_dict.pop('_keys', None)
-        return (self.__class__, (items,), inst_dict)
+        return self.__class__, (items,), inst_dict
 
     # Methods with indirect access via the above methods
 
