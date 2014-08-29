2d830cd04ba2f3ca77db37b7d54ee083654d05de
Andres Riancho <andres.riancho@gmail.com>
2014-08-29 16:22:35 -0300

    Fix AttributeError: 'crawl_infrastructure' object has no attribute '_threadpool' #3978

--- w3af/core/controllers/core_helpers/consumers/base_consumer.py
+++ w3af/core/controllers/core_helpers/consumers/base_consumer.py
@@ -190,7 +190,7 @@ class BaseConsumer(Process):
         # This is a special case which loosely translates to: "If there are any
         # pending tasks in the threadpool, even if they haven't yet called the
         # _add_task method, we know we have pending work to do".
-        if self._threadpool is not None:
+        if hasattr(self, '_threadpool') and self._threadpool is not None:
             if self._threadpool._inqueue.qsize() > 0 \
             or self._threadpool._outqueue.qsize() > 0:
                 return True
