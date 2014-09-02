da807c7eef7b6dae601a5d33c014019856a0c51a
Andres Riancho <andres.riancho@gmail.com>
2014-09-02 09:52:27 -0300

    Cosmetic #2833

--- w3af/plugins/attack/payloads/payloads/mysql_config.py
+++ w3af/plugins/attack/payloads/payloads/mysql_config.py
@@ -1,4 +1,3 @@
-import re
 from w3af.plugins.attack.payloads.base_payload import Payload
 from w3af.core.ui.console.tables import table
 
@@ -9,18 +8,14 @@ class mysql_config(Payload):
     """
     def api_read(self):
         result = {}
-        files = []
+        files = ['my.cnf', 'debian.cnf']
 
-        files.append('my.cnf')
-        files.append('debian.cnf')
+        directory_list = self.exec_payload('mysql_config_directory')['directory']
 
-        directory_list = self.exec_payload(
-            'mysql_config_directory')['directory']
-
-        for file in files:
+        for _file in files:
             for directory in directory_list:
 
-                mysql_conf = directory + file
+                mysql_conf = directory + _file
                 content = self.shell.read(mysql_conf)
 
                 if content:
@@ -34,9 +29,7 @@ class mysql_config(Payload):
         if not api_result:
             return 'MySQL configuration files not found.'
         else:
-            rows = []
-            rows.append(['MySQL configuration file', 'Content'])
-            rows.append([])
+            rows = [['MySQL configuration file', 'Content'], []]
             for filename in api_result:
                 rows.append([filename, api_result[filename]])
                 rows.append([])
