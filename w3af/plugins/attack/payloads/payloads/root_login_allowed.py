7329ecab88674e84a5caf0740a05790d4ed4ac05
Andres Riancho <andres.riancho@gmail.com>
2014-09-02 10:21:16 -0300

    Fix ssh web payload - KeyError: 'ssh_attack' #2563

--- w3af/plugins/attack/payloads/payloads/root_login_allowed.py
+++ w3af/plugins/attack/payloads/payloads/root_login_allowed.py
@@ -1,4 +1,5 @@
 import re
+
 from w3af.plugins.attack.payloads.base_payload import Payload
 from w3af.core.ui.console.tables import table
 
@@ -10,37 +11,18 @@ class root_login_allowed(Payload):
     def api_read(self):
         result = {}
 
-        def parse_securetty(securetty):
-            console = re.search('^console', securetty)
-            if console:
-                return console.group(1)
-            else:
-                return ''
-
-        def parse_permit_root_login(config):
-            match_obj = re.search('PermitRootLogin (yes|no)', config)
-            if match_obj:
-                return match_obj.group(1)
-            else:
-                return ''
-
         ssh_config_result = self.exec_payload('ssh_config_files')
         result['ssh_root_bruteforce'] = 'unknown'
+
         for config in ssh_config_result.values():
-            ssh_allows = parse_permit_root_login(config)
-            if ssh_allows == 'yes':
-                result['ssh_root_bruteforce'] = True
-                break
-            elif ssh_allows == 'no':
-                result['ssh_root_bruteforce'] = False
+            ssh_root_bruteforce = parse_permit_root_login(config)
+            result['ssh_root_bruteforce'] = ssh_root_bruteforce
+            if ssh_root_bruteforce:
                 break
 
         securetty = self.shell.read('/etc/securetty')
         if securetty:
-            if parse_securetty(securetty):
-                result['securetty_root_login'] = True
-            else:
-                result['securetty_root_login'] = False
+            result['securetty_root_login'] = parse_securetty(securetty)
 
         return result
 
@@ -48,21 +30,37 @@ class root_login_allowed(Payload):
         api_result = self.api_read()
 
         if not api_result:
-            msg = 'Failed to verify if root login is allowed, '
-            msg += ' a SSH bruteforce attack might still be possible.'
+            msg = 'Failed to verify if root login is allowed, a SSH bruteforce'\
+                  ' attack might still be possible.'
             return msg
         else:
 
-            rows = []
-            rows.append(['Root login allowed', ])
-            rows.append([])
-            if api_result['ssh_attack']:
+            rows = [['Root login allowed', ], []]
+
+            if api_result['ssh_root_bruteforce']:
                 rows.append(['A SSH Bruteforce attack is possible.', ])
-            if api_result['root_login']:
+
+            if api_result['securetty_root_login']:
                 rows.append(['Root user is allowed to login on CONSOLE.', ])
-            if not api_result['root_login'] and not api_result['ssh_attack']:
-                rows.append(['Root user is not allowed to login through SSH nor console.', ])
+
+            if not api_result['ssh_root_bruteforce'] and not api_result['securetty_root_login']:
+                rows.append(['Root user is not allowed to login through SSH'
+                             ' nor console.', ])
 
             result_table = table(rows)
             result_table.draw(80)
             return rows
+
+
+def parse_securetty(securetty):
+    console = re.search('^console', securetty)
+    pts = re.search('^pts', securetty)
+    return bool(console) or bool(pts)
+
+
+def parse_permit_root_login(config):
+    match_obj = re.search('PermitRootLogin (yes|no)', config)
+    if match_obj is not None:
+        return True if match_obj.group(1) == 'yes' else False
+
+    return False
