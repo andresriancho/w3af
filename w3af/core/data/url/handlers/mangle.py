2bb3fb360982af7dadfd9accefccfd9bb16c06e7
Andres Riancho <andres.riancho@gmail.com>
2014-09-04 11:48:27 -0300

    Fixing Keepalive: self.fp = sock.makefile('rb', 0) #2172

--- w3af/core/data/url/handlers/mangle.py
+++ w3af/core/data/url/handlers/mangle.py
@@ -24,8 +24,6 @@ import urllib2
 from w3af.core.data.request.fuzzable_request import FuzzableRequest
 from w3af.core.data.url.HTTPRequest import HTTPRequest
 from w3af.core.data.url.HTTPResponse import HTTPResponse
-from w3af.core.data.dc.headers import Headers
-from w3af.core.data.parsers.url import URL
 from w3af.core.data.url.handlers.keepalive import HTTPResponse as kaHTTPResponse
 from w3af.core.data.url.handlers.output_manager import OutputManagerHandler
 
@@ -53,26 +51,18 @@ class MangleHandler(urllib2.BaseHandler):
 
     def http_response(self, request, response):
 
-        if len(self._plugin_list) and response._connection.sock is not None:
+        if self._plugin_list:
             # Create the HTTPResponse object
-            code, msg, hdrs = response.code, response.msg, response.info()
-            hdrs = Headers(hdrs.items())
-            url_instance = URL(response.geturl())
-            body = response.read()
-            # Id is not here, the mangle is done BEFORE logging
-            # id = response.id
-
-            http_resp = HTTPResponse(code, body, hdrs, url_instance,
-                                     request.url_object, msg=msg)
+            http_resp = HTTPResponse.from_httplib_resp(response)
 
             for plugin in self._plugin_list:
                 plugin.mangle_response(http_resp)
 
-            response = self._HTTPResponse2httplib(response, http_resp)
+            response = self._http_resp_2_httplib(response, http_resp)
 
         return response
 
-    def _HTTPResponse2httplib(self, originalResponse, mangled_response):
+    def _http_resp_2_httplib(self, original_response, mangled_response):
         """
         Convert an HTTPResponse.HTTPResponse object to a httplib.httpresponse
         subclass that I created in keepalive.
@@ -80,17 +70,30 @@ class MangleHandler(urllib2.BaseHandler):
         :param HTTPResponse: HTTPResponse.HTTPResponse object
         :return: httplib.httpresponse subclass
         """
-        ka_resp = kaHTTPResponse(originalResponse._connection.sock,
-                                 debuglevel=0, strict=0, method=None)
+        ka_resp = MangledKeepAliveHTTPResponse()
 
         ka_resp.set_body(mangled_response.get_body())
         ka_resp.headers = mangled_response.get_headers()
         ka_resp.code = mangled_response.get_code()
         ka_resp._url = mangled_response.get_uri().url_string
-        ka_resp.msg = originalResponse.msg
-        ka_resp.id = originalResponse.id
+        ka_resp.msg = original_response.msg
+        ka_resp.id = original_response.id
         ka_resp.encoding = mangled_response.charset
+
         return ka_resp
 
     https_request = http_request
     https_response = http_response
+
+
+class MangledKeepAliveHTTPResponse(kaHTTPResponse):
+    def __init__(self):
+        """
+        Overriding in order to allow me to create a response with a socket
+        instance. At this point I've already read everything I needed from the
+        socket, so it doesn't make any sense to keep a pointer to it.
+
+        :see: https://github.com/andresriancho/w3af/issues/2172
+        """
+        self._rbuf = ''
+        self._method = None
