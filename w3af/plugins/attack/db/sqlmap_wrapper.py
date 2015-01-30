07d3a2663281ca6b007425a5742882f589b3015d
Andres Riancho <andres.riancho@gmail.com>
2015-01-30 18:26:15 -0300

    Workaround for #6439 , now we show a message in the console and hope for the best.

--- w3af/plugins/attack/db/sqlmap_wrapper.py
+++ w3af/plugins/attack/db/sqlmap_wrapper.py
@@ -61,6 +61,8 @@ class SQLMapWrapper(object):
         self.verified_vulnerable = False
         self.proxy = None
         self.local_proxy_url = None
+        self.last_stdout = None
+        self.last_stderr = None
 
         if uri_opener is not None:
             self.start_proxy(uri_opener)
@@ -192,13 +194,11 @@ class SQLMapWrapper(object):
         return self.run_sqlmap_with_pipes(extra_params)
     
     def get_wrapper_params(self, extra_params=[]):
-        params = []
-        
         # TODO: This one will dissapear the day I add stdin handling support
         #       for the wrapper. Please remember that this support will have to
         #       take care of stdin and all other inputs from other UIs
-        params.append('--batch')
-        
+        params = ['--batch']
+
         if not self.coloring:
             params.append('--disable-coloring')
         
@@ -222,23 +222,23 @@ class SQLMapWrapper(object):
         return self.last_command, process
         
     def dbs(self):
-        return self._wrap_param(['--dbs',])
+        return self._wrap_param(['--dbs'])
 
     def tables(self):
-        return self._wrap_param(['--tables',])
+        return self._wrap_param(['--tables'])
 
     def users(self):
-        return self._wrap_param(['--users',])
+        return self._wrap_param(['--users'])
 
     def dump(self):
-        return self._wrap_param(['--dump',])
+        return self._wrap_param(['--dump'])
 
     def read(self, filename):
         """
         :param filename: The file to be read
         :return: The contents of the file that was passed as parameter
         """
-        cmd, process = self._wrap_param(['--file-read=%s' % filename,])
+        cmd, process = self._wrap_param(['--file-read=%s' % filename])
         local_file_re = re.compile("the local file (.*?) and")
         # pylint: disable=E1101
         stdout = process.stdout.read()
