d76e9d7bae1751081c95479029ffdb4528deaa5f
Julián Landerreche <yo@julianlanderreche.com.ar>
2015-02-03 17:41:46 -0300

    Added: missing newlines (\n) in warning messages.
    To fix an issue that would output the warning message
    just after the command. Like this:
    
         w3af>>> startWarning: You (...)
    
    instead of this:
    
         w3af>>> start
         Warning: You (...)

--- w3af/core/ui/console/rootMenu.py
+++ w3af/core/ui/console/rootMenu.py
@@ -78,7 +78,7 @@ class rootMenu(menu):
         # Check if the console output plugin is enabled or not, and warn.
         output_plugins = self._w3af.plugins.get_enabled_plugins('output')
         if 'console' not in output_plugins:
-            msg = "Warning: You disabled the console output plugin. If you"\
+            msg = "\nWarning: You disabled the console output plugin. If you"\
                   " start a new scan, the discovered vulnerabilities won\'t be"\
                   " printed to the console, we advise you to enable at least"\
                   " one output plugin in order to be able to actually see the"\
