5c8e8614dcc2c8e9b9f1efc331f84d06045d7600
Andres Riancho <andres.riancho@gmail.com>
2015-02-23 14:07:48 -0300

    Allow specific tags from cmd line param

--- extras/docker/docker-build.sh
+++ extras/docker/docker-build.sh
@@ -7,8 +7,15 @@ cp Dockerfile ../../
 cp .dockerignore ../../
 
 cd ../../
-COMMIT=`git rev-parse --short HEAD`
-sudo docker build -t andresriancho/w3af:${COMMIT} .
+
+
+if [ $# -eq 1 ]; then
+    TAG=$1
+else
+    TAG=`git rev-parse --short HEAD`
+fi
+
+sudo docker build -t andresriancho/w3af:${TAG} .
 
 rm -rf Dockerfile
 rm -rf .dockerignore
