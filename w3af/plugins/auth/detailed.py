50e04069bcbd19cf61adbed1031fc1f9a31fd744
Andres Riancho <andres.riancho@gmail.com>
2015-03-09 20:04:51 -0300

    Cleanup of New 'detailed' auth plugin flag 'follow_redirects' #7481 (untested)

--- w3af/plugins/auth/detailed.py
+++ w3af/plugins/auth/detailed.py
@@ -26,10 +26,13 @@ from w3af.core.controllers.exceptions import BaseFrameworkException
 
 from w3af.core.data.options.opt_factory import opt_factory
 from w3af.core.data.options.option_list import OptionList
+from w3af.core.data.url.handlers.redirect import GET_HEAD_CODES
 
 
 class detailed(AuthPlugin):
     """Detailed authentication plugin."""
+
+    MAX_REDIRECTS = 10
 
     def __init__(self):
         AuthPlugin.__init__(self)
@@ -44,11 +47,13 @@ class detailed(AuthPlugin):
         self.check_url = 'http://host.tld/'
         self.check_string = ''
         self._login_error = True
+        self.follow_redirects = False
 
     def login(self):
         """
         Login to the application.
         """
+
         msg = 'Logging into the application using %s/%s' % (self.username,
                                                             self.password)
         om.out.debug(msg)
@@ -56,15 +61,47 @@ class detailed(AuthPlugin):
         data = self._get_data_from_format()
 
         try:
+            # Send the auth HTTP request
             functor = getattr(self._uri_opener, self.method)
-            functor(self.auth_url, data)
+            response = functor(self.auth_url, data)
 
+            redir_count = 0
+            working_auth_url = self.auth_url
+
+            # follow redirects if the feature is enabled
+            while self.follow_redirects and redir_count < self.MAX_REDIRECTS:
+                
+                if response.get_code() not in GET_HEAD_CODES:
+                    # no redirect received, continue
+                    break
+
+                # Avoid endless loops
+                redir_count += 1
+
+                location_header_value, _ = response.get_headers().iget('location')
+                uri_header_value, _ = response.get_headers().iget('uri')
+                redir_url = location_header_value or uri_header_value
+
+                redir_url = response.get_url().url_join(redir_url)
+                
+                om.out.debug('auth.detailed was redirected to URL: "%s"' % redir_url)
+                
+                # on HTTP redirect we can only follow up with GET
+                response = self._uri_opener.GET(redir_url)
+
+            if redir_count == self.MAX_REDIRECTS:
+                msg = "auth.detailed seems to have entered an" \
+                      " endless HTTP redirect loop with {0}" \
+                      " redirects, the last URL was {1}"
+                raise Exception(msg.format(redir_count, redir_url))
+
+            # check if we're logged in
             if not self.is_logged():
                 raise Exception("Can't login into web application as %s/%s"
                                 % (self.username, self.password))
             else:
-                om.out.debug('Login success for %s/%s' % (
-                    self.username, self.password))
+                om.out.debug('Login success for %s/%s' % (self.username,
+                                                          self.password))
                 return True
         except Exception, e:
             if self._login_error:
@@ -157,7 +194,10 @@ class detailed(AuthPlugin):
              '    - %U for the username value\n'
              '    - %p for the password parameter name value\n'
              '    - %P for the password value\n'),
-
+            ('follow_redirects',
+             self.follow_redirects,
+             'boolean',
+             'Follow HTTP redirects in multi-stage authentication workflows'),
             ('method',
              self.method,
              'string',
@@ -188,6 +228,7 @@ class detailed(AuthPlugin):
         self.method = options_list['method'].get_value()
         self.auth_url = options_list['auth_url'].get_value()
         self.check_url = options_list['check_url'].get_value()
+        self.follow_redirects= options_list['follow_redirects'].get_value()
 
         for o in options_list:
             if not o.get_value():
@@ -213,4 +254,5 @@ class detailed(AuthPlugin):
             - method
             - check_url
             - check_string
+            - follow_redirects
         """
