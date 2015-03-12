aea26a462af2e40d0c1939fb2e4324faa23e1e78
Andres Riancho <andres.riancho@gmail.com>
2015-03-12 08:09:22 -0300

    Improved error handling and cosmetic changes

--- w3af/plugins/audit/memcachei.py
+++ w3af/plugins/audit/memcachei.py
@@ -19,34 +19,47 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 """
-
 from collections import namedtuple
-import w3af.core.controllers.output_manager as om
+
 import w3af.core.data.constants.severity as severity
+
 from w3af.core.controllers.plugins.audit_plugin import AuditPlugin
-from w3af.core.data.fuzzer.fuzzer import create_mutants
 from w3af.core.controllers.misc.fuzzy_string_cmp import fuzzy_equal
 from w3af.core.controllers.misc.fuzzy_string_cmp import fuzzy_not_equal
+from w3af.core.controllers.exceptions import HTTPRequestException
+from w3af.core.data.fuzzer.fuzzer import create_mutants
 from w3af.core.data.kb.vuln import Vuln
+
+
+MemcacheInjection = namedtuple('MemcacheInjection', ['ok', 'error_1', 'error_2'])
 
 
 class memcachei(AuditPlugin):
 
     def __init__(self):
         AuditPlugin.__init__(self)
-        MemcacheInjection = namedtuple('MemcacheInjection', ['ok', 'error_1', 'error_2'])
-        self.mci = MemcacheInjection(u"key1 0 30 1\r\n1\r\nset injected 0 10 10\r\n1234567890\r\n",
-                                     u"key1 0 f 1\r\n1\r\n",
-                                     u"key1 0 30 0\r\n1\r\n")
+        self.mci = MemcacheInjection(u'key1 0 30 1\r\n1\r\n'
+                                     u'set injected 0 10 10\r\n1234567890\r\n',
+                                     u'key1 0 f 1\r\n1\r\n',
+                                     u'key1 0 30 0\r\n1\r\n')
         self._eq_limit = 0.97
-
-    def debug(self, msg):
-        om.out.debug('[memcachei_debug] ' + msg)
 
     def audit(self, freq, orig_response):
         """
         Tests an URL for memcache injection vulnerabilities.
         """
+        try:
+            self.batch_injection_test(freq, orig_response)
+        except HTTPRequestException:
+            # No need to log, it gets logged at the xurllib level
+            pass
+
+    def batch_injection_test(self, freq, orig_response):
+        """
+        Uses the batch injection technique to find memcache injections
+        """
+        # shortcut
+        send_clean = self._uri_opener.send_clean
 
         # first checking error response
         fake_mutants = create_mutants(freq, ['', ])
@@ -55,9 +68,9 @@ class memcachei(AuditPlugin):
 
             orig_body = orig_response.get_body()
 
-            #trying to break normal execution flow with error1 payload
+            # trying to break normal execution flow with error1 payload
             mutant.set_token_value(self.mci.error_1)
-            error_1_response, body_error_1_response = self._uri_opener.send_clean(mutant)
+            error_1_response, body_error_1_response = send_clean(mutant)
 
             if fuzzy_equal(orig_body, body_error_1_response, self._eq_limit):
                 #
@@ -70,7 +83,7 @@ class memcachei(AuditPlugin):
             # it!
 
             mutant.set_token_value(self.mci.ok)
-            ok_response, body_ok_response = self._uri_opener.send_clean(mutant)
+            ok_response, body_ok_response = send_clean(mutant)
 
             if fuzzy_not_equal(orig_body, body_ok_response, self._eq_limit):
                 #
@@ -78,10 +91,10 @@ class memcachei(AuditPlugin):
                 #
                 continue
 
-            #error2 request to just make sure that wasn't random bytes
+            # error2 request to just make sure that wasn't random bytes
 
             mutant.set_token_value(self.mci.error_2)
-            error_2_response, body_error_2_response = self._uri_opener.send_clean(mutant)
+            error_2_response, body_error_2_response = send_clean(mutant)
 
             if fuzzy_equal(orig_body, body_error_2_response, self._eq_limit):
                 #
@@ -92,7 +105,7 @@ class memcachei(AuditPlugin):
 
             response_ids = [error_1_response.id,
                             ok_response.id,
-                            error_2_response.id,]
+                            error_2_response.id]
 
             desc = 'Memcache injection was found at: "%s", using'\
                    ' HTTP method %s. The injectable parameter is: "%s"'
@@ -103,8 +116,6 @@ class memcachei(AuditPlugin):
             v = Vuln.from_mutant('Memcache injection vulnerability', desc,
                                  severity.HIGH, response_ids, 'memcachei',
                                  mutant)
-
-            self.debug(v.get_desc())
 
             v['ok_html'] = ok_response.get_body()
             v['error_1_html'] = error_1_response.get_body()
@@ -121,5 +132,6 @@ class memcachei(AuditPlugin):
         return """
         This plugin identifies memcache injections using error based techniques,
         it can identify these injection types:
+
             * Batch injection (command injection) - 0x0a/0x0d bytes
         """
