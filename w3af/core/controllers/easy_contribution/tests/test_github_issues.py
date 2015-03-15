d98bfe3c4f93e753830c835df2dfcd171973b06e
Andres Riancho <andres.riancho@gmail.com>
2015-03-15 15:47:18 -0300

    Fix unittest

--- w3af/core/controllers/easy_contribution/tests/test_github_issues.py
+++ w3af/core/controllers/easy_contribution/tests/test_github_issues.py
@@ -27,7 +27,7 @@ from nose.plugins.attrib import attr
 from w3af.core.controllers.easy_contribution.github_issues import (GithubIssues,
                                                                    OAUTH_TOKEN,
                                                                    OAuthTokenInvalid,
-                                                                   LoginFailed)
+                                                                   UserCredentialsInvalid)
 
 
 @attr('internet')
@@ -61,4 +61,4 @@ class TestGithubIssues(unittest.TestCase):
     
     def test_login_failed_user_pass(self):
         gh = GithubIssues('foobar', 'testbar')
-        self.assertRaises(LoginFailed, gh.login)
+        self.assertRaises(UserCredentialsInvalid, gh.login)
