a45bec99e539c8a9d02c0e197d0f1252f0af9fa0
Andres Riancho <andres.riancho@gmail.com>
2015-03-21 20:20:23 -0300

    New plugin to detect missing STS header

---
+++ w3af/plugins/grep/strict_transport_security.py
@@ -0,0 +1,84 @@
+"""
+strict_transport_security.py
+
+Copyright 2015 Andres Riancho
+
+This file is part of w3af, http://w3af.org/ .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""
+from w3af.core.data.kb.info import Info
+from w3af.core.data.kb.info_set import InfoSet
+from w3af.core.controllers.plugins.grep_plugin import GrepPlugin
+
+STS_HEADER = 'Strict-Transport-Security'
+
+
+class strict_transport_security(GrepPlugin):
+    """
+    Check if HTTPS responses have the Strict-Transport-Security header set.
+
+    :author: Andres Riancho (andres.riancho@gmail.com)
+    """
+    def grep(self, request, response):
+        """
+        Check if HTTPS responses have the Strict-Transport-Security header set.
+
+        :param request: The HTTP request object.
+        :param response: The HTTP response object
+        :return: None, all results are saved in the kb.
+        """
+        if request.get_url().get_protocol() != 'https':
+            return
+
+        sts_header_value, _ = response.get_headers().iget(STS_HEADER, None)
+        if sts_header_value is not None:
+            return
+
+        desc = 'The web server uses HTTPS but does not set the '\
+               ' Strict-Transport-Security header.'
+        i = Info('Missing Strict Transport Security header', desc,
+                 response.id, self.get_name())
+        i.set_url(response.get_url())
+        i.add_to_highlight(response.get_msg())
+        i[STSInfoSet.ITAG] = response.get_url().get_domain()
+
+        self.kb_append_uniq_group(self, 'strict_transport_security', i,
+                                  group_klass=STSInfoSet)
+
+    def get_long_desc(self):
+        """
+        :return: A DETAILED description of the plugin functions and features.
+        """
+        return """
+        Check if HTTPS responses have the Strict-Transport-Security header set
+        and report missing URLs.
+
+        https://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security
+        """
+
+
+class STSInfoSet(InfoSet):
+    ITAG = 'domain'
+    TEMPLATE = (
+        'The remote web server sent {{ uris|length }} HTTPS responses which'
+        ' do not contain the Strict-Transport-Security header. The first ten'
+        ' URLs which did not send the header are:\n'
+        ''
+        '{% for url in uris[:10] %}'
+        ' - {{ url }}\n'
+        '{% endfor %}'
+    )
+
---
+++ w3af/plugins/tests/grep/test_strict_transport_security.py
@@ -0,0 +1,124 @@
+"""
+test_strict_transport_security.py
+
+Copyright 2015 Andres Riancho
+
+This file is part of w3af, http://w3af.org/ .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""
+import unittest
+
+import w3af.core.data.kb.knowledge_base as kb
+
+from w3af.core.data.url.HTTPResponse import HTTPResponse
+from w3af.core.data.request.fuzzable_request import FuzzableRequest
+from w3af.core.data.parsers.url import URL
+from w3af.core.data.dc.headers import Headers
+from w3af.core.controllers.misc.temp_dir import create_temp_dir
+
+from w3af.plugins.grep.strict_transport_security import strict_transport_security
+
+
+class TestSTSSecurity(unittest.TestCase):
+
+    def setUp(self):
+        create_temp_dir()
+        self.plugin = strict_transport_security()
+
+    def tearDown(self):
+        self.plugin.end()
+        kb.kb.cleanup()
+
+    def test_http_no_vuln(self):
+        body = ''
+        url = URL('http://www.w3af.com/')
+        headers = Headers([('content-type', 'text/html')])
+        request = FuzzableRequest(url, method='GET')
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        self.plugin.grep(request, resp)
+        self.assertEquals(len(kb.kb.get('strict_transport_security',
+                                        'strict_transport_security')), 0)
+
+    def test_https_with_sts(self):
+        body = ''
+        url = URL('https://www.w3af.com/')
+        headers = Headers([('content-type', 'text/html'),
+                           ('strict-transport-security',
+                            'max-age=31536000; includeSubDomains; preload')])
+        request = FuzzableRequest(url, method='GET')
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        self.plugin.grep(request, resp)
+        self.assertEquals(len(kb.kb.get('strict_transport_security',
+                                        'strict_transport_security')), 0)
+
+    def test_https_without_sts(self):
+        body = ''
+        url = URL('https://www.w3af.com/')
+        headers = Headers([('content-type', 'text/html')])
+        request = FuzzableRequest(url, method='GET')
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        self.plugin.grep(request, resp)
+
+        findings = kb.kb.get('strict_transport_security',
+                             'strict_transport_security')
+        self.assertEquals(len(findings), 1, findings)
+
+        info_set = findings[0]
+        expected_desc = u'The remote web server sent 1 HTTPS responses which' \
+                        u' do not contain the Strict-Transport-Security' \
+                        u' header. The first ten URLs which did not send the' \
+                        u' header are:\n - https://www.w3af.com/\n'
+
+        self.assertEqual(info_set.get_id(), [1])
+        self.assertEqual(info_set.get_desc(), expected_desc)
+        self.assertEqual(info_set.get_name(),
+                         'Missing Strict Transport Security header')
+
+    def test_https_without_sts_group_by_domain(self):
+        body = ''
+        url = URL('https://www.w3af.com/1')
+        headers = Headers([('content-type', 'text/html')])
+        request = FuzzableRequest(url, method='GET')
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        self.plugin.grep(request, resp)
+
+        body = ''
+        url = URL('https://www.w3af.com/2')
+        headers = Headers([('content-type', 'text/html')])
+        request = FuzzableRequest(url, method='GET')
+        resp = HTTPResponse(200, body, headers, url, url, _id=2)
+
+        self.plugin.grep(request, resp)
+
+        findings = kb.kb.get('strict_transport_security',
+                             'strict_transport_security')
+        self.assertEquals(len(findings), 1, findings)
+
+        info_set = findings[0]
+        expected_desc = u'The remote web server sent 2 HTTPS responses which' \
+                        u' do not contain the Strict-Transport-Security' \
+                        u' header. The first ten URLs which did not send the' \
+                        u' header are:\n - https://www.w3af.com/1\n' \
+                        u' - https://www.w3af.com/2\n'
+
+        self.assertEqual(info_set.get_id(), [1, 2])
+        self.assertEqual(info_set.get_desc(), expected_desc)
+        self.assertEqual(info_set.get_name(),
+                         'Missing Strict Transport Security header')
