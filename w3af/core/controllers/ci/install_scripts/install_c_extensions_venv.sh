43efe6fbf7ccb434220708fb4e41d6c66ce9beb5
Andres Riancho <andres.riancho@gmail.com>
2015-03-31 11:08:17 -0300

    I'm bad with bash

--- w3af/core/controllers/ci/install_scripts/install_c_extensions_venv.sh
+++ w3af/core/controllers/ci/install_scripts/install_c_extensions_venv.sh
@@ -5,17 +5,16 @@ set -x
 mkdir -p /home/ubuntu/virtualenvs/venv-2.7.3/lib/python2.7/dist-packages/
 cd /home/ubuntu/virtualenvs/venv-2.7.3/lib/python2.7/dist-packages/
 
-if [ ! -L glib ]
-then
+echo "Creating links from venv to main python installation"
 
-    echo "Creating links from venv to main python installation"
+ln -s /usr/lib/python2.7/dist-packages/glib/ glib
+ln -s /usr/lib/python2.7/dist-packages/gobject/ gobject
+ln -s /usr/lib/python2.7/dist-packages/gtk-2.0* gtk-2.0
+ln -s /usr/lib/python2.7/dist-packages/pygtk.pth pygtk.pth
+ln -s /usr/lib/python2.7/dist-packages/cairo cairo
+ln -s /usr/lib/python2.7/dist-packages/webkit/ webkit
+ln -s /usr/lib/python2.7/dist-packages/webkit.pth webkit.pth
 
-    ln -s /usr/lib/python2.7/dist-packages/glib/ glib
-    ln -s /usr/lib/python2.7/dist-packages/gobject/ gobject
-    ln -s /usr/lib/python2.7/dist-packages/gtk-2.0* gtk-2.0
-    ln -s /usr/lib/python2.7/dist-packages/pygtk.pth pygtk.pth
-    ln -s /usr/lib/python2.7/dist-packages/cairo cairo
-    ln -s /usr/lib/python2.7/dist-packages/webkit/ webkit
-    ln -s /usr/lib/python2.7/dist-packages/webkit.pth webkit.pth
+echo "Done creating links"
+exit 0
 
-fi
