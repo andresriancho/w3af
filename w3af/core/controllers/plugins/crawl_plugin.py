5631a09caad01420cc78ee6afe6f88fcaaaa12a0
Andres Riancho <andres.riancho@gmail.com>
2015-04-16 23:01:58 -0300

    Small refactoring to make function reusable

--- w3af/core/controllers/plugins/crawl_plugin.py
+++ w3af/core/controllers/plugins/crawl_plugin.py
@@ -19,13 +19,11 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 """
-import copy
-
 from stopit import ThreadingTimeout, TimeoutException
 
 from w3af.core.data.request.fuzzable_request import FuzzableRequest
+from w3af.core.controllers.misc.safe_deepcopy import safe_deepcopy
 from w3af.core.controllers.plugins.plugin import Plugin
-from w3af.core.controllers.misc.decorators import retry
 from w3af.core.controllers.core_helpers.fingerprint_404 import is_404
 from w3af.core.controllers.exceptions import (BaseFrameworkException,
                                               FourOhFourDetectionException)
@@ -114,20 +112,3 @@ class CrawlPlugin(Plugin):
                 on_success(http_response, url, *args)
 
         return http_response
-
-
-@retry(2, delay=0.5, backoff=1.1)
-def safe_deepcopy(instance):
-    """
-    In most cases this will just be a wrapper around copy.deepcopy(instance)
-    without any added features, but when that fails because of a race condition
-    such as dictionary changed size during iteration - crawl_plugin.py #8956 ,
-    then we retry.
-
-    I don't want to debug the real issue since it only happen once and I can
-    live with the retry.
-
-    :param instance: The object instance we want to copy
-    :return: A deep copy of the instance
-    """
-    return copy.deepcopy(instance)
