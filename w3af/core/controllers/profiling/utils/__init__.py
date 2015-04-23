e90038730ac4295d80369f083f304487fbcd5f5d
Andres Riancho <andres.riancho@gmail.com>
2015-04-23 13:47:08 -0300

    Better error handling to debug Ctrl+C #9587

--- w3af/core/controllers/profiling/utils/__init__.py
+++ w3af/core/controllers/profiling/utils/__init__.py
@@ -35,7 +35,12 @@ def dump_data_every_thread(func, delay_minutes, save_thread_ptr):
     """
     This is a thread target which every X minutes
     """
-    func()
+    try:
+        func()
+    except KeyboardInterrupt:
+        # Ok, this one failed because the user was playing with Ctrl+C, but we
+        # queue the next run in the lines below
+        pass
 
     save_thread = threading.Timer(delay_minutes * 60,
                                   dump_data_every_thread,
