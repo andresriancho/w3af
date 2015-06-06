844ad86e5427f1e6b50cef69deceb029023d4c38
Andres Riancho <andres.riancho@gmail.com>
2015-06-05 23:28:08 -0300

    Simple test for cookie parser

---
+++ w3af/core/data/parsers/doc/tests/test_cookie_parser.py
@@ -0,0 +1,35 @@
+"""
+test_cookie_parser.py
+
+Copyright 2015 Andres Riancho
+
+This file is part of w3af, http://w3af.org/ .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""
+import unittest
+
+from w3af.core.data.parsers.doc.cookie_parser import parse_cookie
+
+
+class TestParseCookie(unittest.TestCase):
+    def test_basic(self):
+        cookie = parse_cookie('abc=def')
+        self.assertIn('abc', cookie)
+
+    def test_with_path(self):
+        cookie = parse_cookie('abc=def; path=/x')
+        self.assertEqual(cookie['abc']['path'], '/x')
+
