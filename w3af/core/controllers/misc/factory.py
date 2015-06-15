3e6de225c774820b9fcfa9c84c9d617e0af328a7
Andres Riancho <andres.riancho@gmail.com>
2015-06-14 23:28:27 -0300

    Simple solution for #5112

--- w3af/core/controllers/misc/factory.py
+++ w3af/core/controllers/misc/factory.py
@@ -31,7 +31,7 @@ from w3af import ROOT_PATH
 
 def factory(module_name, *args):
     """
-    This function creates an instance of a class thats inside a module
+    This function creates an instance of a class that's inside a module
     with the same name.
 
     Example :
@@ -62,7 +62,7 @@ def factory(module_name, *args):
         # dependencies
         #
         # https://github.com/andresriancho/w3af/issues/9688
-        msg = ('It seems that your Python installation doesn\'t have all the'
+        msg = ('It seems that your Python installation does not have all the'
                ' modules required by the w3af framework. For more information'
                ' about how to install and debug dependency issues please browse'
                ' to http://docs.w3af.org/en/latest/install.html')
@@ -80,15 +80,18 @@ def factory(module_name, *args):
     try:
         module_inst = sys.modules[module_name]
         a_class = getattr(module_inst, class_name)
-    except Exception, e:
-        msg = 'The requested plugin (%s) doesn\'t have a correct format: "%s".'
-        raise BaseFrameworkException(msg % (module_name, e))
+    except Exception:
+        msg = ('The requested plugin (%s) does not have the expected format.'
+               ' Our plugins need to define a class with the same name as the'
+               ' module/file, in other words, if you name the module foo.py'
+               ' there should be a "class foo(...):" inside that file.')
+        raise BaseFrameworkException(msg % module_name)
 
     try:
         inst = a_class(*args)
     except Exception, e:
-        msg = ('Failed to get an instance of "%s". Original exception:'
-               ' "%s". Traceback for this error:\n%s')
+        msg = ('Failed to create an instance of "%s". The original exception'
+               ' was: "%s". Traceback for this error:\n%s')
         msg = msg % (class_name, e, traceback.format_exc())
         raise BaseFrameworkException(msg)
 
