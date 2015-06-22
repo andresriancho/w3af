f391600820c6aa3c58c675910a6803b7fd033122
Andres Riancho <andres.riancho@gmail.com>
2015-06-22 15:57:23 -0300

    Fix Route / to a 404 error page #10532

--- w3af/core/ui/api/resources/__init__.py
+++ w3af/core/ui/api/resources/__init__.py
@@ -1,3 +1,4 @@
 from . import kb
 from . import scans
 from . import error_handlers
+from . import index
---
+++ w3af/core/ui/api/resources/index.py
@@ -0,0 +1,28 @@
+"""
+index.py
+
+Copyright 2015 Andres Riancho
+
+This file is part of w3af, http://w3af.org/ .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""
+from w3af.core.ui.api import app
+from flask import jsonify
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'docs': 'http://docs.w3af.org/en/latest/api/index.html'})
