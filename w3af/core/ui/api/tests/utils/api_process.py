30fe7f0f23bda652e696c736cb190a4834c7731a
Owen Tuz <owen.tuz@telegraph.co.uk>
2015-06-30 19:34:30 +0100

    Fix broken import in unit tests

--- w3af/core/ui/api/tests/utils/api_process.py
+++ w3af/core/ui/api/tests/utils/api_process.py
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 import subprocess
 import requests
-import hashlib
+from hashlib import sha512
 import time
 import sys
 import os
