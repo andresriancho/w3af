98a7f709a157c8df0b50342351f8c6eea718883f
Andres Riancho <andres.riancho@gmail.com>
2015-07-25 11:21:35 -0300

    I suspect that halberd is breaking nginx+uwsgi

--- w3af/plugins/tests/infrastructure/test_hmap.py
+++ w3af/plugins/tests/infrastructure/test_hmap.py
@@ -18,10 +18,13 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
+from nose.plugins.attrib import attr
+
 from w3af.core.controllers.ci.moth import get_moth_http, get_moth_https
 from w3af.plugins.tests.helper import PluginTest, PluginConfig
 
 
+@attr('ci_fails')
 class TestHmap(PluginTest):
 
     base_url = get_moth_http()
