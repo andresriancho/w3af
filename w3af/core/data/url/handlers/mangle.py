ef7915c68cdad261d197847e6c17eaaacab17011
Andres Riancho <andres.riancho@gmail.com>
2015-08-05 11:24:28 -0300

    Fix MangledKeepAliveHTTPResponse instance has no attribute 'fp' #11822

--- w3af/core/data/url/handlers/mangle.py
+++ w3af/core/data/url/handlers/mangle.py
@@ -95,3 +95,25 @@ class MangledKeepAliveHTTPResponse(kaHTTPResponse):
         """
         self._rbuf = ''
         self._method = None
+
+    def close(self):
+        """
+        Since this HTTP response doesn't have a socket, there is nothing to
+        close. We just "pass" to avoid issues like
+
+            https://github.com/andresriancho/w3af/issues/11822
+
+        :return: None
+        """
+        pass
+
+    def close_connection(self):
+        """
+        Since this HTTP response doesn't have a socket, there is nothing to
+        close. We just "pass" to avoid issues like
+
+            https://github.com/andresriancho/w3af/issues/11822
+
+        :return: None
+        """
+        pass
