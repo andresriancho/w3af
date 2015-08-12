80ff7dfd23c91864fc103629bc575f67dee4714b
Piotr Lizończyk <piotr.lizonczyk@gmail.com>
2015-08-12 11:51:50 +0200

    Most of merge request comments applied

--- w3af/plugins/tests/infrastructure/test_webapp_detection.py
+++ w3af/plugins/tests/infrastructure/test_webapp_detection.py
@@ -21,39 +21,67 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 from w3af.plugins.tests.helper import PluginTest, PluginConfig, MockResponse
 
 
-class TestWebappDetection(PluginTest):
-    target_url = 'http://webapp-detection/'
+class TestApplicationFingerprint(PluginTest):
+    target_url = 'http://application-fingerprint/'
 
     _run_configs = {
         'cfg': {
             'target': target_url,
-            'plugins': {'infrastructure': (PluginConfig('webapp_detection'),)}
+            'plugins': {
+                'infrastructure': (PluginConfig('application_fingerprint'),)
+            }
         }
     }
 
     MOCK_RESPONSES = [
-        MockResponse('http://webapp-detection/jquery-in-content', '<script src="jquery.js"></script>'),
-        MockResponse('http://webapp-detection/apache-in-headers-no-content', '', headers={'Server': 'Apache'}),
+        MockResponse('http://application-fingerprint/jquery-in-content',
+                     '<script src="jquery.js"></script>'),
+        MockResponse('http://application-fingerprint/apache-without-content',
+                     '', headers={'Server': 'Apache'}),
     ]
+
+    def get_human_readable_info(self):
+        infos = self.kb.get('application_fingerprint',
+                            'Application fingerprint')
+
+        self.assertEqual(len(infos), 1, infos)
+        return infos[0]
+
+    def get_raw_data(self, url):
+        raw_dict = self.kb.raw_read('application_fingerprint',
+                               'application_fingerprint')
+
+        self.assertEqual(len(raw_dict.keys()), 1, raw_dict.keys())
+        self.assertIn(url, raw_dict)
+        apps = [data_dict['app'] for data_dict in raw_dict[url]]
+        return raw_dict, apps
 
     def test_apache(self):
         cfg = self._run_configs['cfg']
-        self._scan(self.target_url + 'apache-in-headers-no-content', cfg['plugins'])
+        url = self.target_url + 'apache-without-content'
+        self._scan(url, cfg['plugins'])
 
-        infos = self.kb.get('webapp_detection', 'webapp_detection')
-
-        self.assertEqual(len(infos), 1, infos)
-        info = infos[0]
+        info = self.get_human_readable_info()
 
         self.assertIn('Apache', info.get_desc())
+        self.assertNotIn('jQuery', info.get_desc())
+
+        raw_dict, apps = self.get_raw_data(url)
+
+        self.assertIn('Apache', apps)
+        self.assertNotIn('jQuery', apps)
 
     def test_jquery(self):
         cfg = self._run_configs['cfg']
-        self._scan(self.target_url + 'jquery-in-content', cfg['plugins'])
+        url = self.target_url + 'jquery-in-content'
+        self._scan(url, cfg['plugins'])
 
-        infos = self.kb.get('webapp_detection', 'webapp_detection')
-
-        self.assertEqual(len(infos), 1, infos)
-        info = infos[0]
+        info = self.get_human_readable_info()
 
         self.assertIn('jQuery', info.get_desc())
+        self.assertNotIn('Apache', info.get_desc())
+
+        raw_dict, apps = self.get_raw_data(url)
+
+        self.assertIn('jQuery', apps)
+        self.assertNotIn('Apache', apps)
