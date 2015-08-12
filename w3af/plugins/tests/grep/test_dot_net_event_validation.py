40abf2499fa9891d531d09ba0829207d84678a5e
Andres Riancho <andres.riancho@gmail.com>
2015-08-12 08:27:16 -0300

    Cosmetic

--- w3af/plugins/tests/grep/test_dot_net_event_validation.py
+++ w3af/plugins/tests/grep/test_dot_net_event_validation.py
@@ -18,19 +18,16 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
-from nose.plugins.attrib import attr
-
 from w3af.core.controllers.ci.moth import get_moth_http
 from w3af.plugins.tests.helper import PluginTest, PluginConfig
 
 
-@attr('ci_ready')
 class TestEventValidation(PluginTest):
 
     dot_net_event_validation_url = get_moth_http('/grep/dot_net_event_validation/')
 
     _run_configs = {
-        'cfg1': {
+        'cfg': {
             'target': dot_net_event_validation_url,
             'plugins': {
                 'grep': (PluginConfig('dot_net_event_validation'),),
@@ -44,23 +41,24 @@ class TestEventValidation(PluginTest):
     }
 
     def test_found_vuln(self):
-        cfg = self._run_configs['cfg1']
+        cfg = self._run_configs['cfg']
         self._scan(cfg['target'], cfg['plugins'])
         vulns = self.kb.get('dot_net_event_validation',
                             'dot_net_event_validation')
 
-        self.assertEquals(3, len(vulns), vulns)
+        expected_vulns = {('event_validation.html',
+                           'decode the viewstate contents.'),
 
-        EXPECTED_VULNS = set(
-            [('event_validation.html', 'decode the viewstate contents.'),
-             ('without_event_validation.html',
-              'decode the viewstate contents.'),
-             ('without_event_validation.html', 'r should be manually verified.')])
+                          ('without_event_validation.html',
+                           'decode the viewstate contents.'),
+
+                          ('without_event_validation.html',
+                           'r should be manually verified.')}
 
         vulns_set = set()
+
         for vuln in vulns:
             ending = vuln.get_desc(with_id=False)[-30:]
             vulns_set.add((vuln.get_url().get_file_name(), ending))
 
-        self.assertEqual(EXPECTED_VULNS,
-                         vulns_set)
+        self.assertEqual(expected_vulns, vulns_set)
