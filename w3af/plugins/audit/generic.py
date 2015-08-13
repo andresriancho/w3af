fcdd71de9ed95665a3a708c63eef67f4da389880
Andres Riancho <andres.riancho@gmail.com>
2015-08-12 23:16:53 -0300

    Fix Improve generic plugin to use long list #12125

--- w3af/plugins/audit/generic.py
+++ w3af/plugins/audit/generic.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 """
 generic.py
 
@@ -19,18 +20,19 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 """
-import copy
+from itertools import izip, repeat
 
 import w3af.core.data.kb.knowledge_base as kb
 import w3af.core.data.constants.severity as severity
 
 from w3af.core.data.options.opt_factory import opt_factory
+from w3af.core.data.options.option_types import BOOL, FLOAT
 from w3af.core.data.options.option_list import OptionList
 from w3af.core.data.fuzzer.fuzzer import create_mutants
 from w3af.core.data.fuzzer.utils import rand_number, rand_alnum
 from w3af.core.data.kb.vuln import Vuln
 from w3af.core.data.db.disk_list import DiskList
-
+from w3af.core.controllers.threads.threadpool import one_to_many
 from w3af.core.controllers.plugins.audit_plugin import AuditPlugin
 from w3af.core.controllers.misc.fuzzy_string_cmp import relative_distance
 
@@ -41,8 +43,6 @@ class generic(AuditPlugin):
     :author: Andres Riancho (andres.riancho@gmail.com)
     """
 
-    ERROR_STRINGS = ['d\'kc"z\'gj\'\"**5*(((;-*`)', '']
-
     def __init__(self):
         AuditPlugin.__init__(self)
 
@@ -51,59 +51,152 @@ class generic(AuditPlugin):
 
         #   User configured variables
         self._diff_ratio = 0.30
+        self._extensive = False
 
-    def audit(self, freq, orig_response):
+    def audit(self, freq, original_response):
         """
         Find all kind of "generic" bugs without using a fixed error database
 
         :param freq: A FuzzableRequest
         """
-        # First, get the original response and create the mutants
-        mutants = create_mutants(freq, ['', ], orig_resp=orig_response)
+        # Prevent some false positives for cases where the original response
+        # is already triggering an error
+        if original_response.get_code() == 500:
+            return
 
-        for m in mutants:
+        # Get the original response and create the mutants
+        mutants = create_mutants(freq, ['', ], orig_resp=original_response)
 
-            # First I check that the current modified parameter in the mutant
-            # doesn't have an already reported vulnerability. I don't want to
-            # report vulnerabilities more than once.
-            if (m.get_url(), m.get_token_name()) in self._potential_vulns:
+        original_response_repeat = repeat(original_response)
+        args_iterator = izip(original_response_repeat, mutants)
+        check_mutant = one_to_many(self._check_mutant)
+
+        self.worker_pool.imap_unordered(check_mutant, args_iterator)
+
+    def _check_mutant(self, original_response, mutant):
+        # First I check that the current modified parameter in the mutant
+        # doesn't have an already reported vulnerability. I don't want to
+        # report vulnerabilities more than once.
+        if self._has_potential_vuln(mutant):
+            return
+
+        # Now, we request the limit (something that doesn't exist)
+        # If http://localhost/a.php?b=1
+        #   * Then I should request b=12938795 (random number)
+        #
+        # If http://localhost/a.php?b=abc
+        #   * Then I should request b=hnv98yks (random alnum)
+        limit_response = self._get_limit_response(mutant)
+
+        # Now I request something that could generate an error
+        # If http://localhost/a.php?b=1
+        #   * Then I should request b=<payload>
+        for payload_string in self._get_payloads():
+
+            mutant.set_token_value(payload_string)
+            error_response = self._uri_opener.send_mutant(mutant)
+
+            self._analyze_responses(original_response,
+                                    limit_response,
+                                    error_response,
+                                    mutant)
+
+    def _get_payloads(self):
+        """
+        :return: A payload list, size depends on the "extensive" user configured
+                 parameter. Most payloads came from [0]!
+
+                 [0] https://github.com/minimaxir/big-list-of-naughty-strings/
+        """
+        # This is the reduced payload set which is effective in triggering
+        # most of the errors you'll find
+        payloads = [u'1/0',
+                    u'Ω≈ç√∫˜µ≤≥÷',
+                    u'<>?:"{}|_+\',./;\'[]\\-=',
+                    u'%*.*s',
+                    u'']
+
+        # Add more payloads if the user wants to perform a detailed scan
+        if self._extensive:
+            payloads += [u'undefined',
+                         u'undef',
+                         u'null',
+                         u'NULL',
+                         u'nil',
+                         u'NIL',
+                         u'true',
+                         u'false',
+                         u'True',
+                         u'False',
+                         u'None',
+                         u'-1',
+                         u'0.0/0',
+                         u'NaN',
+                         u'Infinity',
+                         u"$ENV{'HOME'}",
+                         u'00˙Ɩ$-',
+                         ]
+
+        return set(payloads)
+
+    def _add_potential_vuln(self, mutant, id_list):
+        """
+        Stores the information about the potential vulnerability
+
+        :param mutant: The mutant, containing the payload which triggered the
+                       HTTP response with the error.
+        :param id_list: The HTTP response ids associated with the error
+        :return: None
+        """
+        self._potential_vulns.append((mutant.get_url(),
+                                      mutant.get_token_name(),
+                                      mutant, id_list))
+
+    def _has_potential_vuln(self, mutant):
+        """
+        :param mutant: The mutant to verify
+        :return: True if the mutant is already tagged as a potential vuln
+        """
+        for url, token_name, stored_mutant, id_list in self._potential_vulns:
+            if mutant.get_url() != url:
                 continue
 
-            # Now, we request the limit (something that doesn't exist)
-            # If http://localhost/a.php?b=1
-            #   * Then I should request b=12938795 (random number)
-            #
-            # If http://localhost/a.php?b=abc
-            #   * Then I should request b=hnv98yks (random alnum)
-            limit_response = self._get_limit_response(m)
+            if mutant.get_token_name() != token_name:
+                continue
 
-            # Now I request something that could generate an error
-            # If http://localhost/a.php?b=1
-            #   * Then I should request b=d'kcz'gj'"**5*(((*)
-            #
-            # If http://localhost/a.php?b=abc
-            #   * Then I should request b=d'kcz'gj'"**5*(((*)
-            #
-            # I also try to trigger errors by sending empty strings
-            #     If http://localhost/a.php?b=1 ; then I should request b=
-            #     If http://localhost/a.php?b=abc ; then I should request b=
-            for error_string in self.ERROR_STRINGS:
+            return True
 
-                m.set_token_value(error_string)
-                error_response = self._uri_opener.send_mutant(m)
+        return False
 
-                # Now I compare responses
-                self._analyze_responses(orig_response,
-                                        limit_response,
-                                        error_response,
-                                        m)
-
-    def _analyze_responses(self, orig_resp, limit_response, error_response, mutant):
+    def _analyze_responses(self, orig_resp, limit_response, error_response,
+                           mutant):
         """
-        Analyze responses; if error_response doesn't look like orig_resp nor
-        limit_response, then we have a vuln.
-
+        Analyze responses using various methods.
         :return: None
+        """
+        for analyzer in {self._analyze_code, self._analyze_body}:
+            is_vuln = analyzer(orig_resp, limit_response,
+                               error_response, mutant)
+            if is_vuln:
+                break
+
+    def _analyze_code(self, orig_resp, limit_response, error_response, mutant):
+        """
+        :return: True if we found a bug using the response code
+        """
+        if error_response.get_code() == 500 and \
+           limit_response.get_code() != 500:
+
+            id_list = [orig_resp.id, limit_response.id, error_response.id]
+            self._add_potential_vuln(mutant, id_list)
+
+            return True
+
+        return False
+
+    def _analyze_body(self, orig_resp, limit_response, error_response, mutant):
+        """
+        :return: True if we found a bug by comparing the response bodies
         """
         original_to_error = relative_distance(orig_resp.get_body(),
                                               error_response.get_body())
@@ -119,18 +212,16 @@ class generic(AuditPlugin):
             # (and the error page really found the limit),
             # let's request a new limit (one that hopefully doesn't exist)
             # in order to remove some false positives
-            limit_response2 = self._get_limit_response(mutant)
+            limit_response_2 = self._get_limit_response(mutant)
+            limit_to_limit = relative_distance(limit_response_2.get_body(),
+                                               limit_response.get_body())
 
-            id_list = [orig_resp.id, limit_response.id, error_response.id]
-
-            if relative_distance(limit_response2.get_body(),
-                                 limit_response.get_body()) > 1 - self._diff_ratio:
+            if limit_to_limit > 1 - self._diff_ratio:
                 # The two limits are "equal"; It's safe to suppose that we have
                 # found the limit here and that the error string really produced
                 # an error
-                self._potential_vulns.append((mutant.get_url(),
-                                              mutant.get_token_name(),
-                                              mutant, id_list))
+                id_list = [orig_resp.id, limit_response.id, error_response.id]
+                self._add_potential_vuln(mutant, id_list)
 
     def _get_limit_response(self, mutant):
         """
@@ -180,9 +271,16 @@ class generic(AuditPlugin):
         """
         ol = OptionList()
 
-        d = ('If two strings have a diff ratio less than diff_ratio, then they'
-             '  are really different.')
-        o = opt_factory('diff_ratio', self._diff_ratio, d, 'float')
+        d = ('Ratio to use when comparing two HTTP response bodies, if two'
+             ' strings have a ratio less than diff_ratio, then they are'
+             ' really different.')
+        o = opt_factory('diff_ratio', self._diff_ratio, d, FLOAT)
+        ol.add(o)
+
+        d = ('When enabled this plugin will send an extended payload set which'
+             ' might trigger bugs and vulnerabilities which are not found by'
+             ' the default (reduced, fast) payload set.')
+        o = opt_factory('extensive', self._extensive, d, BOOL)
         ol.add(o)
 
         return ol
@@ -196,6 +294,7 @@ class generic(AuditPlugin):
         :return: No value is returned.
         """
         self._diff_ratio = options_list['diff_ratio'].get_value()
+        self._extensive = options_list['extensive'].get_value()
 
     def get_long_desc(self):
         """
@@ -203,5 +302,5 @@ class generic(AuditPlugin):
         """
         return """
         This plugin identifies unhandled Web application exceptions by sending
-        specially crafted strings to each input.
+        specially crafted strings to each application input.
         """
--- w3af/plugins/tests/audit/test_generic.py
+++ w3af/plugins/tests/audit/test_generic.py
@@ -18,35 +18,34 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
+import re
+import urllib
 
-from nose.plugins.attrib import attr
-from w3af.plugins.tests.helper import PluginTest, PluginConfig
+from w3af.plugins.tests.helper import PluginTest, PluginConfig, MockResponse
+from w3af.plugins.audit.sqli import sqli
 
 
-class TestGeneric(PluginTest):
+class TestGenericOnly(PluginTest):
 
-    target_url = 'http://moth/w3af/audit/sql_injection/select/sql_injection_integer.php'
+    target_url = 'http://mock/?id='
 
-    _run_configs = {
-        'generic_only': {
-                         'target': target_url + '?id=1',
-                         'plugins': {
-                                     'audit': (PluginConfig('generic'),),
-                                     }
-                         },
-        'generic_sqli': {
-                         'target': target_url + '?id=1',
-                         'plugins': {
-                                     'audit': (PluginConfig('generic'),
-                                               PluginConfig('sqli'),),
-                                     }
-                         }
-    }
+    class GenericErrorMockResponse(MockResponse):
+        def get_response(self, http_request, uri, response_headers):
+            uri = urllib.unquote(uri)
 
-    @attr('ci_fails')
+            if uri.endswith('1/0'):
+                body = 'Error found!'
+            else:
+                body = 'Sunny outside'
+
+            return self.status, response_headers, body
+
+    CONFIG = {'audit': (PluginConfig('generic'),)}
+    MOCK_RESPONSES = [GenericErrorMockResponse(re.compile('.*'), body=None,
+                                               method='GET', status=200)]
+
     def test_found_generic(self):
-        cfg = self._run_configs['generic_only']
-        self._scan(cfg['target'], cfg['plugins'])
+        self._scan(self.target_url, self.CONFIG)
         
         vulns = self.kb.get('generic', 'generic')
         
@@ -55,13 +54,69 @@ class TestGeneric(PluginTest):
         # Now some tests around specific details of the found vuln
         vuln = vulns[0]
         self.assertEquals('Unhandled error in web application', vuln.get_name())
-        self.assertEquals(self.target_url, str(vuln.get_url()))
+        self.assertEquals('http://mock/?id=1/0', str(vuln.get_uri()))
+        self.assertEquals(vuln.get_mutant().get_token_name(), 'id')
 
-    @attr('ci_fails')
-    def test_found_generic_not_reported(self):
-        cfg = self._run_configs['generic_sqli']
-        self._scan(cfg['target'], cfg['plugins'])
-        
+
+class TestGenericExtensive(PluginTest):
+
+    target_url = 'http://mock/?id='
+
+    class GenericErrorMockResponse(MockResponse):
+        def get_response(self, http_request, uri, response_headers):
+            uri = urllib.unquote(uri)
+
+            if uri.endswith('Infinity'):
+                body = 'Error found!'
+            else:
+                body = 'Sunny outside'
+
+            return self.status, response_headers, body
+
+    CONFIG = {'audit': (PluginConfig('generic',
+                                     ('extensive', True, PluginConfig.BOOL),),)}
+    MOCK_RESPONSES = [GenericErrorMockResponse(re.compile('.*'), body=None,
+                                               method='GET', status=200)]
+
+    def test_found_generic_extensive(self):
+        self._scan(self.target_url, self.CONFIG)
+
         vulns = self.kb.get('generic', 'generic')
-        
+
+        self.assertEquals(1, len(vulns))
+
+        # Now some tests around specific details of the found vuln
+        vuln = vulns[0]
+        self.assertEquals('Unhandled error in web application', vuln.get_name())
+        self.assertEquals('http://mock/?id=Infinity', str(vuln.get_uri()))
+        self.assertEquals(vuln.get_mutant().get_token_name(), 'id')
+
+
+class TestGenericSQLInjection(PluginTest):
+
+    target_url = 'http://mock/?id='
+
+    class SQLIMockResponse(MockResponse):
+        def get_response(self, http_request, uri, response_headers):
+            uri = urllib.unquote(uri)
+
+            if uri.endswith('1/0') or uri.endswith(sqli.SQLI_STRINGS[0]):
+                body = 'PostgreSQL query failed:'
+            else:
+                body = 'Sunny outside'
+
+            return self.status, response_headers, body
+
+    CONFIG = {'audit': (PluginConfig('generic'),
+                        PluginConfig('sqli'))}
+    MOCK_RESPONSES = [SQLIMockResponse(re.compile('.*'), body=None,
+                                       method='GET', status=200)]
+
+    def test_found_sqli_not_generic(self):
+        self._scan(self.target_url, self.CONFIG)
+
+        vulns = self.kb.get('generic', 'generic')
         self.assertEquals(0, len(vulns))
+
+        vulns = self.kb.get('sqli', 'sqli')
+        self.assertEquals(1, len(vulns))
