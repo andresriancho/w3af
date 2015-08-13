1c8965bcf41178f757b3b54c5f28151fc70b1739
Andres Riancho <andres.riancho@gmail.com>
2015-08-13 16:21:31 -0300

    Fix Add JetLeak check to w3af #8523

---
+++ w3af/plugins/infrastructure/jetleak.py
@@ -0,0 +1,68 @@
+"""
+jetleak.py
+
+Copyright 2015 Andres Riancho
+
+This file is part of w3af, http://w3af.org/ .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""
+from w3af.core.controllers.plugins.infrastructure_plugin import InfrastructurePlugin
+from w3af.core.controllers.exceptions import RunOnce
+from w3af.core.controllers.misc.decorators import runonce
+from w3af.core.data.constants.severity import HIGH
+from w3af.core.data.dc.headers import Headers
+from w3af.core.data.kb.vuln import Vuln
+
+
+class jetleak(InfrastructurePlugin):
+    """
+    Detect CVE-2015-2080 aka. JetLeak
+    :author: Andres Riancho (andres.riancho@gmail.com)
+    """
+
+    @runonce(exc_class=RunOnce)
+    def discover(self, fuzzable_request):
+        url = fuzzable_request.get_url()
+        headers = Headers([('Referer', '\x00')])
+
+        response = self._uri_opener.GET(url,
+                                        cache=False,
+                                        grep=False,
+                                        headers=headers)
+
+        if response.get_code() != 400:
+            return
+
+        if '400 Illegal character 0x0 in state' not in response.get_msg():
+            return
+
+        desc = ('The application appears to be running a version of Jetty'
+                ' vulnerable to CVE-2015-2080, which allows attackers to'
+                ' read arbitrary server memory buffers')
+
+        v = Vuln('JetLeak', desc, HIGH, response.id, self.get_name())
+        v.set_url(response.get_url())
+
+        self.kb_append_uniq(self, 'jetleak', v)
+
+    def get_long_desc(self):
+        """
+        :return: A DETAILED description of the plugin functions and features.
+        """
+        return """
+        Detect CVE-2015-2080 Jetty vulnerability also known as JetLeak
+        """
+
---
+++ w3af/plugins/tests/infrastructure/test_jetleak.py
@@ -0,0 +1,98 @@
+"""
+test_jetleak.py
+
+Copyright 2015 Andres Riancho
+
+This file is part of w3af, http://w3af.org/ .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+import re
+
+from mock import patch
+from httpretty.http import STATUSES
+from w3af.plugins.tests.helper import PluginTest, PluginConfig, MockResponse
+
+
+class TestJetLeak(PluginTest):
+
+    target_url = 'http://httpretty/'
+
+    _run_configs = {
+        'cfg': {
+            'target': target_url,
+            'plugins': {'infrastructure': (PluginConfig('jetleak'),)}
+        }
+    }
+
+    JETLEAK_ERROR = '400 Illegal character 0x0 in state'
+
+    class JettyMockResponse(MockResponse):
+        def get_response(self, http_request, uri, response_headers):
+            referer = http_request.headers.getrawheader('Referer')
+
+            if referer is not None and '\x00' in referer:
+                body = 'See HTTP reason text'
+                status = 400
+            else:
+                body = 'Regular response'
+                status = 200
+
+            return status, response_headers, body
+
+    MOCK_RESPONSES = [JettyMockResponse(re.compile('.*'), body=None,
+                                        method='GET', status=200)]
+
+    def test_vulnerable_jetty(self):
+        cfg = self._run_configs['cfg']
+
+        with patch.dict(STATUSES, {400: self.JETLEAK_ERROR}):
+            self._scan(self.target_url, cfg['plugins'])
+
+        vulns = self.kb.get('jetleak', 'jetleak')
+
+        self.assertEqual(len(vulns), 1, vulns)
+        vuln = vulns[0]
+
+        self.assertEqual(vuln.get_name(), 'JetLeak')
+
+
+class TestNoJetLeak(PluginTest):
+
+    target_url = 'http://httpretty/'
+
+    _run_configs = {
+        'cfg': {
+            'target': target_url,
+            'plugins': {'infrastructure': (PluginConfig('jetleak'),)}
+        }
+    }
+
+    class FixedJettyMockResponse(MockResponse):
+        def get_response(self, http_request, uri, response_headers):
+            body = 'Regular response'
+            status = 200
+
+            return status, response_headers, body
+
+    MOCK_RESPONSES = [FixedJettyMockResponse(re.compile('.*'), body=None,
+                                             method='GET', status=200)]
+
+    def test_fixed_jetty(self):
+        cfg = self._run_configs['cfg']
+        self._scan(self.target_url, cfg['plugins'])
+
+        vulns = self.kb.get('jetleak', 'jetleak')
+
+        self.assertEqual(len(vulns), 0, vulns)
