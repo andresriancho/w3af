40545c6018f760b9c1a50ae74a294269bf77e15c
Andres Riancho <andres.riancho@gmail.com>
2015-09-28 17:21:24 -0300

    MCIR eval tests #8694

---
+++ w3af/core/controllers/ci/mcir.py
@@ -0,0 +1,36 @@
+"""
+mcir.py
+
+Copyright 2015 Andres Riancho
+
+This file is part of w3af, http://w3af.org/ .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""
+
+HTTP_MCIR = '/tmp/mcir.txt'
+DEFAULT_MCIR = 'mcir-fallback:80'
+
+
+def get_mcir_http(path='/'):
+    try:
+        mcir_netloc = file(HTTP_MCIR).read().strip()
+    except IOError:
+        mcir_netloc = DEFAULT_MCIR
+
+    return 'http://%s%s' % (mcir_netloc, path)
+
+
+
