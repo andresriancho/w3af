228109ee70740d86df93a7fe45c1f1339b2ff019
Andres Riancho <andres.riancho@gmail.com>
2015-09-30 11:25:41 -0300

    Final fix for build fails like https://circleci.com/gh/andresriancho/w3af/2608 ?

--- w3af/tests/waitfor-test-dependencies.py
+++ w3af/tests/waitfor-test-dependencies.py
@@ -6,6 +6,7 @@ import urllib2
 
 LOOPS = 25
 DELAY = 1
+STABLE_LOOPS = 5
 
 WAVSEP_TEST_URL = ('http://127.0.0.1:8098/wavsep/active/SQL-Injection/'
                    'SInjection-Detection-Evaluation-GET-200Error/'
@@ -21,32 +22,78 @@ TEST_DEPENDENCIES = [('http://127.0.0.1:8000', None),
                      (WAVSEP_TEST_URL, 'hello user1'),
                      ('http://127.0.0.1:8090', None)]
 
-is_available = []
 
-for _ in xrange(LOOPS):
-    time.sleep(DELAY)
+def is_online(url, match_string):
+    try:
+        content = urllib2.urlopen(url).read()
+    except urllib2.HTTPError, e:
+        content = e.read()
+    except Exception, e:
+        print('%s is offline (%s)' % (url, e.__class__.__name__))
+        return False
 
-    for url, match_string in TEST_DEPENDENCIES:
-        if url in is_available:
-            continue
+    if match_string is None:
+        print('%s is UP' % url)
+        return True
 
-        try:
-            content = urllib2.urlopen(url).read()
-        except Exception, e:
-            print('%s is offline (%s)' % (url, e.__class__.__name__))
-        else:
-            if match_string is None:
-                print('%s is UP' % url)
+    elif match_string in content:
+        print('%s is UP and matches string' % url)
+        return True
+
+    else:
+        print('%s is UP but string does NOT match' % url)
+
+    return False
+
+
+def waitfor_test_dependencies():
+    print('Waiting for dependencies to be up...')
+    is_available = []
+
+    for _ in xrange(LOOPS):
+        time.sleep(DELAY)
+
+        for url, match_string in TEST_DEPENDENCIES:
+            if url in is_available:
+                continue
+
+            if is_online(url, match_string):
                 is_available.append(url)
 
-            elif match_string in content:
-                print('%s is UP and matches string' % url)
-                is_available.append(url)
+    if len(is_available) != len(TEST_DEPENDENCIES):
+        print('Timeout waiting for test dependencies!')
+        sys.exit(1)
 
+    return True
+
+
+def wait_until_stable():
+    print('Waiting for dependencies to be stable...')
+    test_results = {}
+
+    for _ in xrange(LOOPS):
+        time.sleep(DELAY)
+
+        for url, match_string in TEST_DEPENDENCIES:
+            if is_online(url, match_string):
+                if url in test_results:
+                    test_results[url] += 1
+                else:
+                    test_results[url] = 1
             else:
-                print('%s is UP but string does NOT match' % url)
+                test_results[url] = 0
 
+    up_count_summary = []
 
-if len(is_available) != len(TEST_DEPENDENCIES):
-    print('Timeout waiting for test dependencies!')
-    sys.exit(1)
+    for up_count in test_results.itervalues():
+        up_count_summary.append(up_count >= STABLE_LOOPS)
+
+    if not all(up_count_summary):
+        print('Test dependencies are not stable')
+        sys.exit(1)
+
+    return True
+
+if __name__ == '__main__':
+    waitfor_test_dependencies()
+    wait_until_stable()
