4148909709016197a2573baba9b2d1417aa3635a
Andres Riancho <andres.riancho@gmail.com>
2017-01-03 09:55:48 -0300

    Adding support for ubuntu 16.04

--- w3af/core/controllers/dependency_check/platforms/current_platform.py
+++ w3af/core/controllers/dependency_check/platforms/current_platform.py
@@ -22,6 +22,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 from .ubuntu1204 import Ubuntu1204
 from .ubuntu1404 import Ubuntu1404
 from .ubuntu1410 import Ubuntu1410
+from .ubuntu1604 import Ubuntu1604
 from .debian76 import Debian76
 from .debian78 import Debian78
 from .debian80 import Debian80
@@ -38,7 +39,7 @@ from .default import DefaultPlatform
 
 KNOWN_PLATFORMS = [Debian76, Debian78, Debian80, Ubuntu1204, CentOS65, CentOS,
                    Fedora, Kali, Kali2, MacOSX, OpenBSD5, SuSE, Ubuntu1404,
-                   Ubuntu1410, ElementaryOS02]
+                   Ubuntu1410, ElementaryOS02, Ubuntu1604]
 
 
 def get_current_platform(known_platforms=KNOWN_PLATFORMS):
