69ff07c2bf368ebc0d6f0700a9735451ed851999
Andres Riancho <andres.riancho@gmail.com>
2018-01-02 14:58:58 -0300

    This commit fixes an issue introduced when we added more payloads to the global_redirect plugin: before we used 2 HTTP requests per parameter, after the improvement we used ~10.
    More requests mean more coverage, and more chances of identifying a vulnerability. But also mean long scan times.
    
    I changed a couple of things in the plugin in order to decide when it makes sense to send all the "extended payload set". Also added more tests to make sure everything is working.

--- w3af/plugins/tests/audit/test_global_redirect.py
+++ w3af/plugins/tests/audit/test_global_redirect.py
@@ -18,31 +18,39 @@ You should have received a copy of the GNU General Public License
 along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
+from unittest import TestCase
+
 from w3af.core.controllers.ci.moth import get_moth_http
-from w3af.plugins.tests.helper import PluginTest, PluginConfig
+from w3af.plugins.tests.helper import PluginTest, PluginConfig, MockResponse
+from w3af.plugins.audit.global_redirect import global_redirect
+from w3af.core.data.url.HTTPResponse import HTTPResponse
+from w3af.core.data.parsers.doc.url import URL
+from w3af.core.data.dc.headers import Headers
+
+
+SCAN_CONFIG = {
+    'cfg': {
+        'target': None,
+        'plugins': {
+            'audit': (PluginConfig('global_redirect'),),
+            'crawl': (
+                PluginConfig(
+                    'web_spider',
+                    ('only_forward', True, PluginConfig.BOOL)),
+            )
+
+        }
+    },
+}
 
 
 class TestGlobalRedirect(PluginTest):
 
     target_url = get_moth_http('/audit/global_redirect/')
 
-    _run_configs = {
-        'cfg': {
-            'target': target_url,
-            'plugins': {
-                'audit': (PluginConfig('global_redirect'),),
-                'crawl': (
-                    PluginConfig(
-                        'web_spider',
-                        ('only_forward', True, PluginConfig.BOOL)),
-                )
-
-            }
-        },
-    }
-
     def test_found_redirect(self):
-        cfg = self._run_configs['cfg']
+        cfg = SCAN_CONFIG['cfg']
+        cfg['target'] = self.target_url
         self._scan(cfg['target'], cfg['plugins'])
 
         vulns = self.kb.get('global_redirect', 'global_redirect')
@@ -59,3 +67,193 @@ class TestGlobalRedirect(PluginTest):
         ]
 
         self.assertExpectedVulnsFound(EXPECTED, vulns)
+
+
+class TestGlobalRedirectBasic(PluginTest):
+
+    target_url = 'http://httpretty'
+
+    MOCK_RESPONSES = [MockResponse('http://httpretty/',
+                                   '<a href="/redir?target=">redirect</a>'),
+                      MockResponse('http://httpretty/redir?target=',
+                                   'No redirect'),
+                      MockResponse('http://httpretty/redir?target=http://www.w3af.org/',
+                                   status=302,
+                                   headers={'Location': 'https://www.w3af.org/'},
+                                   body='')]
+
+    def test_original_response_has_no_redirect(self):
+        cfg = SCAN_CONFIG['cfg']
+        cfg['target'] = self.target_url
+
+        self._scan(cfg['target'], cfg['plugins'])
+
+        vulns = self.kb.get('global_redirect', 'global_redirect')
+        expected = [('redir', 'target')]
+
+        self.assertAllVulnNamesEqual('Insecure redirection', vulns)
+        self.assertExpectedVulnsFound(expected, vulns)
+
+
+class TestGlobalRedirectBasicWithMetaRedir(PluginTest):
+
+    target_url = 'http://httpretty'
+
+    MOCK_RESPONSES = [MockResponse('http://httpretty/',
+                                   '<a href="/redir?target=">redirect</a>'),
+                      MockResponse('http://httpretty/redir?target=',
+                                   '<meta http-equiv="refresh" content="0; url=">'),
+                      MockResponse('http://httpretty/redir?target=http://www.w3af.org/',
+                                   body='<meta http-equiv="refresh" content="0; url=http://www.w3af.org/">')]
+
+    def test_original_response_has_meta_redirect(self):
+        cfg = SCAN_CONFIG['cfg']
+        cfg['target'] = self.target_url
+
+        self._scan(cfg['target'], cfg['plugins'])
+
+        vulns = self.kb.get('global_redirect', 'global_redirect')
+        expected = [('redir', 'target')]
+
+        self.assertAllVulnNamesEqual('Insecure redirection', vulns)
+        self.assertExpectedVulnsFound(expected, vulns)
+
+
+class TestGlobalRedirectExtendedPayloadSet(PluginTest):
+
+    target_url = 'http://httpretty'
+
+    MOCK_RESPONSES = [MockResponse('http://httpretty/',
+                                   '<a href="/redir?target=">redirect</a>'),
+                      MockResponse('http://httpretty/redir?target=',
+                                   status=302,
+                                   headers={'Location': 'http://httpretty/default'},
+                                   body=''),
+                      MockResponse('http://httpretty/redir?target=//httpretty.w3af.org/',
+                                   status=302,
+                                   headers={'Location': 'httpretty.w3af.org'},
+                                   body='')]
+
+    def test_original_response_has_redirect(self):
+        cfg = SCAN_CONFIG['cfg']
+        cfg['target'] = self.target_url
+
+        self._scan(cfg['target'], cfg['plugins'])
+
+        vulns = self.kb.get('global_redirect', 'global_redirect')
+        expected = [('redir', 'target')]
+
+        self.assertAllVulnNamesEqual('Insecure redirection', vulns)
+        self.assertExpectedVulnsFound(expected, vulns)
+
+
+class TestGlobalRedirectUnitExtractScript(TestCase):
+    def test_extract_script_code_simple(self):
+        plugin = global_redirect()
+
+        body = '<script>var x=1;var y=2;</script>'
+        url = URL('http://www.w3af.com/')
+        headers = Headers([('content-type', 'text/html')])
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        code = plugin._extract_script_code(resp)
+        code = [c for c in code]
+
+        self.assertEqual(code, [u'var x=1', u'var y=2'])
+
+    def test_extract_script_code_new_line(self):
+        plugin = global_redirect()
+
+        body = '<script>var x=1;\nvar y=2;alert(1)</script>'
+        url = URL('http://www.w3af.com/')
+        headers = Headers([('content-type', 'text/html')])
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        code = plugin._extract_script_code(resp)
+        code = [c for c in code]
+
+        self.assertEqual(code, [u'var x=1', u'var y=2', u'alert(1)'])
+
+
+class TestGlobalRedirectUnitJSRedirect(TestCase):
+    def test_javascript_redirect_simple(self):
+        plugin = global_redirect()
+
+        body = '<script>window.location = "http://w3af.org/"</script>'
+        url = URL('http://www.w3af.com/')
+        headers = Headers([('content-type', 'text/html')])
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        self.assertTrue(plugin._javascript_redirect(resp))
+
+    def test_javascript_redirect_assign(self):
+        plugin = global_redirect()
+
+        body = '<script>window.location.assign("http://www.w3af.org")</script>'
+        url = URL('http://www.w3af.com/')
+        headers = Headers([('content-type', 'text/html')])
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        self.assertTrue(plugin._javascript_redirect(resp))
+
+
+class TestGlobalRedirectUnitResponseHasRedirect(TestCase):
+    def test_response_has_redirect_headers(self):
+        plugin = global_redirect()
+
+        body = ''
+        url = URL('http://www.w3af.com/')
+        headers = Headers([('content-type', 'text/html'), ('Location',  'http://w3af.org')])
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        self.assertTrue(plugin._response_has_redirect(resp))
+
+    def test_response_has_redirect_meta(self):
+        plugin = global_redirect()
+
+        body = '<meta http-equiv="refresh" content="0; url=">'
+        url = URL('http://www.w3af.com/')
+        headers = Headers([('content-type', 'text/html')])
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        self.assertTrue(plugin._response_has_redirect(resp))
+
+    def test_response_has_redirect_js_1(self):
+        plugin = global_redirect()
+
+        body = '<script>window.location.assign("http://www.w3af.org")</script>'
+        url = URL('http://www.w3af.com/')
+        headers = Headers([('content-type', 'text/html')])
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        self.assertTrue(plugin._response_has_redirect(resp))
+
+    def test_response_has_redirect_js_2(self):
+        plugin = global_redirect()
+
+        body = '<script>window.location.href = "http://www.w3af.org"</script>'
+        url = URL('http://www.w3af.com/')
+        headers = Headers([('content-type', 'text/html')])
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        self.assertTrue(plugin._response_has_redirect(resp))
+
+    def test_response_has_redirect_js_false(self):
+        plugin = global_redirect()
+
+        body = '<script>alert(window.location)</script>'
+        url = URL('http://www.w3af.com/')
+        headers = Headers([('content-type', 'text/html')])
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        self.assertFalse(plugin._response_has_redirect(resp))
+
+    def test_response_has_redirect_headers_false(self):
+        plugin = global_redirect()
+
+        body = '<meta generator="">'
+        url = URL('http://www.w3af.com/')
+        headers = Headers([('content-type', 'text/html')])
+        resp = HTTPResponse(200, body, headers, url, url, _id=1)
+
+        self.assertFalse(plugin._response_has_redirect(resp))
