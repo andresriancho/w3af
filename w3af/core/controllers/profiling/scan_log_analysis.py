aebe9b274ae8550f963658c9624b8eccb1d452d1
Andres Riancho <andres.riancho@gmail.com>
2018-01-10 11:57:44 -0300

    Adding HTTP requests over time

--- w3af/core/controllers/profiling/scan_log_analysis.py
+++ w3af/core/controllers/profiling/scan_log_analysis.py
@@ -87,6 +87,7 @@ def show_scan_stats(scan):
     show_total_http_requests(scan)
     show_rtt_histo(scan)
     show_timeout(scan)
+    show_http_requests_over_time(scan)
 
     print('')
 
@@ -109,6 +110,50 @@ def show_scan_finished_in(scan):
         if match:
             print(match.group(0))
             break
+
+
+def show_http_requests_over_time(scan):
+    scan.seek(0)
+    requests_by_minute = []
+    requests_in_this_minute = 0
+
+    line = scan.readline()
+    prev_line_epoch = get_line_epoch(line)
+
+    for line in scan:
+
+        match = HTTP_CODE_RE.search(line)
+        if match:
+            requests_in_this_minute += 1
+
+        try:
+            cur_line_epoch = get_line_epoch(line)
+        except InvalidTimeStamp:
+            continue
+
+        if cur_line_epoch - prev_line_epoch > 60:
+            prev_line_epoch = cur_line_epoch
+            requests_by_minute.append(requests_in_this_minute)
+            requests_in_this_minute = 0
+
+    print('HTTP requests sent by minute')
+    print('')
+
+    fig = plotille.Figure()
+    fig.width = 90
+    fig.height = 20
+    fig.y_label = 'HTTP requests'
+    fig.x_label = 'Time'
+    fig.color_mode = 'byte'
+    fig.set_x_limits(min_=0, max_=None)
+    fig.set_y_limits(min_=0, max_=None)
+
+    fig.plot(xrange(len(requests_by_minute)),
+             requests_by_minute)
+
+    print(fig.show())
+    print('')
+    print('')
 
 
 def show_timeout(scan):
