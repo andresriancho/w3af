7b505a93212c70dab45fb2fc3715b183556f0af0
Andres Riancho <andres.riancho@gmail.com>
2018-02-16 10:27:25 -0300

    Added helper function that decodes b64 without padding

---
+++ w3af/core/data/misc/base64_nopadding.py
@@ -0,0 +1,13 @@
+import base64
+
+
+def decode_base64(data):
+    """Decode base64, padding being optional.
+
+    :param data: Base64 data as an ASCII byte string
+    :returns: The decoded byte string.
+    """
+    missing_padding = len(data) % 4
+    if missing_padding != 0:
+        data += b'=' * (4 - missing_padding)
+    return base64.decodestring(data)
