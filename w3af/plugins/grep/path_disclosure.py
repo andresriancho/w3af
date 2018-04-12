1c6c387ee78f30a1f9b8a3edf095ff0089e1c56d
Andres Riancho <self@andresriancho.com>
2018-04-12 15:43:29 -0300

    Minor perf improvement

--- w3af/plugins/grep/path_disclosure.py
+++ w3af/plugins/grep/path_disclosure.py
@@ -75,13 +75,14 @@ class path_disclosure(GrepPlugin):
 
         if self.find_path_disclosure(request, response):
             self._update_kb_path_list()
-        
+
     def find_path_disclosure(self, request, response):
         """
         Actually find the path disclosure vulnerabilities
         """
-        body_text = response.get_body()
         match_list = []
+        body_text = response.get_body()
+        real_url = response.get_url().url_decode()
 
         for match, _, _ in self._signature_re.query(body_text):
             match_list.append(match.group(1))
@@ -89,7 +90,6 @@ class path_disclosure(GrepPlugin):
         # Sort by the longest match, this is needed for filtering out
         # some false positives please read the note below.
         match_list.sort(longest_cmp)
-        real_url = response.get_url().url_decode()
 
         for match in match_list:
             # Avoid duplicated reports
@@ -124,11 +124,9 @@ class path_disclosure(GrepPlugin):
         if request.sent(match):
             return True
 
-        if self._is_attr_value(match, response):
-            return True
-
         # https://github.com/andresriancho/w3af/issues/6640
         url_list = kb.kb.get_all_known_urls()
+
         for url in url_list:
             path_and_file = url.get_path()
             if match == path_and_file:
@@ -137,26 +135,66 @@ class path_disclosure(GrepPlugin):
         # There is a rare bug also, which is triggered in cases like this one:
         #
         #   >>> import re
+        #
         #   >>> re.findall('/var/www/.*','/var/www/foobar/htdocs/article.php')
         #   ['/var/www/foobar/htdocs/article.php']
+        #
         #   >>> re.findall('/htdocs/.*','/var/www/foobar/htdocs/article.php')
         #   ['/htdocs/article.php']
-        #   >>>
         #
-        #   What I need to do here, is to keep the longest match.
+        # What I need to do here, is to keep the longest match.
         for real_url_reported, match_reported in self._reported:
             if match_reported.endswith(match):
-                break
-        else:
-            # Note to self: I get here when "break" is NOT executed.
-            # It's a new one, report!
-            return False
+                return True
 
-        return True
+        # Check if the match we got is part of a tag attribute value
+        #
+        # This part of the function is the one that consumes the most CPU usage
+        # thus we run it last, hoping that at least one of the methods we
+        # implemented above tags this match as a false positive and we don't
+        # have to run the expensive method
+        if self._is_attr_value(match, response):
+            return True
+
+        return False
 
     def _is_attr_value(self, path_disclosure_string, response):
         """
         This method was created to remove some false positives.
+
+        This method consumes 99% of the CPU usage of the plugin, but there
+        are only a few improvements that come to mind:
+
+            * Run the code that checks if the value is in the attributes
+              in the subprocess. The performance of this plugin will be
+              slightly improved.
+
+            * Before calling the document parser check at least it looks like
+              the path_disclosure_string is part of an attribute value using
+              a regular expression such as:
+
+                </?\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[\^'">\s]+))?)+\s*|\s*)/?>
+
+                (I just need to add the path_disclosure_string somewhere there)
+
+              At some point I was using a similar approach [0] but it seems
+              that it was slow? (I doubt that it will be slower than parsing
+              the response with lxml).
+
+              Something that could be done, and given that we know that this
+              is an HTML string is:
+
+                - Find all places in the response where path_disclosure_string
+                  appears
+
+                - Create 'HTTP response snippets' with the locations of
+                  path_disclosure_string +/- 500 strings.
+
+                - Apply the regular expression over those strings only, avoiding
+                  the cost of applying the regex to the whole HTML response
+
+        [0] https://github.com/andresriancho/w3af/commit/f1029328fcaf7e790cc317701b63954c55a3f4c8
+        [1] https://haacked.com/archive/2004/10/25/usingregularexpressionstomatchhtml.aspx/
 
         :return: True if path_disclosure_string is the value of an attribute
                  inside a tag.
