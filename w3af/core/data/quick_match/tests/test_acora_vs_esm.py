ca11a500dcd267473f675fd02c4ab9944e639188
Andres Riancho <self@andresriancho.com>
2018-05-07 18:06:28 -0300

    Remove old and unused lib import (leave just comment for testing)

--- w3af/core/data/quick_match/tests/test_acora_vs_esm.py
+++ w3af/core/data/quick_match/tests/test_acora_vs_esm.py
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 import unittest
 import esm
-import ahocorasick
+#import ahocorasick
 
 from acora import AcoraBuilder, PyAcora
 from nose.plugins.skip import SkipTest
