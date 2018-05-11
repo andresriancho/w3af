0d24b715da5a9bef70b7c8544bef70b3fc453a90
Andres Riancho <self@andresriancho.com>
2018-05-11 09:50:36 -0300

    Improved serialization for performance. I would like to do the same with parsers but they are very complex... at the same time they are the ones that get serialized / deserialized the most...

--- w3af/core/data/parsers/utils/response_uniq_id.py
+++ w3af/core/data/parsers/utils/response_uniq_id.py
@@ -1,5 +1,5 @@
 """
-request_uniq_id.py
+response_uniq_id.py
 
 Copyright 2015 Andres Riancho
 
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 import zlib
 
 
-def get_request_unique_id(http_response, prepend=None):
+def get_response_unique_id(http_response, prepend=None):
     """
     Before I used md5, but I realized that it was unnecessary. I
     experimented a little bit with python's hash functions and the builtin
@@ -44,6 +44,35 @@ def get_request_unique_id(http_response, prepend=None):
         body_str = body_str.encode('utf-8', 'replace')
 
     _to_hash = body_str + uri_str
+
+    # Added adler32 after finding some hash() collisions in builds
+    hash_string = str(hash(_to_hash))
+    hash_string += str(zlib.adler32(_to_hash))
+
+    if prepend:
+        hash_string = '%s-%s' % (prepend, hash_string)
+
+    return hash_string
+
+
+def get_body_unique_id(http_response, prepend=None):
+    """
+    Before I used md5, but I realized that it was unnecessary. I
+    experimented a little bit with python's hash functions and the builtin
+    hash was the fastest.
+
+    At first I thought that the built-in hash wasn't good enough, as it
+    could create collisions... but... the real probability of a collision
+    in the way I'm using this is too low.
+
+    :return: The key to be used in the self._processes to identify the
+             working parsers
+    """
+    body_str = http_response.body
+    if isinstance(body_str, unicode):
+        body_str = body_str.encode('utf-8', 'replace')
+
+    _to_hash = body_str
 
     # Added adler32 after finding some hash() collisions in builds
     hash_string = str(hash(_to_hash))
