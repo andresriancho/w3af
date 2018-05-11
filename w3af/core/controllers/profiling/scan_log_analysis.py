e3a72b22e3292ef5f3170f2ca5122f2d6a588af4
Andres Riancho <self@andresriancho.com>
2018-05-11 15:52:50 -0300

    Adding analysis of known teardown issue which is hard to find/reproduce

--- w3af/core/controllers/profiling/scan_log_analysis.py
+++ w3af/core/controllers/profiling/scan_log_analysis.py
@@ -168,6 +168,7 @@ def show_scan_stats(scan):
     print('')
 
     show_freeze_locations(scan)
+    show_known_problems(scan)
 
 
 def show_extended_urllib_error_rate(scan):
@@ -1041,6 +1042,40 @@ def show_total_http_requests(scan):
         print('    Sent %s HTTP requests which returned code %s' % (code, num))
 
     print('')
+
+
+def show_known_problems(scan):
+    """
+    This will query the log for some known issues and if those appear show
+    alerts in the output.
+
+    :param scan: The file handler for the scan log
+    :return: None, all printed to the output
+    """
+    scan.seek(0)
+
+    #
+    #   Identify a problem I rarely see: grep plugin finishes and other plugins
+    #   are still running. This seems to be an issue in the teardown process.
+    #
+    found_grep_teardown = None
+
+    grep_teardown = 'Finished Grep consumer _teardown'
+    discover_call = '.discover(uri='
+
+    for line in scan:
+        if grep_teardown in line:
+            found_grep_teardown = line
+            continue
+
+        if discover_call in line and found_grep_teardown:
+            print('Known issue found!')
+            print('')
+            print('The grep consumer was finished at:')
+            print('    %s' % found_grep_teardown)
+            print('But calls to discover were found after:')
+            print('    %s' % line)
+            break
 
 
 def show_freeze_locations(scan):
