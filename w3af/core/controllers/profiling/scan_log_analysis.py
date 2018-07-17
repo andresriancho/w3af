bdad28eb844ccbd29cc0fb2aab3202dd2a6c8e0b
Andres Riancho <andres.riancho@gmail.com>
2018-07-17 18:15:27 -0300

    Show chrome pool data

--- w3af/core/controllers/profiling/scan_log_analysis.py
+++ w3af/core/controllers/profiling/scan_log_analysis.py
@@ -77,6 +77,7 @@ PARSER_PROCESS_MEMORY_LIMIT = re.compile('Using RLIMIT_AS memory usage limit (.*
 
 GREP_PLUGIN_RE = re.compile('\] (.*?).grep\(uri=".*"\) took (.*?)s to run')
 
+CHROME_POOL_SIZE = re.compile('Chrome pool stats \(free:(.*?) / in_use:(.*?) / max:(.*?)\)')
 CHROME_POOL_TIMES = re.compile('ChromePool.get\(\) took (.*?) seconds to return an instance')
 CHROME_CRAWL_TIMES = re.compile('Extracted .*? new HTTP requests from .*? in (.*?) seconds using')
 
@@ -156,6 +157,7 @@ def show_scan_stats(scan):
     print('')
 
     show_chrome_pool_times(scan)
+    show_chrome_pool_size(scan)
     show_chrome_crawl_times(scan)
 
     print('')
@@ -955,6 +957,57 @@ def show_chrome_pool_times(scan):
              label='Chrome pool')
 
     print(fig.show())
+    print('')
+    print('')
+
+
+def show_chrome_pool_size(scan):
+    scan.seek(0)
+
+    chrome_pool_free = []
+    chrome_pool_used = []
+    chrome_pool_timestamps = []
+
+    for line in scan:
+        match = CHROME_POOL_SIZE.search(line)
+        if match:
+            chrome_pool_free.append(int(match.group(1)))
+            chrome_pool_used.append(int(match.group(2)))
+            chrome_pool_timestamps.append(get_line_epoch(line))
+
+    # Get the last timestamp to use as max in the graphs
+    first_timestamp = get_first_timestamp(scan)
+    last_timestamp = get_last_timestamp(scan)
+    spent_time_epoch = last_timestamp - first_timestamp
+    chrome_pool_timestamps = [ts - first_timestamp for ts in chrome_pool_timestamps]
+
+    if not chrome_pool_timestamps:
+        print('No chrome pool size data found')
+        return
+
+    print('Chrome pool size')
+    print('')
+
+    fig = plotille.Figure()
+    fig.width = 90
+    fig.height = 20
+    fig.register_label_formatter(float, _num_formatter)
+    fig.register_label_formatter(int, _num_formatter)
+    fig.y_label = 'Pool size'
+    fig.x_label = 'Time'
+    fig.color_mode = 'byte'
+    fig.set_x_limits(min_=0, max_=spent_time_epoch)
+    fig.set_y_limits(min_=0, max_=None)
+
+    fig.plot(chrome_pool_timestamps,
+             chrome_pool_free,
+             label='Free chrome instances')
+
+    fig.plot(chrome_pool_timestamps,
+             chrome_pool_used,
+             label='Used chrome instances')
+
+    print(fig.show(legend=True))
     print('')
     print('')
 
