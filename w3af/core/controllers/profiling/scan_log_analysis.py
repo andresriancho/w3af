d9846570c84c8a3b67f7a29bbd3b12554d5b0e71
Andres Riancho <andres.riancho@gmail.com>
2018-07-21 18:38:47 -0300

    Draw graphs for each phase and one for the whole scan

--- w3af/core/controllers/profiling/scan_log_analysis.py
+++ w3af/core/controllers/profiling/scan_log_analysis.py
@@ -77,7 +77,9 @@ PARSER_PROCESS_MEMORY_LIMIT = re.compile('Using RLIMIT_AS memory usage limit (.*
 
 GREP_PLUGIN_RE = re.compile('\] (.*?).grep\(uri=".*"\) took (.*?)s to run')
 
-SCAN_PROGRESS = re.compile('The scan will finish in .*? \((.*?)% done\)')
+SCAN_PROGRESS = re.compile('The scan will finish in .*? seconds \((.*?)% done\)')
+CALCULATED_ETA = re.compile('Calculated (.*?) ETA: (.*?) seconds')
+CRAWL_INFRA_FINISHED = 'Producer CrawlInfra has finished'
 
 
 def _num_formatter(val, chars, delta, left=False):
@@ -788,6 +790,77 @@ def show_rtt_histo(scan):
 
 
 def show_progress_delta(scan):
+    first_timestamp = get_first_timestamp(scan)
+
+    #
+    # Find the end times for crawl, audit, grep
+    #
+    scan.seek(0)
+
+    crawl_end_timestamp = None
+    audit_end_timestamp = None
+    grep_end_timestamp = None
+
+    for line in scan:
+        if CRAWL_INFRA_FINISHED in line:
+            crawl_end_timestamp = get_line_epoch(line)
+
+        if 'seconds to join' not in line:
+            continue
+
+        match = JOIN_TIMES.search(line)
+        if match:
+            if 'audit' in line.lower():
+                audit_end_timestamp = get_line_epoch(line)
+            if 'grep' in line.lower():
+                grep_end_timestamp = get_line_epoch(line)
+
+    #
+    # Find the crawl, audit and grep progress estimations
+    #
+    scan.seek(0)
+
+    crawl_progress = []
+    crawl_progress_timestamps = []
+
+    audit_progress = []
+    audit_progress_timestamps = []
+
+    grep_progress = []
+    grep_progress_timestamps = []
+
+    for line in scan:
+        match = CALCULATED_ETA.search(line)
+        if match:
+            ts = get_line_epoch(line)
+
+            eta = match.group(2)
+            if eta == 'None':
+                eta = '0.0'
+
+            eta = float(eta)
+            percentage = (ts - first_timestamp) / (ts - first_timestamp + eta) * 100
+
+            if 'crawl' in line.lower():
+                crawl_progress_timestamps.append(ts)
+                crawl_progress.append(percentage)
+
+            if 'audit' in line.lower():
+                audit_progress_timestamps.append(ts)
+                audit_progress.append(percentage)
+
+            if 'grep' in line.lower():
+                grep_progress_timestamps.append(ts)
+                grep_progress.append(percentage)
+
+    # Make the timestamps relative to the scan finish
+    crawl_progress_timestamps = [ts - first_timestamp for ts in crawl_progress_timestamps]
+    audit_progress_timestamps = [ts - first_timestamp for ts in audit_progress_timestamps]
+    grep_progress_timestamps = [ts - first_timestamp for ts in grep_progress_timestamps]
+
+    #
+    # Find the overall progress estimations
+    #
     scan.seek(0)
 
     progress = []
@@ -800,7 +873,6 @@ def show_progress_delta(scan):
             progress_timestamps.append(get_line_epoch(line))
 
     # Get the last timestamp to use as max in the graphs
-    first_timestamp = get_first_timestamp(scan)
     progress_timestamps = [ts - first_timestamp for ts in progress_timestamps]
 
     if not progress:
@@ -826,6 +898,111 @@ def show_progress_delta(scan):
     print('Progress delta (estimated vs. real)')
     print('')
 
+    if crawl_progress:
+        fig = plotille.Figure()
+        fig.width = 90
+        fig.height = 20
+        fig.register_label_formatter(float, _num_formatter)
+        fig.register_label_formatter(int, _num_formatter)
+        fig.y_label = 'Progress'
+        fig.x_label = 'Time'
+        fig.color_mode = 'byte'
+        fig.set_x_limits(min_=0, max_=spent_time_epoch)
+        fig.set_y_limits(min_=0, max_=None)
+
+        fig.plot(crawl_progress_timestamps,
+                 crawl_progress,
+                 label='Crawl (estimated)')
+
+        crawl_real_spent = int(crawl_end_timestamp) - int(first_timestamp)
+        crawl_real_progress_timestamps = range(int(first_timestamp),
+                                               int(crawl_end_timestamp),
+                                               1)
+        crawl_real_progress_timestamps = [ts - first_timestamp for ts in crawl_real_progress_timestamps]
+
+        crawl_real_progress = []
+
+        for ts in crawl_real_progress_timestamps:
+            crawl_real_progress.append(float(ts) / crawl_real_spent * 100)
+
+        fig.plot(crawl_real_progress_timestamps,
+                 crawl_real_progress,
+                 label='Crawl (real)')
+
+        print(fig.show(legend=True))
+        print('')
+        print('')
+
+    if audit_progress:
+        fig = plotille.Figure()
+        fig.width = 90
+        fig.height = 20
+        fig.register_label_formatter(float, _num_formatter)
+        fig.register_label_formatter(int, _num_formatter)
+        fig.y_label = 'Progress'
+        fig.x_label = 'Time'
+        fig.color_mode = 'byte'
+        fig.set_x_limits(min_=0, max_=spent_time_epoch)
+        fig.set_y_limits(min_=0, max_=None)
+
+        fig.plot(audit_progress_timestamps,
+                 audit_progress,
+                 label='Audit (estimated)')
+
+        audit_real_spent = int(audit_end_timestamp) - int(first_timestamp)
+        audit_real_progress_timestamps = range(int(first_timestamp),
+                                               int(audit_end_timestamp),
+                                               1)
+        audit_real_progress_timestamps = [ts - first_timestamp for ts in audit_real_progress_timestamps]
+
+        audit_real_progress = []
+
+        for ts in audit_real_progress_timestamps:
+            audit_real_progress.append(float(ts) / audit_real_spent * 100)
+
+        fig.plot(audit_real_progress_timestamps,
+                 audit_real_progress,
+                 label='Audit (real)')
+
+        print(fig.show(legend=True))
+        print('')
+        print('')
+
+    if grep_progress:
+        fig = plotille.Figure()
+        fig.width = 90
+        fig.height = 20
+        fig.register_label_formatter(float, _num_formatter)
+        fig.register_label_formatter(int, _num_formatter)
+        fig.y_label = 'Progress'
+        fig.x_label = 'Time'
+        fig.color_mode = 'byte'
+        fig.set_x_limits(min_=0, max_=spent_time_epoch)
+        fig.set_y_limits(min_=0, max_=None)
+
+        fig.plot(grep_progress_timestamps,
+                 grep_progress,
+                 label='Grep (estimated)')
+
+        grep_real_spent = int(grep_end_timestamp) - int(first_timestamp)
+        grep_real_progress_timestamps = range(int(first_timestamp),
+                                              int(grep_end_timestamp),
+                                              1)
+        grep_real_progress_timestamps = [ts - first_timestamp for ts in grep_real_progress_timestamps]
+
+        grep_real_progress = []
+
+        for ts in grep_real_progress_timestamps:
+            grep_real_progress.append(float(ts) / grep_real_spent * 100)
+
+        fig.plot(grep_real_progress_timestamps,
+                 grep_real_progress,
+                 label='Grep (real)')
+
+        print(fig.show(legend=True))
+        print('')
+        print('')
+
     fig = plotille.Figure()
     fig.width = 90
     fig.height = 20
@@ -835,11 +1012,11 @@ def show_progress_delta(scan):
     fig.x_label = 'Time'
     fig.color_mode = 'byte'
     fig.set_x_limits(min_=0, max_=spent_time_epoch)
-    fig.set_y_limits(min_=0, max_=100)
+    fig.set_y_limits(min_=0, max_=None)
 
     fig.plot(progress_timestamps,
              progress,
-             label='Estimated')
+             label='Overall (estimated)')
 
     real_progress = []
     for ts in progress_timestamps:
@@ -848,7 +1025,7 @@ def show_progress_delta(scan):
 
     fig.plot(progress_timestamps,
              real_progress,
-             label='Real')
+             label='Overall (real)')
 
     print(fig.show(legend=True))
     print('')
