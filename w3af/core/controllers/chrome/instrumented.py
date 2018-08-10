7363d1820af0a2463458ae8024bc5064ee50d88c
Andres Riancho <andres.riancho@gmail.com>
2018-08-10 08:47:00 -0300

    Disable ugly logging to stdout used by websocket

--- w3af/core/controllers/chrome/instrumented.py
+++ w3af/core/controllers/chrome/instrumented.py
@@ -19,6 +19,9 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 """
+import logging
+
+from contextlib import contextmanager
 from requests import ConnectionError
 
 import w3af.core.controllers.output_manager as om
@@ -203,7 +206,8 @@ class InstrumentedChrome(object):
             om.out.debug(msg % args)
 
         try:
-            self.chrome_conn.close()
+            with all_logging_disabled:
+                self.chrome_conn.close()
         except Exception, e:
             msg = 'Failed to close chrome connection, exception: "%s" (did: %s)'
             args = (e, self.debugging_id)
@@ -266,3 +270,27 @@ class InstrumentedChrome(object):
 
 class InstrumentedChromeException(Exception):
     pass
+
+
+@contextmanager
+def all_logging_disabled(highest_level=logging.CRITICAL):
+    """
+    A context manager that will prevent any logging messages
+    triggered during the body from being processed.
+
+    :param highest_level: The maximum logging level in use.
+                          This would only need to be changed if a custom level
+                          greater than CRITICAL is defined.
+    """
+    # two kind-of hacks here:
+    #    * can't get the highest logging level in effect => delegate to the user
+    #    * can't get the current module-level override => use an undocumented
+    #       (but non-private!) interface
+    previous_level = logging.root.manager.disable
+
+    logging.disable(highest_level)
+
+    try:
+        yield
+    finally:
+        logging.disable(previous_level)
