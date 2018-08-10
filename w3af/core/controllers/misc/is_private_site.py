733c51212528af790e760e674b4705d09e19b171
Andres Riancho <andres.riancho@gmail.com>
2018-08-10 10:36:09 -0300

    Welcome to the future

--- w3af/core/controllers/misc/is_private_site.py
+++ w3af/core/controllers/misc/is_private_site.py
@@ -23,29 +23,45 @@ import re
 import socket
 
 
-def is_private_site(domain_or_IP_address):
+TEN_X = re.compile('(10\.\d?\d?\d?\.\d?\d?\d?\.\d?\d?\d?)')
+ONE_SEVEN_TWO = re.compile('(172\.[1-3]\d?\d?\.\d?\d?\d?\.\d?\d?\d?)')
+ONE_NINE_TWO = re.compile('(192\.168\.\d?\d?\d?\.\d?\d?\d?)')
+ONE_TWO_SEVEN = re.compile('(127\.\d?\d?\d?\.\d?\d?\d?\.\d?\d?\d?)')
+
+
+def is_private_site(domain_or_ip_address):
     """
-    :param domain_or_IP_address: The domain or IP address that we want to check
+    :param domain_or_ip_address: The domain or IP address that we want to check
     :return: Get the IP address of the domain, return True if its a private address.
     """
-    if re.match('(10\.\d?\d?\d?\.\d?\d?\d?\.\d?\d?\d?)', domain_or_IP_address) or\
-    re.match('(172\.[1-3]\d?\d?\.\d?\d?\d?\.\d?\d?\d?)', domain_or_IP_address) or\
-    re.match('(192\.168\.\d?\d?\d?\.\d?\d?\d?)', domain_or_IP_address) or\
-    re.match('(127\.\d?\d?\d?\.\d?\d?\d?\.\d?\d?\d?)', domain_or_IP_address):
+    if matches_private_ip(domain_or_ip_address):
+        return True
+
+    try:
+        addrinfo = socket.getaddrinfo(domain_or_ip_address, 0)
+    except socket.gaierror:
+        # If I can't resolve this DNS name, then it's a private domain
         return True
     else:
-        addrinfo = None
-        try:
-            addrinfo = socket.getaddrinfo(domain_or_IP_address, 0)
-        except socket.gaierror:
-            # If I can't resolve this DNS name, then it's a private domain
-            return True
-        else:
-            ip_address_list = [info[4][0] for info in addrinfo]
-            for ip_address in ip_address_list:
-                if re.match('(10\.\d?\d?\d?\.\d?\d?\d?\.\d?\d?\d?)', ip_address) or\
-                re.match('(172\.[1-3]\d?\d?\.\d?\d?\d?\.\d?\d?\d?)', ip_address) or\
-                re.match('(192\.168\.\d?\d?\d?\.\d?\d?\d?)', ip_address) or\
-                re.match('(127\.\d?\d?\d?\.\d?\d?\d?\.\d?\d?\d?)', ip_address):
-                    return True
+        ip_address_list = [info[4][0] for info in addrinfo]
+        for ip_address in ip_address_list:
+            if matches_private_ip(ip_address):
+                return True
+
+    return False
+
+
+def matches_private_ip(ip_address):
+    if TEN_X.match(ip_address):
+        return True
+
+    if ONE_SEVEN_TWO.match(ip_address):
+        return True
+
+    if ONE_NINE_TWO.match(ip_address):
+        return True
+
+    if ONE_TWO_SEVEN.match(ip_address):
+        return True
+
     return False
