ddcb4abcddce3f1a5f056785df9749de505e066e
Andres Riancho <andres.riancho@gmail.com>
2018-11-29 09:32:54 -0300

    Better logging

--- w3af/core/controllers/core_helpers/not_found/generate_404.py
+++ w3af/core/controllers/core_helpers/not_found/generate_404.py
@@ -188,8 +188,8 @@ def send_404(uri_opener, url404, debugging_id=None):
         om.out.debug(message % args)
         raise FourOhFourDetectionException(message % args)
     else:
-        msg = 'Generated forced 404 for %s (id:%s, did:%s)'
-        args = (url404, response.id, debugging_id)
+        msg = 'Generated forced 404 for %s (id:%s, did:%s, len:%s)'
+        args = (url404, response.id, debugging_id, len(response.body))
         om.out.debug(msg % args)
 
     return response
