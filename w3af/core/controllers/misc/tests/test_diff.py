d6a4b7aeaf118b8c03ba1c327c73875ef1f266e7
Andres Riancho <andres.riancho@gmail.com>
2019-02-16 17:21:32 -0300

    Fix unittests and added perf test for split by sep

--- w3af/core/controllers/misc/tests/test_diff.py
+++ w3af/core/controllers/misc/tests/test_diff.py
@@ -20,9 +20,10 @@ along with w3af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 """
+import re
 import unittest
 
-from w3af.core.controllers.misc.diff import chunked_diff, diff_dmp
+from w3af.core.controllers.misc.diff import chunked_diff, diff_dmp, split_by_sep
 
 
 class TestChunkedDiff(unittest.TestCase):
@@ -30,13 +31,23 @@ class TestChunkedDiff(unittest.TestCase):
     def test_equal(self):
         self.assertEqual(chunked_diff('123456', '123456'), ('', ''))
 
-    def test_middle(self):
+    def test_middle_0(self):
         self.assertEqual(chunked_diff('123456', '123a56'),
-                         ('4', 'a'))
+                         ('123456', '123a56'))
 
-    def test_start(self):
+    def test_middle_1(self):
+        a = 'A\nB\nC'
+        b = 'A\nX\nC'
+        self.assertEqual(chunked_diff(a, b), ('B', 'X'))
+
+    def test_start_0(self):
         self.assertEqual(chunked_diff('yes 123abc', 'no 123abc'),
-                         ('yes', 'no'))
+                         ('yes 123abc', 'no 123abc'))
+
+    def test_start_1(self):
+        a = 'X\nB\nC'
+        b = 'A\nB\nC'
+        self.assertEqual(chunked_diff(a, b), ('X', 'A'))
 
     def test_end(self):
         self.assertEqual(chunked_diff('123abc\nyes', '123abc\nno'),
@@ -45,11 +56,6 @@ class TestChunkedDiff(unittest.TestCase):
     def test_nono(self):
         self.assertEqual(chunked_diff('123abc\nyes', 'no\n123abc\nno'),
                          ('yes', 'nono'))
-
-    def test_middle(self):
-        a = 'A\nB\nC'
-        b = 'A\nX\nC'
-        self.assertEqual(chunked_diff(a, b), ('B', 'X'))
 
     def test_all_no_sep(self):
         a = 'ABC'
@@ -64,11 +70,6 @@ class TestChunkedDiff(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(chunked_diff('', ''), ('', ''))
 
-    def test_start(self):
-        a = 'X\nB\nC'
-        b = 'A\nB\nC'
-        self.assertEqual(chunked_diff(a, b), ('X', 'A'))
-
     def test_special_chars(self):
         a = 'X\tB\nC'
         b = 'A<B\nC'
@@ -80,13 +81,23 @@ class TestDiffDMP(unittest.TestCase):
     def test_equal(self):
         self.assertEqual(diff_dmp('123456', '123456'), ('', ''))
 
-    def test_middle(self):
+    def test_middle_0(self):
         self.assertEqual(diff_dmp('123456', '123a56'),
                          ('4', 'a'))
 
-    def test_start(self):
+    def test_middle_1(self):
+        a = 'A\nB\nC'
+        b = 'A\nX\nC'
+        self.assertEqual(diff_dmp(a, b), ('B', 'X'))
+
+    def test_start_0(self):
         self.assertEqual(diff_dmp('yes 123abc', 'no 123abc'),
                          ('yes', 'no'))
+
+    def test_start_1(self):
+        a = 'X\nB\nC'
+        b = 'A\nB\nC'
+        self.assertEqual(diff_dmp(a, b), ('X', 'A'))
 
     def test_end(self):
         self.assertEqual(diff_dmp('123abc yes', '123abc no'),
@@ -95,11 +106,6 @@ class TestDiffDMP(unittest.TestCase):
     def test_nono(self):
         self.assertEqual(diff_dmp('123abc yes', 'no 123abc no'),
                          ('yes', 'no \nno'))
-
-    def test_middle(self):
-        a = 'A\nB\nC'
-        b = 'A\nX\nC'
-        self.assertEqual(diff_dmp(a, b), ('B', 'X'))
 
     def test_all_no_sep(self):
         a = 'ABC'
@@ -114,12 +120,20 @@ class TestDiffDMP(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(diff_dmp('', ''), ('', ''))
 
-    def test_start(self):
-        a = 'X\nB\nC'
-        b = 'A\nB\nC'
-        self.assertEqual(diff_dmp(a, b), ('X', 'A'))
-
     def test_special_chars(self):
         a = 'X\tB\nC'
         b = 'A<B\nC'
         self.assertEqual(diff_dmp(a, b), ('X\t', 'A<'))
+
+
+class TestSplitBySep(unittest.TestCase):
+    def test_split_by_sep_perf(self):
+        loops = 1000
+        inputs = [unittest.__doc__,
+                  re.__doc__,
+                  '',
+                  'hello world<bye bye!']
+
+        for _ in xrange(loops):
+            for _input in inputs:
+                split_by_sep(_input)
