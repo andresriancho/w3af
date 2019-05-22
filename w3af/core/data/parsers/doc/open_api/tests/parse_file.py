a8155d68cdca12fc5e0e4a005b8c5ec9ef14af1b
Andres Riancho <andres.riancho@gmail.com>
2019-05-22 10:05:39 -0300

    Improve test script: include output

--- w3af/core/data/parsers/doc/open_api/tests/parse_file.py
+++ w3af/core/data/parsers/doc/open_api/tests/parse_file.py
@@ -1,14 +1,37 @@
-import json
-from bravado_core.spec import Spec
+import os
+import sys
+import pprint
 
-config = {'use_models': False}
-url = 'http://w3af.org/api/'
-_file = 'data/simple.json'
+sys.path.append(os.getcwd())
 
-spec_dict = json.loads(file(_file).read())
+from w3af.core.data.dc.headers import Headers
+from w3af.core.data.parsers.doc.url import URL
+from w3af.core.data.parsers.doc.open_api import OpenAPI
+from w3af.core.data.url.HTTPResponse import HTTPResponse
 
-spec = Spec.from_dict(spec_dict,
-                      origin_url=url,
-                      config=config)
+spec_filename = sys.argv[1]
+
+_, extension = os.path.splitext(spec_filename)
+
+body = file(spec_filename).read()
+headers = Headers({'Content-Type': 'application/%s' % extension}.items())
+response = HTTPResponse(200, body, headers,
+                        URL('http://moth/swagger.%s' % extension),
+                        URL('http://moth/swagger.%s' % extension),
+                        _id=1)
 
 
+parser = OpenAPI(response)
+parser.parse()
+api_calls = parser.get_api_calls()
+
+for api_call in api_calls:
+    method = api_call.get_method()
+    headers = api_call.get_headers()
+    data = api_call.get_data()
+
+    uri = api_call.get_uri().url_string
+
+    data = (method, uri, headers, data)
+
+    pprint.pprint(data)
