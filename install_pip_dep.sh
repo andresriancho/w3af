d1c4c1df57345da9382f2b8ad1be20dbd2815094
root <root@servicesvm.internal.lan>
2019-10-17 20:52:47 +0000

    dep script create

--- install_pip_dep.sh
+++ install_pip_dep.sh
@@ -1,44 +1,153 @@
-pip2 install Flask
-pip2 install git.util
-pip2 install github
-pip2 install GitPython
-pip2 install guess_language
-pip2 install guess-language
-pip2 install Halberd
-pip2 install ipaddresses
-pip2 install jinja2
-pip2 install lxml
-pip2 install lz4
-pip2 install markdown
-pip2 install mitmproxy
-pip2 install msgpack
-pip2 install ndg-httpsclient
-pip2 install nltk
-pip2 install ntlm
-pip2 install pdfminer
-pip2 install pebble
-pip2 install phply
-pip2 install psutil
-pip2 install pyasn1
-pip2 install pybloomfilter
+sudo apt-get install build-essential autoconf libtool pkg-config python-opengl npm python-pil python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libssl-dev
+
+sudo apt-get install libxml2-dev libxslt-dev python-dev
+
 pip2 install pyclamd
-pip2 install PyGithub
-pip2 install pyOpenSSL
-pip2 install python-ntlm
-pip2 install scapy
-pip2 install scapy.config
-pip2 install subprocess32
-pip2 install tldextract
-pip2 install vulners
-pip2 install darts.util.lru
-pip2 install future
-pip2 install futures
-pip2 install gitpython
-pip2 install ndg-httpsclient
-pip2 install ruamel_yaml
+
+pip2 install github
+
+pip2 install git.util
+
+pip2 install pybloomfilter
+
+pip2 install phply
+
+pip2 install nltk
+
+pip2 install chardet
+
 pip2 install tblib
-pip2 install termcolor
-pip2 install vulndb
-pip2 install pebble
+
+pip2 install pdfminer
+
+pip2 install concurrent.futures
+
+pip2 install OpenSSL
+
+pip2 install ndg
+
+pip2 install pyasn1
+
 pip2 install lxml
+
+pip2 install scapy.config
+
+pip2 install guess_language
+
+pip2 install cluster
+
+pip2 install msgpack
+
+pip2 install ntlm
+
+pip2 install Halberd
+
+pip2 install darts.lib.utils
+
+pip2 install jinja2
+
+pip2 install vulndb
+
+pip2 install markdown
+
+pip2 install psutil
+
+pip2 install ds_store
+
+pip2 install termcolor
+
+pip2 install mitmproxy
+
+
+pip2 install ruamel.ordereddict
+
+pip2 install Flask
+
+pip2 install yaml
+
+pip2 install tldextract
+
+pip2 install pebble
+
+pip2 install acora
+
+pip2 install esmre
+
+pip2 install diff_match_patch
+
+pip2 install bravado_core
+
+pip2 install lz4
+
+pip2 install vulners
+
+pip2 install ipaddresses
+
+pip2 install subprocess32
+
+
+
+pip2 install PyGithub==1.21.0
+
+pip2 install GitPython==2.1.3
+
+pip2 install pybloomfiltermmap==0.3.14
+
+pip2 install phply==0.9.1
+
+pip2 install nltk==3.0.1
+
+pip2 install tblib==0.2.0
+
+pip2 install pdfminer==20140328
+
+pip2 install futures==3.2.0
+
+pip2 install pyOpenSSL==18.0.0
+
+pip2 install ndg-httpsclient==0.4.0
+
+pip2 install pyasn1==0.4.2
+
+pip2 install lxml==3.4.4
+
+pip2 install scapy==2.4.0
+
+pip2 install cluster==1.1.1b3
+
+pip2 install msgpack==0.5.6
+
+pip2 install python-ntlm==1.0.1
+
+pip2 install darts.util.lru==0.5
+
+pip2 install Jinja2==2.10
+
+pip2 install vulndb==0.1.1
+
+pip2 install markdown==2.6.1
+
+pip2 install psutil==5.4.8
+
+pip2 install mitmproxy==0.13
+
+pip2 install ruamel.ordereddict==0.4.8
+
+pip2 install Flask==0.10.1
+
+pip2 install PyYAML==3.12
+
+pip2 install tldextract==1.7.2
+
+pip2 install pebble==4.3.8
+
+pip2 install acora==2.1
+
+pip2 install diff-match-patch==20121119
+
+pip2 install bravado-core==5.12.1
+
+pip2 install lz4==1.1.0
+
+pip2 install vulners==1.3.0
 
