e237ba5fe1431738c2c0f801aa47eea4455a2a6b
hougomartim <mr.parishan@gmail.com>
2019-10-30 09:45:46 +0330

    Update payloads.py

--- w3af/plugins/attack/db/sqlmap/lib/parse/payloads.py
+++ w3af/plugins/attack/db/sqlmap/lib/parse/payloads.py
@@ -74,11 +74,11 @@ def parseXmlNode(node):
 def loadBoundaries():
     try:
         doc = et.parse(paths.BOUNDARIES_XML)
-    except Exception as ex:
+    except Exception, ex:
         errMsg = "something appears to be wrong with "
         errMsg += "the file '%s' ('%s'). Please make " % (paths.BOUNDARIES_XML, getSafeExString(ex))
         errMsg += "sure that you haven't made any changes to it"
-        raise SqlmapInstallationException as errMsg
+        raise SqlmapInstallationException, errMsg
 
     root = doc.getroot()
     parseXmlNode(root)
@@ -89,11 +89,11 @@ def loadPayloads():
 
         try:
             doc = et.parse(payloadFilePath)
-        except Exception as ex:
+        except Exception, ex:
             errMsg = "something appears to be wrong with "
             errMsg += "the file '%s' ('%s'). Please make " % (payloadFilePath, getSafeExString(ex))
             errMsg += "sure that you haven't made any changes to it"
-            raise SqlmapInstallationException as errMsg
+            raise SqlmapInstallationException, errMsg
 
         root = doc.getroot()
         parseXmlNode(root)
